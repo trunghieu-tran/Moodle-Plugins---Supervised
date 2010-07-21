e673aead23286e900ac2b87f776f96baced1937f
????? <?????@localhost>
2010-07-21 16:24:02 +0400

    ???????? ???? lang\en_utf8\qtype_preg.php, ?.?. ? ???? ??????????? ??????? ???????????.

---
+++ lang/en_utf8/qtype_preg.php
@@ -0,0 +1,10 @@
+<?php // $Id: qtype_preg.php,v 1.1.2.2 2009/08/31 16:37:53 arborrow Exp $
+$string['answerno'] = 'Answer $a';
+$string['addingpreg'] = 'Adding regular expression question';
+$string['addmoreanswerblanks'] = 'Adding regular expression options';
+$string['correctanswer'] =' Correct answer';
+$string['editingpreg'] = 'Editing regular expression question';
+$string['exactmatch'] = 'Exact matching';
+$string['norightanswermatch'] = 'No maximum grade regular expression matches correct answer.';
+$string['preg'] = 'Regular expression';
+?>
