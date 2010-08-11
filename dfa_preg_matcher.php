20c8b18b2678c059ef2874c3e0b1a22da20d8b45
????? <?????@localhost>
2010-08-12 01:03:20 +0400

    ?????? ????????? ??????????? ???????????.

--- dfa_preg_matcher.php
+++ dfa_preg_matcher.php
@@ -14,6 +14,12 @@
 
 require_once($CFG->dirroot . '/question/type/preg/preg_matcher.php');
 
+define('MAX_STATE_COUNT', 64);/* if you put large constant here, than big dfa will be
+                                  correct, but big dfa will be build slow, if you small
+                                  constant here dfa will must small, but complexy regex
+                                  will be get error on validation.
+                               */
+
 class finite_automate_state {//finite automate state
     var $asserts;
     var $passages;//contain numbers of state which can go from this
@@ -43,10 +49,9 @@ class dfa_preg_matcher extends preg_matcher {
     *@return array of errors, if no error - return true.
     */
     static function validate($regex) {
-        $matcher = new dfa_preg_matcher;
         $errors = array();
-        //building tree
-        $matcher->build_tree($regex);
+        //building tree and dfa
+        $matcher = new dfa_preg_matcher($regex);
         //validation tree
         $for_regexp=$regex;
         if (strpos($for_regexp,'/')!==false) {//escape any slashes
@@ -57,6 +62,9 @@ class dfa_preg_matcher extends preg_matcher {
             dfa_preg_matcher::find_unsupported_operation($matcher->roots[0], $errors);
         } else {
             $errors[0] = 'incorrectregex';
+        }
+        if (count($matcher->finiteautomates, COUNT_RECURSIVE) > MAX_STATE_COUNT) {
+            $errors[1] = 'largedfa';
         }
         if (!count($errors)) {
             return true;
@@ -72,37 +80,37 @@ class dfa_preg_matcher extends preg_matcher {
     static function find_unsupported_operation($node, &$errors) {
         switch ($node->subtype) {
             case LEAF_LINK:
-                $errors[1] = 'link';
+                $errors[2] = 'link';
                 break;
             case NODE_ITER:
             case NODE_PLUSQUANT:
             case NODE_QUANT:
             case NODE_QUESTQUANT:
                 if (!$node->greed) {
-                    $errors[2] = 'lazyquant';
+                    $errors[3] = 'lazyquant';
                 }
                 dfa_preg_matcher::find_unsupported_operation($node->firop, $errors);
                 break;
             case  NODE_SUBPATT:
-                $errors[3] = 'subpattern';
+                $errors[4] = 'subpattern';
                 dfa_preg_matcher::find_unsupported_operation($node->firop, $errors);
                 break;
             case NODE_CONDSUBPATT:
-                $errors[4] = 'condsubpatt';
+                $errors[5] = 'condsubpatt';
                 dfa_preg_matcher::find_unsupported_operation($node->firop, $errors);
                 dfa_preg_matcher::find_unsupported_operation($node->secop, $errors);
                 dfa_preg_matcher::find_unsupported_operation($node->thirdop, $errors);
                 break;
             case NODE_ASSERTFF:
-                $errors[5] = 'assertff';
+                $errors[6] = 'assertff';
                 dfa_preg_matcher::find_unsupported_operation($node->firop, $errors);
                 break;
             case NODE_ASSERTFB:
-                $errors[6] = 'assertfb';
+                $errors[7] = 'assertfb';
                 dfa_preg_matcher::find_unsupported_operation($node->firop, $errors);
                 break;
             case NODE_ASSERTTB:
-                $errors[7] = 'asserttb';
+                $errors[8] = 'asserttb';
                 dfa_preg_matcher::find_unsupported_operation($node->firop, $errors);
                 break;
             case NODE_ALT:
