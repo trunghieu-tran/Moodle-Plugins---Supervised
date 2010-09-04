4ea51f49d4ef46155148409bc07cb375e01feaee
Teacher <teacher@localhost>
2010-09-05 01:40:55 +0400

    ????????? ?????? ???????. ????? ?? ????????? ?????? ???? ?????? ?????? ?? ????? ??????? ???????? ??? ?????????.

--- edit_preg_form.php
+++ edit_preg_form.php
@@ -27,7 +27,7 @@ class question_edit_preg_form extends question_edit_shortanswer_form {
         $mform->addElement('selectyesno', 'usehint', get_string('usehint','qtype_preg'));
         $mform->setDefault('usehint',0);
         $mform->addElement('text', 'hintpenalty', get_string('hintpenalty','qtype_preg'), array('size' => 3));
-        $mform->setDefault('hintpenalty','0.1');
+        $mform->setDefault('hintpenalty','0.2');
         $mform->setType('hintpenalty', PARAM_NUMBER);
         $creategrades = get_grade_options();
         $mform->addElement('select','hintgradeborder',get_string('hintgradeborder','qtype_preg'),$creategrades->gradeoptions);
