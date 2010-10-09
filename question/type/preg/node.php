954f6ce5d6d49f6367a0eac45636d792d4463450
????? <?????@localhost>
2010-10-09 15:38:49 +0400

    ???????, ?? ?? ???????????? ?????? ?????.

--- question/type/preg/node.php
+++ question/type/preg/node.php
@@ -7,7 +7,8 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
  * @package questions
  */
-if (!defined('NODE')) {
+if (!defined('PREG_NODE')) {
+    define('PREG_NODE','preg_node');
     define('LEAF',0);
     define('NODE',1);
     define('LEAF_CHARCLASS',2);
@@ -30,9 +31,9 @@ if (!defined('NODE')) {
     define('NODE_ASSERTFF',17);
     define('NODE_ASSERTFB',18);
     define('NODE_ONETIMESUBPATT',19);
-    define('ASSERT','1073741824');
-    define('DOT','987654321');
-    define('STREND','123456789');
+    define('ASSERT',1073741824);
+    define('DOT',987654321);
+    define('STREND',123456789);
     define('ERROR',999999);
 
     class node {
@@ -58,6 +59,102 @@ if (!defined('NODE')) {
         function name() {
             return 'node';
         }
+    }
+//разобраться с ф-цией копирование поддерева
+    class preg_node {
+        //constants for number of special leaf types
+        const dot = 987654321;
+        const strend = 123456789;
+        const assert = 1073741824;
+        const error = 999999;
+        //constants for node types
+        const leaf_charclass = 0;
+        const leaf_strend = 1;
+        const leaf_link = 2;
+        const leaf_empty = 3;
+        const leaf_dot = 4;
+        const leaf_wordbreak = 5;
+        const leaf_wordnotbreak = 6;
+        const node_conc = 7;
+        const node_alt = 8;
+        const node_iter = 9;
+        const node_quest = 10;
+        const node_quant = 11;
+        const node_subpatt = 12;
+        const node_condsubpatt = 13;
+        const node_onetimesubpatt = 14;
+        const node_asserttf = 15;
+        const node_assertff = 16;
+        const node_asserttb = 17;
+        const node_assertfb = 18;
+        var $subtype;
+        var $error;
+    }
+    class preg_node_leaf extends preg_node {
+        var $direction;
+        var $chars;
+        /**
+        *Function determine character included in this character class, or not
+        *@param $char character which need search in this charclass
+        *@return true if included else false
+        */
+        public function is_character_included($char) {
+            switch ($this->subtype) {
+                case preg_node::leaf_charclass:
+                    if (strpos($this->chars, $char) !== false) {
+                        $result = true;
+                    } else {
+                        $result = false;
+                    }
+                    break;
+                case preg_node::leaf_dot:
+                    $result = true;
+                    break;
+                case preg_node::leaf_strend:
+                    $result = $char === 123456789 || $char === 0;
+                    break;
+                default:
+                    $result = false;
+                    break;
+            }
+            return $result;
+        }
+    }
+    class preg_node_conc extends preg_node {
+        var $firop;
+        var $secop;
+    }
+    class preg_node_alt extends preg_node {
+        var $firop;
+        var $secop;
+    }
+    class preg_node_iter extends preg_node {
+        var $firop;
+        var $greed;
+    }
+    class preg_node_quest extends preg_node {
+        var $firop;
+        var $greed;
+    }
+    class preg_node_quant extends preg_node {
+        var $firop;
+        var $greed;
+        var $leftborder;
+        var $rightborder;
+    }
+    class preg_node_subpatt extends preg_node {
+        var $firop;
+    }
+    class preg_node_condsubpatt extends preg_node {
+        var $firop;
+        var $secop;
+        var $thirdop;
+    }
+    class preg_node_onetimesubpatt extends preg_node {
+        var $firop;
+    }
+    class preg_node_assert extends preg_node {
+        var $firop;
     }
 }
 ?>
