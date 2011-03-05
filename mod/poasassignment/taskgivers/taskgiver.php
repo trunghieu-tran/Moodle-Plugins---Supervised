f9ed4da7b6c7e901cc733c08bcf9e5d517917c0f
Arkanif <oggfire@gmail.com>
2011-03-05 20:25:06 +0300

    ???????? ? ?????? ?????????? ???????. ???? ???????? ??? - ????????? ????? ? ????? ????????.

---
+++ mod/poasassignment/taskgivers/taskgiver.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of taskgiver
+ *
+ * @author Arkanif
+ */
+abstract class taskgiver {
+    function process_before_tasks() {}
+    function get_task_extra_string(){}
+    function process_after_tasks() {}
+}
+?>
