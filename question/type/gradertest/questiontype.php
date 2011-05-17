2ef6fb848b41210b355b665251d2de5cdf37acdd
Arkanif <oggfire@gmail.com>
2011-05-17 21:35:40 +0400

    ????????????? ? ????? ????? ?? ????????

--- question/type/gradertest/questiontype.php
+++ question/type/gradertest/questiontype.php
@@ -1,5 +1,11 @@
 <?
 class gradertest_qtype extends default_questiontype {
+
+    public $fileoptions = array(
+        'subdirs' => false,
+        'maxfiles' => -1,
+        'maxbytes' => 0,
+        );
     function name() {
         return 'gradertest';
     }
@@ -11,6 +17,7 @@ class gradertest_qtype extends default_questiontype {
         $gradertest->questionid = $question->id;
         $gradertest->text = $question->testtext;
         $gradertest->files = 42;
+        $this->save_testfiles($question->testfiles, $question->context, 'questiontext', $question->id);
         if (!$DB->record_exists('gradertest', array('questionid' => $question->id))) {
             $DB->insert_record('gradertest', $gradertest);
         }
@@ -21,11 +28,25 @@ class gradertest_qtype extends default_questiontype {
         }
         return null;
     }
+    function save_testfiles($element, $context, $area = 'questiontext', $id) {
+        global $DB;
+        $fs = get_file_storage();
+        file_save_draft_area_files($element, 
+                                   $context->id, 
+                                   'question', 
+                                   $area, 
+                                   $id, 
+                                   $this->fileoptions);
+    }
     function get_question_options(&$question) {
         global $DB;
         if ($DB->record_exists('gradertest', array('questionid' => $question->id))) {
             $test = $DB->get_record('gradertest', array('questionid' => $question->id));
             $question->testtext = $test->text;
+            
+            //$draftitemid = file_get_submitted_draft_itemid('testfiles');
+            //file_prepare_draft_area($draftitemid, $question->context->id, 'question', 'gradertests', 0, array('subdirs'=>true));
+            //$question->testfiles = $draftitemid;
         }
     }
 
