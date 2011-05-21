fdfd67e4aa7b6e100b6de602804618854c900ca6
Arkanif <oggfire@gmail.com>
2011-05-21 23:03:54 +0400

    ????????? ????????? ?????? ?????????? ????? ??????????? ????????????. ???? ??????? ???, ?????????? ?????????????? ????????????.

--- mod/poasassignment/taskgivers/randomchoice/lang/en/poasassignmenttaskgivers_randomchoice.php
+++ mod/poasassignment/taskgivers/randomchoice/lang/en/poasassignmenttaskgivers_randomchoice.php
@@ -1,4 +1,5 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Random choice';
-$string['randomchoice']='Random choice';
+$string['randomchoice'] = 'Random choice';
+$string['noavailabletask'] = 'No task for you';
--- mod/poasassignment/taskgivers/randomchoice/randomchoice.php
+++ mod/poasassignment/taskgivers/randomchoice/randomchoice.php
@@ -7,17 +7,21 @@ class randomchoice extends taskgiver {
     public $showtasks = false;
     public $hassettings = false;
 
-    function process_before_tasks($cmid,$poasassignment) {
+    function process_before_tasks($cmid, $poasassignment) {
         global $USER,$DB;
         if(!$DB->record_exists('poasassignment_assignee',array('poasassignmentid'=>$poasassignment->id,'userid'=>$USER->id))) {
-                $tasks=$DB->get_records('poasassignment_tasks',array('poasassignmentid'=>$poasassignment->id,'hidden'=>0));
-                $tasksarray=array();
-                foreach($tasks as $task) $tasksarray[]=$task->id;
-                if(count($tasksarray)>0) {
-                    $taskid=$tasksarray[rand(0,count($tasksarray)-1)];
+                $tasks = poasassignment_model::get_instance($poasassignment)->get_available_tasks($poasassignment->id, $USER->id);
+                $tasksarray = array();
+                foreach($tasks as $task) 
+                    $tasksarray[] = $task->id;
+                if(count($tasksarray) > 0) {
+                    $taskid = $tasksarray[rand(0, count($tasksarray) - 1)];
                     $poasmodel = poasassignment_model::get_instance($poasassignment);
                     $poasmodel->bind_task_to_assignee($USER->id,$taskid);
                     redirect(new moodle_url('view.php',array('id'=>$cmid,'page'=>'view')),null,0);
+                }
+                else {
+                    print_string('noavailabletask','poasassignmenttaskgivers_randomchoice');
                 }
             }
     }
