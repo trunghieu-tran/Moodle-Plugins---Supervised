68a2b0e2b8028fef0e56f1a5aac174885555680c
Arkanif <oggfire@gmail.com>
2011-05-21 23:23:24 +0400

    ???????????? ???????? ? ??????????????? ?????.

--- mod/poasassignment/taskgivers/parameterchoice/lang/en/poasassignmenttaskgivers_parameterchoice.php
+++ mod/poasassignment/taskgivers/parameterchoice/lang/en/poasassignmenttaskgivers_parameterchoice.php
@@ -3,3 +3,5 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Parametric search';
 $string['parameterchoice'] = 'Parametric search';
 $string['makefieldparameters'] = 'Checking field turns it into parameter of search';
+$string['nosatisfyingtasks'] = 'No tasks satisfying to your wishes';
+$string['notasks'] = 'No tasks available';
