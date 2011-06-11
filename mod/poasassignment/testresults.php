1488808bdd457d92455255b296fbc66d2cb3efdd
Arkanif <oggfire@gmail.com>
2011-06-12 01:15:25 +0400

    autotester ?????????? ????? ?????? ? ?????????? ?? ? ????? ????? ??? ???????? ?????????, ????????? ?????????

--- mod/poasassignment/testresults.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-$attemptid = 6;
-global $DB;
-require_once('grader/autotester/autotester.php');
-$grader = new autotester();
-echo $grader->test_attempt($attemptid);
-exec('Call grader/autotester/runattempt' . $attemptid . '.bat');
-$grader->clean_files($attemptid);
