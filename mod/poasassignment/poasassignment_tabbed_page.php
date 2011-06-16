860a92bf8a01c7c203aff072db2969535b1337bf
Arkanif <oggfire@gmail.com>
2011-06-16 21:28:54 +0400

    ???????? ??????? ????????????? ?? ????????????? ?????? ??????

--- mod/poasassignment/poasassignment_tabbed_page.php
+++ mod/poasassignment/poasassignment_tabbed_page.php
@@ -1,13 +1,10 @@
 <?php
-
+require_once('model.php');
 /** Class of view.php page, displays list of pages
  *  and content of current page
  */
 class poasassignment_tabbed_page {
-    private $cm;
-    private $poasassignment;
     private $currentpage;        // name of current page
-    private $context;
 
     /** Standard constuctor for poasassignment_tabbed_page
      * @param $pages array of pages to be displayed
@@ -29,22 +26,18 @@ class poasassignment_tabbed_page {
         } else {
             error(get_string('errornoid','poasassignment'));
         }
+        
+        poasassignment_model::get_instance()->cash_instance($poasassignment->id);
 
         require_login($course, true, $cm);
 
         // Add record to log
         add_to_log($course->id, 'poasassignment', 'view', "view.php?id=$cm->id&page=$page", $poasassignment->name, $cm->id);
 
-        
-        //$PAGE->navbar->add(get_string('view','poasassignment'));
-
-        $this->currentpage=$page;
-        $this->cm=$cm;
-        $this->poasassignment=$poasassignment;
-        $this->context=get_context_instance(CONTEXT_MODULE,$this->cm->id);
+        $this->currentpage = $page;
         
         $PAGE->set_url('/mod/poasassignment/view.php', array('id' => $cm->id,'page'=>$page));
-        $PAGE->set_title($course->shortname.': '.get_string('modulename','poasassignment').': '.$this->poasassignment->name);
+        $PAGE->set_title($course->shortname.': '.get_string('modulmodulename','poasassignment').': '.$poasassignment->name);
         $PAGE->set_heading($course->fullname);
         $PAGE->set_button(update_module_button($cm->id, $course->id, get_string('modulename', 'poasassignment')));
 
@@ -55,67 +48,34 @@ class poasassignment_tabbed_page {
     function view() {
         global $PAGE;
 
-        require_capability('mod/poasassignment:view', $this->context);
+        require_capability('mod/poasassignment:view', poasassignment_model::get_instance()->get_context());
 
         $PAGE->navbar->add(get_string($this->currentpage, 'poasassignment'));
 
         $this->view_header($this->currentpage);
 
         // Check available date or students
-        if (time()<$this->poasassignment->availabledate && !has_capability('mod/poasassignment:managetasks', $this->context)) {
+        if (time() < poasassignment_model::get_instance()->get_poasassignment()->availabledate 
+            && !has_capability('mod/poasassignment:managetasks', poasassignment_model::get_instance()->get_context())) {
+            
             echo get_string('thismoduleisntopenedyet', 'poasassignment');
         }
         else {
-            // TODO: убрать метод view_pages
-            //$this->view_pages($this->cm);
             $this->view_body();
         }
         $this->view_footer();        
-    }
-
-    /** Displays pages at the top of page
-     */
-    function view_pages() {
-        global $CFG;
-        $cm=$this->cm;
-        // for all pages in $pages array
-        for ($i = 0; $i < count($this->pages); $i++) {
-            $pagei = $this->pages[$i];
-            $pagetype = $pagei.'_page';
-            require_once($pagetype.'.php');
-            
-            // If user has ability to view <pagename>_page - add page on panel
-            $pageinstance = new $pagetype($cm, $this->poasassignment);
-            if ($pageinstance->has_ability_to_view()) {
-                $row[] = new pageobject($pagei, "$CFG->wwwroot/mod/poasassignment/view.php?id=$cm->id&page=".$pagei,get_string($pagei, 'poasassignment'));
-            }
-        }
-
-        // Show pages panel
-        $pages[] = $row;
-        $inactive[] = $this->currentpage;
-        $activated[] = $this->currentpage;
-        if (count($row)>1)
-            print_pages($pages, $this->currentpage, $inactive, $activated);
     }
 
     /** Displays general content of the page
      */
     function view_body() {
         $pagetype = $this->currentpage."_page";
-        //if(file_exists($pagetype.'.php')) {
-        //    require_once($pagetype.'.php');
-        //    $poasassignmentpage = new $pagetype($this->cm, $this->poasassignment);
-        //    $poasassignmentpage->require_ability_to_view();
-        //    $poasassignmentpage->view();
-        //}
-        //else {
         $currentpath = poasassignment_model::$extpages[$this->currentpage];
         require_once($currentpath);
-        $poasassignmentpage = new $pagetype($this->cm, $this->poasassignment);
+        $poasassignmentpage = new $pagetype(poasassignment_model::get_instance()->get_cm(), 
+                                            poasassignment_model::get_instance()->get_poasassignment());
         $poasassignmentpage->require_ability_to_view();
         $poasassignmentpage->view();
-        //}
     }
 
     /** Dislpays header
@@ -123,7 +83,7 @@ class poasassignment_tabbed_page {
     function view_header() {
         global $OUTPUT;
         echo $OUTPUT->header();
-        echo $OUTPUT->heading($this->poasassignment->name.' : '.
+        echo $OUTPUT->heading(poasassignment_model::get_instance()->get_poasassignment()->name.' : '.
                         get_string($this->currentpage, 'poasassignment').
                         $OUTPUT->help_icon($this->currentpage, 'poasassignment'));
     }
