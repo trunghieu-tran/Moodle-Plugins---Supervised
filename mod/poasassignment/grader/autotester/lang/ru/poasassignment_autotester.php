6a4a9a0628ae58173070eb73b725ad60bbeac199
Arkanif <oggfire@gmail.com>
2011-06-18 01:04:13 +0400

    ????????? ?????? ??????? ??????????? ???? ??? ???????? ? ???????? ??????????? ??????????, ?? ?????? ???????????? ???????? ?????? ????? ?????? ????????

---
+++ mod/poasassignment/grader/autotester/lang/ru/poasassignment_autotester.php
@@ -0,0 +1,12 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$string['pluginname'] = 'Автотестер';
+$string['autotester'] = 'Автотестер';
+$string['autotester_help'] = 'Компилирует исходный код и тестирует программу';
+$string['textanswermustbeenabled'] = 'Текстовые ответы должны быть активированы';
+$string['testname'] = 'Название теста';
+$string['testin'] = 'Входные данные теста'; 
+$string['testout'] = 'Выходные данные теста';
+$string['studentout'] = 'Выходные данные программы';
+$string['testpassed'] = 'Тест успешно пройден';
+$string['testnotpassed'] = 'Тест не пройден';
