a2c44b0f0252cc2311f75c63b39eddf741b08d5d
oasychev <oasychev@gmail.com>
2011-11-13 02:05:06 +0300

    ???????? ?????? ?????? ????????? ??? ???? ??????? preg. ????????? shortanswer. ???? display.html ?????? ??? ??????????????: ?????? ??? ???????? ????????.

--- question/type/preg/questiontype.php
+++ question/type/preg/questiontype.php
@@ -24,7 +24,7 @@ class question_preg_qtype extends question_shortanswer_qtype {
     public function available_engines() {
         return array('preg_php_matcher' => get_string('preg_php_matcher','qtype_preg'),
                         'dfa_preg_matcher' => get_string('dfa_preg_matcher','qtype_preg'),
-                        'nfa_preg_matcher' => get_string('nfa_preg_matcher','qtype_preg')/*,        
+                        'nfa_preg_matcher' => get_string('nfa_preg_matcher','qtype_preg')/*,
                         'backtracking_preg_matcher' => 'backtracking_preg_matcher'*/);
     }
 
@@ -81,89 +81,7 @@ class question_preg_qtype extends question_shortanswer_qtype {
     }*/
 
 
-    /**
-    * function additionaly fill $state->responses['__answer'] with best fit answer for further hinting
-    */
-/*    function grade_responses(&$question, &$state, $cmoptions) {
-        global $CFG;
-        require_once($CFG->dirroot . '/question/type/preg/'.$question->options->engine.'.php');
-        $querymatcher = new $question->options->engine;//this matcher will be used to query engine capabilities
-        $knowleftcharacters = $querymatcher->is_supporting(preg_matcher::CHARACTERS_LEFT);
-        $ispartialmatching = $querymatcher->is_supporting(preg_matcher::PARTIAL_MATCHING);
-        
-        //Set an initial value for best fit. This is tricky, since when hinting we need first element within hint grade border
-        reset($question->options->answers);
-        $bestfitanswer = current($question->options->answers);
-        if ($ispartialmatching) {
-            foreach ($question->options->answers as $answer) {
-                if ($answer->fraction >= $question->options->hintgradeborder) {
-                    $bestfitanswer = $answer;
-                    break;//anyone that fits border helps
-                }
-            }
-        }
-        //fitness = (the number of correct letters in response) or  (-1)*(the number of letters left to complete response) so we always look for maximum fitness
-        $maxfitness = (-1)*(strlen($state->responses[''])+1);
-        $full = false;
-        foreach ($question->options->answers as $answer) {
-            $matcher =& $this->get_matcher($question->options->engine, $answer->answer, $question->options->exactmatch, $question->options->usecase, $answer->id);
-            $full = $matcher->match($state->responses['']);
-
-            //check full match
-            if ($full) {//don't need to look more if we find full match
-                $bestfitanswer = $answer;
-                $fitness = strlen($state->responses['']);
-                break;
-            }
-
-            //when hinting we should use only answers within hint border except full matching case and there is some match at all
-            //if engine doesn't support hinting we shoudn't bother with fitness too
-            if (!$ispartialmatching || !$matcher->match_found() || $answer->fraction < $question->options->hintgradeborder) {
-                continue;
-            }
-
-            //calculate fitness now
-            if ($knowleftcharacters) {//engine could tell us how many characters left to complete response, this is the best fitness possible
-                $fitness = (-1)*$matcher->characters_left();//-1 cause the less we need to add the better
-            } else {//we should rely on the length of correct response part
-                $fitness = $matcher->last_correct_character_index() - $matcher->first_correct_character_index() + 1;
-            }
-
-            if ($fitness > $maxfitness) {
-                $maxfitness = $fitness;
-                $bestfitanswer = $answer;
-            }
-        }
-
-        //save best fitted answer for further uses
-        $state->responses['__answer'] = $bestfitanswer;
-
-
-        if ($full) {
-            $state->raw_grade = $bestfitanswer->fraction;
-        } else {
-            $state->raw_grade = 0;//TODO - implement partial grades for partially correct answers
-        }
-
-        // Make sure we don't assign negative or too high marks.
-        $state->raw_grade = min(max((float) $state->raw_grade,
-                            0.0), 1.0) * $question->maxgrade;
-
-        // Update the penalty
-        if (isset($state->responses['hint'])) {
-            $state->penalty = $question->options->hintpenalty * $question->maxgrade;
-        } else {
-            $state->penalty = $question->penalty * $question->maxgrade;
-        }
-
-        // mark the state as graded
-        $state->event = ($state->event ==  QUESTION_EVENTCLOSE) ? QUESTION_EVENTCLOSEANDGRADE : QUESTION_EVENTGRADE;
-
-        return true;
-    }
-*/
-
-    function print_question_submit_buttons(&$question, &$state, $cmoptions, $options) {
+     function print_question_submit_buttons(&$question, &$state, $cmoptions, $options) {
         parent::print_question_submit_buttons(&$question, &$state, $cmoptions, $options);
         if (($cmoptions->optionflags & QUESTION_ADAPTIVE) and !$options->readonly and $question->options->usehint) {
             echo '<input type="submit" name="', $question->name_prefix, 'hint" value="',
@@ -172,91 +90,6 @@ class question_preg_qtype extends question_shortanswer_qtype {
         }
     }
 
-    function print_question_formulation_and_controls(&$question, &$state, $cmoptions, $options) {
-        $this->hintmessage = '';
-        if (array_key_exists('__answer',$state->responses)) {//for the first time - no hint message
-            //form hint messages
-            $answer = $state->responses['__answer'];
-            $matcher =& $this->get_matcher($question->options->engine, $answer->answer, $question->options->exactmatch, $question->options->usecase, $answer->id);
-            $response = $state->responses[''];
-            $matcher->match($response);
-
-            //Calculate strings for response coloring
-            if ($matcher->match_found()) {
-                $firstindex = $matcher->first_correct_character_index();
-                $lastindex = $matcher->last_correct_character_index();
-                $wronghead = '';
-                if ($firstindex > 0) {//if there is wrong heading
-                    $wronghead = '<span class="'.question_get_feedback_class(0).'">'.htmlspecialchars(substr($response, 0, $firstindex)).'</span>';
-                }
-                $correctpart = '';
-                if ($firstindex != -1) {//there were any match
-                    $correctpart = '<span class="'.question_get_feedback_class(1).'">'.htmlspecialchars(substr($response, $firstindex, $lastindex - $firstindex + 1)).'</span>';
-                }
-                $hintedcharacter = '';
-                if (isset($state->responses['hint']) && $matcher->is_supporting(preg_matcher::NEXT_CHARACTER)) {//if hint requested and possible
-                    $hintedcharacter = '<span class="'.question_get_feedback_class(0.5).'">'.htmlspecialchars($matcher->next_char()).'</span>';
-                }
-                $wrongtail = '';
-                if ($lastindex + 1 < strlen($response)) {//if there is wrong tail
-                    $wrongtail = '<span class="'.question_get_feedback_class(0).'">'.htmlspecialchars(substr($response, $lastindex + 1, strlen($response) - $lastindex - 1)).'</span>';
-                }
-
-                //We shouldn't show colored message if there is no match and partial matching is unavailable, because we could mislead student marking all reponse as incorrect, while the correct parts may be there
-                if ($matcher->is_matching_complete() || $matcher->is_supporting(preg_matcher::PARTIAL_MATCHING)) {
-                    $this->hintmessage = $wronghead.$correctpart.$hintedcharacter.$wrongtail;
-                }
-                if (!empty($this->hintmessage)) {
-                    $this->hintmessage .= '<br />';
-                }
-            }
-        }
-
-        parent::print_question_formulation_and_controls($question, $state, $cmoptions, $options);
-    }
-
-    function get_display_html_path() {
-         global $CFG;
-         return $CFG->dirroot.'/question/type/preg/display.html';
-    }
-
-    /*
-    * Insert subpatterns in the subject string instead of {$x} placeholders, where {$0} is the whole match, {$1}  - first subpattern ets
-    @param subject string to insert subpatterns
-    @param question question object to create matcher
-    @param state state of the question attempt to get response
-    @return changed string
-    */
-    function insert_subpatterns($subject, $question, $state) {
-        //Sanity check 
-        if (strpos($subject,'{$') === false || strpos($subject,'}') === false 
-            || !array_key_exists('__answer',$state->responses)) {
-            //there are no placeholders for sure or no match found 
-            return $subject;
-        }
-
-        $answer = $state->responses['__answer'];
-        $matcher =& $this->get_matcher($question->options->engine, $answer->answer, $question->options->exactmatch, $question->options->usecase, $answer->id);
-        $response = $state->responses[''];
-        $matcher->match($response);
-
-        $subpatterncount = $matcher->count_subpatterns();
-
-        for ($i=0;$i <= $subpatterncount;$i++) {//<= because full match counts too
-            $search = '{$'.$i.'}';
-            $startindex = $matcher->first_correct_character_index($i);
-            $endindex = $matcher->last_correct_character_index($i);
-            $replace = substr($response, $startindex, $endindex - $startindex + 1);
-            $subject = str_replace($search, $replace, $subject);
-        }
-
-        return $subject;
-    }
 }
 //// END OF CLASS ////
-
-//////////////////////////////////////////////////////////////////////////
-//// INITIATION - Without this line the question type is not in use... ///
-//////////////////////////////////////////////////////////////////////////
-question_register_questiontype(new question_preg_qtype());
 ?>
---
+++ question/type/preg/renderer.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Preg question renderer class.
+ *
+ * @package    qtype
+ * @subpackage preg
+ * @copyright  2011 Oleg Sychev
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Generates the output for short answer questions.
+ *
+ * @copyright  2011 Oleg Sychev
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_preg_renderer extends qtype_shortanswer_renderer {
+    public function formulation_and_controls(question_attempt $qa,
+            question_display_options $options) {
+
+        $question = $qa->get_question();
+        $currentanswer = $qa->get_last_qt_var('answer');
+        $result = parent::formulation_and_controls($qa,$options);
+
+        //Show colored string if appropriable (i.e. any answer is given)
+        //We must show colored string along with specific_feedback if after student answered wrong specific feedback is shown, but general feedback is not
+        //If all feedback is shown alike, colored string should be shown there
+        
+    }
+
+    public function specific_feedback(question_attempt $qa) {
+
+        $question = $qa->get_question();
+        $currentanswer = $qa->get_last_qt_var('answer');
+        if(!$currentanswer) {//TODO - check with 0 and 000 as answer - maybe Tim is wrong!!!
+            return '';
+        }
+
+        //////Colored string
+        $bestfit = $question->get_best_fit_answer(array('answer' => $currentanswer));
+        $asnwer = $bestfit['answer'];
+        $matchresults = $bestfit['match'];
+        $hintmessage = '';
+        //Calculate strings for response coloring
+        if ($matchresults['is_match']) {
+            $firstindex = $matchresults['index_first'][0];
+            $lastindex = $matchresults['index_last'][0];
+
+            $wronghead = '';
+            if ($firstindex > 0) {//if there is wrong heading
+                $wronghead = html_writer::tag('span', htmlspecialchars(substr($currentanswer, 0, $firstindex)), array('class' => $this->feedback_class(0)));
+            }
+            $correctpart = '';
+            $spanattrs['class'] = ;
+            if ($firstindex != -1) {//there were any match
+                $correctpart = html_writer::tag('span', htmlspecialchars(substr($currentanswer, $firstindex, $lastindex - $firstindex + 1)), array('class' => $this->feedback_class(1)));
+            }
+            $hintedcharacter = '';
+            if (/*isset($state->responses['hint']) &&*/ isset($matchresults['next'])) {//if hint requested and possible - TODO check if hint is requested using behavour...
+                $hintedcharacter = html_writer::tag('span', htmlspecialchars($matchresults['next']), array('class' => $this->feedback_class(0.5)));
+            }
+            $wrongtail = '';
+            $spanattrs['class'] = ;
+            if ($lastindex + 1 < strlen($currentanswer)) {//if there is wrong tail
+                $wrongtail =  html_writer::tag('span', htmlspecialchars(substr($currentanswer, $lastindex + 1, strlen($currentanswer) - $lastindex - 1)), array('class' => $this->feedback_class(0)));
+            }
+
+            $hintmessage = $wronghead.$correctpart.$hintedcharacter.$wrongtail;
+            $hintmessage .= html_writer::empty_tag('br');
+        }
+
+        /*TODO - find out how to define classes in plugins and add separate class for hint message*/
+        //$result = html_writer::tag('div', $hintmessage, array('class' => 'specificfeedback'));//TODO  - this may not be needed as rendererbase.php provides div on it's own
+
+        //////Teacher-defined feedback text for that answer
+        $feedback = '';
+        if($answer->feedback) {
+            $feedbacktext = $question->insert_subpatterns($answer->feedback, array('answer' => $currentanswer));
+            $feedback = question->format_text($feedbacktext, $answer->feedbackformat,
+                $qa, 'question', 'answerfeedback', $answer->id);
+        }
+
+        return $hintmessage.$feedback;
+    }
+
+    public function correct_response(question_attempt $qa) {
+        $question = ;
+
+        $answer = $qa->get_question()->get_correct_response()['answer'];
+        if (!$answer) { //Correct answer isn't set by the teacher
+            return '';
+        }
+
+        return get_string('correctansweris', 'qtype_shortanswer', s($answer));
+    }
+}
