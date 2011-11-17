b38858ea702e66989869e8e67813459cdff0a62a
oasychev <oasychev@gmail.com>
2011-11-18 02:32:58 +0300

    ????????? ??????? ??????????? ????????? ? ???????????: * ??????? ???????? ?? ???? ???????? ???????? ??? ?????? ??? ????????????? ?????????? * ??????? ????????????? ??????? ?????? (????? ??????????? ?????? ?? ?????????) * ????????? ????????????? ?????? ??? ?????????? * ????????? ?????????????? ? ???????? ?? ??????, ? ????? ?????? ???

--- question/behaviour/adaptivewithhint/lang/en/qbehaviour_adaptivewithhint.php
+++ question/behaviour/adaptivewithhint/lang/en/qbehaviour_adaptivewithhint.php
@@ -8,7 +8,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['hintbtn'] = 'Hint {$a}';
 $string['hintused'] = 'Looked for hint: {$a->hint} for response {$a->response} with penalty {$a->penalty}.';
 $string['pluginname'] = 'Adaptive mode with hinting';
 $string['withpenalty'] = ' with penalty of {$a}';
-$string['withpossiblepenalty'] = ' with possible penalty of {$a}'
+$string['withpossiblepenalty'] = ' with possible penalty of {$a}';
