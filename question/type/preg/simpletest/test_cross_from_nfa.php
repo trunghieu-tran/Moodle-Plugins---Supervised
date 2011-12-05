854cfa8e15654b0d3413c7aa666c909f3eb5cac2
vostreltsov <vostreltsov@gmail.com>
2011-12-05 09:33:44 +0400

    ???? ?? ????????? ?????????? ??????? ??? ???????? ??????? ??????? ? future. ????????? ???????? ? ????????? $ ? ^ ??????? ? ????????? ?????.

--- question/type/preg/simpletest/test_cross_from_nfa.php
+++ question/type/preg/simpletest/test_cross_from_nfa.php
@@ -157,27 +157,6 @@ class test_cross_from_nfa extends preg_cross_tester {
                      'tests'=>array($test1, $test2, $test3));
     }
 
-    function data_for_test_assertions_simple_2() {
-        $test1 = array( 'str'=>'abc?z',
-                        'is_match'=>true,
-                        'full'=>true,
-                        'index_first'=>array(0=>0),
-                        'index_last'=>array(0=>4),
-                        'left'=>array(0),
-                        'next'=>'');
-
-        $test2 = array( 'str'=>'abcaa',
-                        'is_match'=>true,
-                        'full'=>false,
-                        'index_first'=>array(0=>0),
-                        'index_last'=>array(0=>3),
-                        'left'=>array(1),
-                        'next'=>'');    // can't generate a character
-
-        return array('regex'=>'^abc[a-z.?!]\b[a-zA-Z]',
-                     'tests'=>array($test1, $test2));
-    }
-
     function data_for_test_zero_length_loop() {
         $test1 = array( 'str'=>' a',
                         'is_match'=>true,
