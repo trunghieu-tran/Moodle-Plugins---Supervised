07286f71775dd371df029453bedb1a0afba3504e
oasychev <oasychev@gmail.com>
2011-12-07 00:17:02 +0300

    ???????

--- question/type/preg/backup/moodle2/backup_qtype_preg_plugin.class.php
+++ question/type/preg/backup/moodle2/backup_qtype_preg_plugin.class.php
@@ -72,105 +72,6 @@ class backup_qtype_preg_plugin extends backup_qtype_plugin {
     }
 
     /**
-     * Attach to $element (usually questions) the needed backup structures
-     * for question_numerical_units for a given question
-     * Used both by calculated and numerical qtypes
-     */
-    protected function add_question_numerical_units($element) {
-        // Check $element is one nested_backup_element
-        if (! $element instanceof backup_nested_element) {
-            throw new backup_step_exception('question_numerical_units_bad_parent_element', $element);
-        }
-
-        // Define the elements
-        $units = new backup_nested_element('numerical_units');
-        $unit = new backup_nested_element('numerical_unit', array('id'), array(
-            'multiplier', 'unit'));
-
-        // Build the tree
-        $element->add_child($units);
-        $units->add_child($unit);
-
-        // Set the sources
-        $unit->set_source_sql('
-                SELECT *
-                FROM {question_numerical_units}
-                WHERE question = :question
-                ORDER BY id',
-                array('question' => backup::VAR_PARENTID));
-
-        // don't need to annotate ids nor files
-    }
-
-    /**
-     * Attach to $element (usually questions) the needed backup structures
-     * for question_numerical_options for a given question
-     * Used both by calculated and numerical qtypes
-     */
-    protected function add_question_numerical_options($element) {
-        // Check $element is one nested_backup_element
-        if (! $element instanceof backup_nested_element) {
-            throw new backup_step_exception('question_numerical_options_bad_parent_element', $element);
-        }
-
-        // Define the elements
-        $options = new backup_nested_element('numerical_options');
-        $option = new backup_nested_element('numerical_option', array('id'), array(
-            'showunits', 'unitsleft', 'unitgradingtype', 'unitpenalty'));
-
-        // Build the tree
-        $element->add_child($options);
-        $options->add_child($option);
-
-        // Set the sources
-        $option->set_source_table('question_numerical_options', array('question' => backup::VAR_PARENTID));
-
-        // don't need to annotate ids nor files
-    }
-
-    /**
-     * Attach to $element (usually questions) the needed backup structures
-     * for question_datasets for a given question
-     * Used by calculated qtypes
-     */
-    protected function add_question_datasets($element) {
-        // Check $element is one nested_backup_element
-        if (! $element instanceof backup_nested_element) {
-            throw new backup_step_exception('question_datasets_bad_parent_element', $element);
-        }
-
-        // Define the elements
-        $definitions = new backup_nested_element('dataset_definitions');
-        $definition = new backup_nested_element('dataset_definition', array('id'), array(
-            'category', 'name', 'type', 'options',
-            'itemcount'));
-
-        $items = new backup_nested_element('dataset_items');
-        $item = new backup_nested_element('dataset_item', array('id'), array(
-            'number', 'value'));
-
-        // Build the tree
-        $element->add_child($definitions);
-        $definitions->add_child($definition);
-
-        $definition->add_child($items);
-        $items->add_child($item);
-
-        // Set the sources
-        $definition->set_source_sql('SELECT qdd.*
-                                       FROM {question_dataset_definitions} qdd
-                                       JOIN {question_datasets} qd ON qd.datasetdefinition = qdd.id
-                                      WHERE qd.question = ?', array(backup::VAR_PARENTID));
-
-        $item->set_source_table('question_dataset_items', array('definition' => backup::VAR_PARENTID));
-
-        // Aliases
-        $item->set_source_alias('itemnumber', 'number');
-
-        // don't need to annotate ids nor files
-    }
-
-    /**
      * Returns all the components and fileareas used by all the installed qtypes
      *
      * The method introspects each qtype, asking it about fileareas used. Then,
@@ -227,7 +128,7 @@ class backup_qtype_preg_plugin extends backup_qtype_plugin {
     protected function define_question_plugin_structure() {
         $pluginname = $this->pluginname;
         $qtype = question_bank::get_qtype($pluginname);
-        $questionidfield = $qtype->questionid_column_name();
+        $questionidfield = 'question';    //$qtype->questionid_column_name();        TODO use questionid_column_name()
         // Define the virtual plugin element with the condition to fulfill.
         $plugin = $this->get_plugin_element(null, '../../qtype', $pluginname);
         // Create one standard named plugin element (the visible container).
