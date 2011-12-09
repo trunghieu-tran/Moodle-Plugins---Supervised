c803a7d84202879f5df9d7c5eb24f897fa5d608a
oasychev <oasychev@gmail.com>
2011-12-09 21:30:54 +0300

    ??????? ???????????, ???????? ????????? ?? shortanswer.

--- question/type/preg/question.php
+++ question/type/preg/question.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Short answer question definition class.
+ * Perl-compatible regular expression question definition class.
  *
  * @package    qtype
  * @subpackage preg
