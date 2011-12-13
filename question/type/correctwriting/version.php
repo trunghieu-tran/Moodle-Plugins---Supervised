ddf95fd5611bdf7912e36c220b7605e38acc90d3
mamontov <mamontov@localhost>
2011-12-13 19:51:55 +0400

    Added version.php

---
+++ question/type/correctwriting/version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Version information for the correctwriting question type.
+ *
+ * @package    correctwriting
+ * @copyright  2011 Mamontov Dmitry
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2011121310;
+$plugin->requires = 2011121310;
