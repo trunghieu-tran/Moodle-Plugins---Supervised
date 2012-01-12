9b05786769ca8f6289a5bbfd36a9b58e20791b64
vostreltsov <vostreltsov@gmail.com>
2012-01-13 02:13:31 +0300

    ??????? ????????? ???????, ??????? ??????? ?????????????????, ?????? ????????.

--- question/type/preg/version.php
+++ question/type/preg/version.php
@@ -1,6 +1,6 @@
 <?PHP // $Id: version.php,v 1.1.2.3 2009/10/04 19:49:58 oasychev Exp $
 
-$plugin->version  = 2011121200;
+$plugin->version  = 2012011300;
 $plugin->requires = 2011070100;
 $plugin->release = 'Preg 2.1';
 $plugin->maturity = MATURITY_STABLE;
