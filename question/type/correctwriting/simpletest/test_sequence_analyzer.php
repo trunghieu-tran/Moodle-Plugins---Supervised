5c5f8f74eef36a266a615158cf06acdf47eeb5d0
Max Satu <Max Satu@marianna.vmks.ru>
2012-02-02 15:23:08 +0400

    ?????????????? ????? LCS

--- question/type/correctwriting/simpletest/test_sequence_analyzer.php
+++ question/type/correctwriting/simpletest/test_sequence_analyzer.php
@@ -90,7 +90,7 @@ function create_response($types,$values) {
        $avalues=array("I","am","testing","!");
        $rtypes=array("noun","verb","verb");
        $rvalues=array("I","am","testing");
-       $lcs=$this->get_test_lcs($types,$values,$types,$rvalues);
+       $lcs=$this->get_test_lcs($atypes,$avalues,$rtypes,$rvalues);
        //Check LCS props
        $this->assertTrue( $lcs!=null );
        $this->assertTrue( count($lcs)==1 );
@@ -107,7 +107,7 @@ function create_response($types,$values) {
        $avalues=array("I","am","testing","!");
        $atypes=array("noun","verb","verb","exclamation_mark","exclamation_mark");
        $avalues=array("I","am","testing","!","!");
-       $lcs=$this->get_test_lcs($types,$values,$types,$rvalues);
+       $lcs=$this->get_test_lcs($atypes,$avalues,$rtypes,$rvalues);
        //Check LCS props
        $this->assertTrue( $lcs!=null );
        $this->assertTrue( count($lcs)==2 );
@@ -125,61 +125,34 @@ function create_response($types,$values) {
     
     //Tests analyzer for empty lcs
     public function test_empty_lcs() {
-       $answer=array( new qtype_correctwriting_token_base("I","noun",true,new qtype_correctwriting_node_position(0,0,0,3)) );
-       $response=array( new qtype_correctwriting_token_base("I","noun",false,new qtype_correctwriting_node_position(0,0,0,3)));
-       $test_seq_an=new qtype_correctwriting_sequence_analyzer(null,$answer,null,$response);
-       $this->assertTrue($test_seq_an->is_errors() );
-       //Check LCS
-       $lcs=$tests_seq_an->lcs();
-       $this->assertTrue( count($lcs)==0);
+       $types=array("noun");
+       $avalues=array("I");
+       $rvalues=array("She");
+       $lcs=$this->get_test_lcs($types,$avalues,$types,$rvalues);
+       //Check LCS props
+       $this->assertTrue( $lcs!=null );
+       $this->assertTrue( count($lcs)==0 );
     }
     
     //Performs a common test for analyzer
     public function test_common() {
-       $answer=array( new qtype_correctwriting_token_base("This","noun",true,new qtype_correctwriting_node_position(0,0,0,3)),
-                      new qtype_correctwriting_token_base("is","article",true,new qtype_correctwriting_node_position(0,0,5,7)),
-                      new qtype_correctwriting_token_base("example","verb",true,new qtype_correctwriting_node_position(0,0,9,16)),
-                      new qtype_correctwriting_token_base("of","verb",true,new qtype_correctwriting_node_position(0,0,9,16)),
-                      new qtype_correctwriting_token_base("correct","verb",true,new qtype_correctwriting_node_position(0,0,9,16)),
-                      new qtype_correctwriting_token_base("answer","verb",true,new qtype_correctwriting_node_position(0,0,9,16)),
-                      new qtype_correctwriting_token_base(".","dot",true,new qtype_correctwriting_node_position(0,0,17,17))
-                    );
-       $response=array( new qtype_correctwriting_token_base("This","noun",false,new qtype_correctwriting_node_position(0,0,0,3)),
-                        new qtype_correctwriting_token_base("is","article",false,new qtype_correctwriting_node_position(0,0,7,9)),
-                        new qtype_correctwriting_token_base("example","verb",false,new qtype_correctwriting_node_position(0,0,13,20)),
-                        new qtype_correctwriting_token_base("of","exclamation_mark",false,new qtype_correctwriting_node_position(0,0,22,22)),
-                        new qtype_correctwriting_token_base("answer","exclamation_mark",false,new qtype_correctwriting_node_position(0,0,23,23)),
-                        new qtype_correctwriting_token_base("that","exclamation_mark",false,new qtype_correctwriting_node_position(0,0,23,23)),
-                        new qtype_correctwriting_token_base("is","exclamation_mark",false,new qtype_correctwriting_node_position(0,0,23,23)),
-                        new qtype_correctwriting_token_base("incorrect","exclamation_mark",false,new qtype_correctwriting_node_position(0,0,23,23)),
-                        new qtype_correctwriting_token_base(".","dot",false,new qtype_correctwriting_node_position(0,0,23,23)),
-                        new qtype_correctwriting_token_base("This","noun",false,new qtype_correctwriting_node_position(0,0,0,3)),
-                        new qtype_correctwriting_token_base("is","article",false,new qtype_correctwriting_node_position(0,0,7,9))
-                      );
-       $test_seq_an=new qtype_correctwriting_sequence_analyzer(null,$answer,null,$response);
-       //Check some errors
-       $this->assertTrue($test_seq_an->is_errors() );
+       $atypes=array("data","data","data","data","data");
+       $avalues=array("This","is","correct","answer",".");
+       $rtypes=array( "data","data","data",  "data",  "data","data"      ,"data");
+       $rvalues=array("This","not","correct","answer","This","definitely","not");
+       $lcs=$this->get_test_lcs($types,$avalues,$types,$rvalues);
+       //Check LCS props
+       $this->assertTrue( $lcs!=null );
+       $this->assertTrue( count($lcs)==3 );
        
-       //Check LCS
-       $lcs=$tests_seq_an->lcs();
-       $this->assertTrue( count($lcs)==4);
-       
-       $this->assertTrue( array_key_exists($lcs[0],0) && lcs[0][0]==0 );
-       $this->assertTrue( array_key_exists($lcs[0],1) && lcs[0][1]==1 );
-       $this->assertTrue( array_key_exists($lcs[0],2) && lcs[0][2]==2 );
-       $this->assertTrue( array_key_exists($lcs[0],3) && lcs[0][3]==3 );
-       $this->assertTrue( count($lcs[0])==4);
+       $this->assertTrue( count($lcs[0])==2);
+       $this->assertTrue( $lcs[0][2]==2 );
+       $this->assertTrue( $lcs[0][3]==3 );       
 
-       $this->assertTrue( array_key_exists($lcs[1],5) && lcs[1][5]==4 );
        $this->assertTrue( count($lcs[1])==1);
-       
-       $this->assertTrue( array_key_exists($lcs[2],1) && lcs[2][1]==6 );
+       $this->assertTrue( $lcs[1][0]==0 );
+      
        $this->assertTrue( count($lcs[2])==1);
-       
-       $this->assertTrue( array_key_exists($lcs[3],0) && lcs[3][0]==8 );
-       $this->assertTrue( array_key_exists($lcs[3],1) && lcs[3][1]==9 );
-       $this->assertTrue( count($lcs[3])==2);
-       
-       
+       $this->assertTrue( $lcs[2][0]==4 );
     }
  }
