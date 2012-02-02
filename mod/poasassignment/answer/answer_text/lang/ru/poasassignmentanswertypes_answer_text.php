91b2e8c29a2c1124174b97d291820ba5c8f36bae
Arkanif <Arkanif@localhost>
2012-02-02 18:15:21 +0400

    ?????????? ????? ??????? ??????? ? ???? ?????. ?????? ???????? ????????? ?????????? ??????, ???????, ??????????? ???? ??????, ? ??? ?? ???????????? ????????? ?????? ?? ???????? ????????.

--- mod/poasassignment/answer/answer_text/lang/ru/poasassignmentanswertypes_answer_text.php
+++ mod/poasassignment/answer/answer_text/lang/ru/poasassignmentanswertypes_answer_text.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
-$string['pluginname'] = 'Текстовый ответ';
-$string['answertext'] = 'Текстовый ответ';
+$string['pluginname'] = 'Ответ: текст';
+$string['answertext'] = 'Использовать';
 $string['answertext_help'] = 'Студенты вводят текст на на странице ответа';
 $string['answertexteditor'] = 'Введите текст своего ответа в это поле';
