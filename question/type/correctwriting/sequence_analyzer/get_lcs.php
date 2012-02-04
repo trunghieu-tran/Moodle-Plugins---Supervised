dc088d86d33a10776114a29a240069d00b280f8d
Max Satu <Max Satu@marianna.vmks.ru>
2012-02-04 11:48:08 +0400

    ????? ????? ??????????? ?????.

--- question/type/correctwriting/sequence_analyzer/get_lcs.php
+++ question/type/correctwriting/sequence_analyzer/get_lcs.php
@@ -11,34 +11,9 @@
 defined('MOODLE_INTERNAL') || die();
  
 require_once($CFG->dirroot.'/question/type/correctwriting/langs_code/tokens_base.php');
+require_once($CFG->dirroot.'/question/type/correctwriting/sequence_analyzer/common.php');
 
-/**
- * Clones an array recursively. Used for lcs computing to clone array
- * @param object used object
- * @return copy of objects if it's array, otherwise same object.
- */
-function qtype_correctwriting_sequence_analyzer_array_clone($object) {
-    if (!is_array($object)) {
-        return $object;
-    }
-    $result=array();
-    foreach($object as $key => $value) {
-        $result[$key]=qtype_correctwriting_sequence_analyzer_array_clone($value);
-    }
-    return $result;
-}
-/**
- * Private implementation of is same function for tokens
-*/
-function qtype_correctwriting_sequence_analyzer_is_same($c1,$c2) {
-    return $c1->is_same($c2);   
-}
-/**
- *  Returns a length of sequence
- */
-function qtype_correctwriting_sequence_analyzer_count($sequence) {
-    return count($sequence);
-}   
+
 
 /**
  * Computes every lcs between answer and response
