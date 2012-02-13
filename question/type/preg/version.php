62aa2462de6f15e836e79ad50885320a68d8e94c
oasychev <oasychev@gmail.com>
2012-02-13 21:36:35 +0300

    Moodle 2.2 upgrade: ?????????? ???????? ? ???????? ???? ???????: * ??????? ?????? ????????? * ???????? ? version.php * ???????? ?????? ?????????? ?? ????? ??????

--- question/type/preg/version.php
+++ question/type/preg/version.php
@@ -1,9 +1,9 @@
 <?PHP // $Id: version.php,v 1.1.2.3 2009/10/04 19:49:58 oasychev Exp $
 
-$pluing->component = 'qtype_preg';
+$plugin->component = 'qtype_preg';
 $plugin->version  = 2012011300;
 $plugin->requires = 2011070100;
-$plugin->release = 'Preg 2.1';
+$plugin->release = 'Preg 2.2';
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->dependencies = array(
