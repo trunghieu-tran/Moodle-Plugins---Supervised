7cf09e73487a051e4b3e00f0ddfe7e27253c46a0
Arkanif <Arkanif@localhost>
2012-03-05 17:17:23 +0400

    ????????? ????????????? ??????? ? ????????? ?? cron

--- mod/poasassignment/additional/auditor_sync/auditor_sync.php
+++ mod/poasassignment/additional/auditor_sync/auditor_sync.php
@@ -1,6 +1,11 @@
 <?php
 require_once(dirname(dirname(dirname(__FILE__))).'/model.php');
+/**
+ * Плагин для синхронизации с заданиями Аудитора
+ */
 class auditor_sync {
+
+    // Singletone
     private static $instance = null;
     private function __construct() {
 
@@ -12,6 +17,14 @@ class auditor_sync {
         return self::$instance;
     }
 
+    /**
+     * Импортировать задание из Аудитора
+     *
+     * @access public
+     * @param $taskrecord данные о задании. Соответствуют записям таблицы poasassignment_tasks
+     * с двумя дополнительными полями - auditorvariantid и comments     *
+     * @return mixed идентификатор вставленной записи
+     */
     public function import_task($taskrecord) {
         global $DB;
 
@@ -27,11 +40,90 @@ class auditor_sync {
         return $auditorrecord->poasassignmenttaskid;
     }
 
+    /**
+     * Выполнить синхронизацию заданий (вызывается по cron)
+     *
+     * @access public
+     */
     public function synchronize() {
-        global $DB;
-        $auditortasks = $DB->get_records('auditor_sync_tasks');
-        foreach ($auditortasks as $auditortask) {
-            
+        // Подключить файл с конфигурацией соединения с внешней базой данных
+        require_once('config.php');
+        $error = $this->connect_auditor($config['server'], $config['dbuser'], $config['dbpass'], $config['database']);
+        if ($error == false) {
+            global $DB;
+            $storedtasks = $DB->get_records('auditor_sync_tasks');
+            $sql = 'SELECT * FROM variants';
+            if (!($result = mysql_query($sql))) {
+                echo get_string('errorcantrunquery', 'poasassignment');
+                echo ' '.$sql;
+            }
+            else{
+                // Для каждого задания из аудитора
+                while ($actualtask = mysql_fetch_assoc($result)) {
+                    // найти те задания poasassignment, которые были импортироаны из него
+                    $taskstosync = $this->get_stored_tasks_by_auditor_variant_id($actualtask['id'], $storedtasks);
+
+                    // и для всех таких заданий обновить комментарии и описание задачи
+                    foreach ($taskstosync as $tasktosync) {
+                        if ($tasktosync->comments != $actualtask['comments']) {
+                            $tasktosync->comments = $actualtask['comments'];
+                            //echo 'UPDATE RECORD auditor_sync_tasks ',print_r($tasktosync);
+                            $DB->update_record('auditor_sync_tasks', $tasktosync);
+                        }
+                        $poasassignmenttask = $DB->get_record('poasassignment_tasks', array('id' => $tasktosync->poasassignmenttaskid), 'id, description');
+                        if ($poasassignmenttask) {
+                            if ($poasassignmenttask->description != $actualtask['description']) {
+                                $poasassignmenttask->description = $actualtask['description'];
+                                //echo 'UPDATE RECORD poasassignment_tasks ',print_r($poasassignmenttask);
+                                $DB->update_record('poasassignment_tasks', $poasassignmenttask);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Получить все задания в Moodle, которые базируются на заданиях аудитора
+     *
+     * @access public
+     * @param $id идентификатор задания в Аудиторе
+     * @param $storedtasks все импортированные задания
+     * @return array задания poasassignment, базирующиеся на указанном задании Аудитора
+     */
+    private function get_stored_tasks_by_auditor_variant_id($id, $storedtasks) {
+        $tasks = array();
+        foreach ($storedtasks as $storedtask) {
+            if ($storedtask->auditorvariantid == $id) {
+                $tasks[$storedtask->id] = $storedtask;
+            }
+        }
+        return $tasks;
+    }
+
+    /**
+     * Подключение к серверу базы данных и базе данных
+     *
+     * @access public
+     * @param $server адрес сервера
+     * @param $user имя пользователя БД
+     * @param $password пароль пользователя БД
+     * @param $database имя БД
+     * @return bool|string текст ошибки или false, если ошибок подключения не возникло
+     */
+    public function connect_auditor($server, $user, $password, $database) {
+        if ($db = mysql_connect($server, $user, $password)) {
+            mysql_set_charset('UTF8');
+            if (!mysql_select_db($database, $db)) {
+                return get_string('errorcantconnecttodatabase', 'poasassignment');
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return get_string('errorcantconnecttoserver', 'poasassignment');
         }
     }
 }
--- mod/poasassignment/pages/tasksimport.php
+++ mod/poasassignment/pages/tasksimport.php
@@ -4,6 +4,8 @@ require_once('abstract_page.php');
 require_once(dirname(dirname(__FILE__)) . '/model.php');
 require_once($CFG->libdir . '/formslib.php');
 require_once($CFG->dirroot.'/lib/tablelib.php');
+
+require_once(dirname(dirname(__FILE__)).'/additional/auditor_sync/auditor_sync.php');
 class tasksimport_page extends abstract_page {
     var $poasassignment;
     var $mform;
@@ -45,100 +47,95 @@ class tasksimport_page extends abstract_page {
         $model = poasassignment_model::get_instance();
         if ($this->mform->get_data()) {
             $data = $this->mform->get_data();
-            if ($db = mysql_connect($data->server, $data->dbuser, $data->dbpass)) {
-                print_string('serverconnected', 'poasassignment');
-                echo '</br>';
-
-                mysql_set_charset('UTF8');
-                if (!mysql_select_db($data->database, $db)) {
-                    print_error('errorcantconnecttodatabase', 'poasassignment');
+            $error = auditor_sync::get_instance()->connect_auditor($data->server, $data->dbuser, $data->dbpass, $data->database);
+            if (($error == false)) {
+                $sql = "SELECT * FROM variants WHERE lessontypeid=".mysql_real_escape_string($data->lessontypeid);
+                if (!($result = mysql_query($sql))) {
+                    echo get_string('errorcantrunquery', 'poasassignment');
+                    echo $sql;
                 }
-                else {
-                    print_string('databaseconnected', 'poasassignment');
-                    echo '</br>';
-                    $sql = "SELECT * FROM variants WHERE lessontypeid=".mysql_real_escape_string($data->lessontypeid);
-                    if (!($result = mysql_query($sql))) {
+                else{
+                    // Узнать заранее идентификатор поля, в котором poasassignment хранит уровень сложности
+                    $complexityfield = $DB->get_record(
+                        'poasassignment_fields',
+                        array('poasassignmentid' =>$model->get_poasassignment()->id, 'name' => $data->kcfieldname),
+                        'id');
+
+                    // Узнать все варианты для поля уровня сложности
+                    $variants = $DB->get_records('poasassignment_variants', array('fieldid' => $complexityfield->id), 'sortorder');
+                    $levels = array();
+                    foreach ($variants as $id => $level) {
+                        array_push($levels, $level->value);
+                    }
+
+                    // Получить все уровни сложности из внешней базы данных
+                    $sql = "SELECT * FROM mod_complexities";
+                    if (!($complexities = mysql_query($sql))) {
                         echo get_string('errorcantrunquery', 'poasassignment');
                         echo $sql;
                     }
-                    else{
-                        // Узнать заранее идентификатор поля, в котором poasassignment хранит уровень сложности
-                        $complexityfield = $DB->get_record(
-                            'poasassignment_fields',
-                            array('poasassignmentid' =>$model->get_poasassignment()->id, 'name' => $data->kcfieldname),
-                            'id');
-
-                        // Узнать все варианты для поля уровня сложности
-                        $variants = $DB->get_records('poasassignment_variants', array('fieldid' => $complexityfield->id), 'sortorder');
-                        $levels = array();
-                        foreach ($variants as $id => $level) {
-                            array_push($levels, $level->value);
+                    else {
+                        $newcomplexities = array();
+                        while($complexity = mysql_fetch_assoc($complexities)) {
+                            array_push($newcomplexities, $complexity);
                         }
+                        $taskrecords = array();
+                        while ($variant = mysql_fetch_assoc($result)) {
 
-                        // Получить все уровни сложности из внешней базы данных
-                        $sql = "SELECT * FROM mod_complexities";
-                        if (!($complexities = mysql_query($sql))) {
-                            echo get_string('errorcantrunquery', 'poasassignment');
-                            echo $sql;
-                        }
-                        else {
-                            $newcomplexities = array();
-                            while($complexity = mysql_fetch_assoc($complexities)) {
-                                array_push($newcomplexities, $complexity);
+                            $taskrecord = new stdClass();
+                            $taskrecord->name = get_string('defaulttaskname', 'poasassignment') .' №' . $variant['num'];
+                            $taskrecord->description = $variant['description'];
+                            $taskrecord->comments = $variant['comments'];
+                            $taskrecord->id = $variant['id'];
+
+                            $kcfieldname = 'field' . $complexityfield->id;
+                            $taskrecord->$kcfieldname = array();
+                            $taskrecord->modifications = array();
+                            // Для каждого задания получить список модификаций
+                            $sql = "SELECT id, num, kc FROM modifications WHERE variantid=".mysql_real_escape_string($variant['id']);
+                            if (!($modifications = mysql_query($sql))) {
+                                echo get_string('errorcantrunquery', 'poasassignment');
+                                echo $sql;
                             }
-                            $taskrecords = array();
-                            while ($variant = mysql_fetch_assoc($result)) {
+                            else {
+                                while ($modification = mysql_fetch_assoc($modifications)) {
+                                    array_push(
+                                        $taskrecord->modifications,
+                                        array('id' => $modification['id'], 'num' => $modification['num'], 'kc' => $modification['kc'])
+                                    );
+                                    foreach ($newcomplexities as $complexity) {
+                                        if ($modification['kc'] >= $complexity['kcmin']
+                                            && $modification['kc'] <= $complexity['kcmax']) {
 
-                                $taskrecord = new stdClass();
-                                $taskrecord->name = get_string('defaulttaskname', 'poasassignment') .' №' . $variant['num'];
-                                $taskrecord->description = $variant['description'];
-
-                                $kcfieldname = 'field' . $complexityfield->id;
-                                $taskrecord->$kcfieldname = array();
-                                $taskrecord->modifications = array();
-                                // Для каждого задания получить список модификаций
-                                $sql = "SELECT id, num, kc FROM modifications WHERE variantid=".mysql_real_escape_string($variant['id']);
-                                if (!($modifications = mysql_query($sql))) {
-                                    echo get_string('errorcantrunquery', 'poasassignment');
-                                    echo $sql;
-                                }
-                                else {
-                                    while ($modification = mysql_fetch_assoc($modifications)) {
-                                        array_push(
-                                            $taskrecord->modifications,
-                                            array('id' => $modification['id'], 'num' => $modification['num'], 'kc' => $modification['kc'])
-                                        );
-                                        foreach ($newcomplexities as $complexity) {
-                                            if ($modification['kc'] >= $complexity['kcmin']
-                                                && $modification['kc'] <= $complexity['kcmax']) {
-
-                                                // Узнать id этого уровня сложности в таблице poasassignment_variants
-                                                if (array_search($complexity['name'], $levels) !== false) {
-                                                    if (array_search(array_search($complexity['name'], $levels), $taskrecord->$kcfieldname) === false) {
-                                                        array_push($taskrecord->$kcfieldname, array_search($complexity['name'], $levels));
-                                                    }
+                                            // Узнать id этого уровня сложности в таблице poasassignment_variants
+                                            // Иногда последний символ в таблице - №13. Учитывать это
+                                            if (($index = array_search($complexity['name'], $levels)) === false) {
+                                                $index = array_search($complexity['name'].chr(13), $levels);
+                                            }
+                                            if ( $index !== false) {
+                                                if (array_search($index, $taskrecord->$kcfieldname) === false) {
+                                                    array_push($taskrecord->$kcfieldname, $index);
                                                 }
                                             }
                                         }
                                     }
                                 }
-                                array_push($taskrecords, $taskrecord);
                             }
-                            //echo '<pre>',print_r($levels),'</pre>';
-                            //echo '<pre>',print_r($taskrecords),'</pre>';
-                            foreach ($taskrecords as $taskrecord) {
-                                $model->add_task($taskrecord);
-                                echo get_string('taskimported', 'poasassignment');
-                                echo " ($taskrecord->name, ".shorten_text($taskrecord->description).")";
-                                echo '<br/>';
-                            }
+                            array_push($taskrecords, $taskrecord);
                         }
-
+                        //echo '<pre>',print_r($levels),'</pre>';
+                        //echo '<pre>',print_r($taskrecords),'</pre>';
+                        foreach ($taskrecords as $taskrecord) {
+                            $taskrecord->id = auditor_sync::get_instance()->import_task($taskrecord);
+                            echo get_string('taskimported', 'poasassignment');
+                            echo " (id=$taskrecord->id, $taskrecord->name, ".shorten_text($taskrecord->description).")";
+                            echo '<br/>';
+                        }
                     }
                 }
             }
             else {
-                print_error('errorcantconnecttoserver', 'poasassignment');
+                echo $error;
             }
         }
         else {
@@ -151,16 +148,24 @@ class tasksimport_form extends moodleform {
         $mform = $this->_form;
         $instance = $this->_customdata;
 
+        // Подключить файл с конфигурацией соединения с внешней базой данных
+        require_once(dirname(dirname(__FILE__)).'/additional/auditor_sync/config.php');
+        //print_r($config);
+
         $mform->addElement('text','server',get_string('server','poasassignment'),array('size'=>45));
+        $mform->setDefault('server', $config['server']);
         $mform->addRule('server', null, 'required', null, 'client');
 
         $mform->addElement('text','database',get_string('database','poasassignment'),array('size'=>45));
+        $mform->setDefault('database', $config['database']);
         $mform->addRule('database', null, 'required', null, 'client');
 
         $mform->addElement('text','dbuser',get_string('dbuser','poasassignment'),array('size'=>45));
+        $mform->setDefault('dbuser', $config['dbuser']);
         $mform->addRule('dbuser', null, 'required', null, 'client');
 
         $mform->addElement('password','dbpass',get_string('dbpass','poasassignment'),array('size'=>45));
+        $mform->setDefault('dbpass', $config['dbpass']);
 
         $mform->addElement('text','lessontypeid','lessontypeid', array('size'=>45));
         $mform->addRule('lessontypeid', null, 'required', null, 'client');
