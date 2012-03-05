3387ceb5172064293ddb3d83bbb8cfec98a2c32e
Arkanif <Arkanif@localhost>
2012-03-05 17:17:05 +0400

    ????????? ????? ???? ??????, ??????? ??? ?????? ???????.

---
+++ mod/poasassignment/additional/auditor_sync/version.php
@@ -0,0 +1,4 @@
+<?php
+
+$plugin->version  = 2012030501; // Today's date in YYYYMMDD format, plus two additional digits
+$plugin->requires = 2011032300; // Moodle version - copy from $version in the top-level version.php file.
