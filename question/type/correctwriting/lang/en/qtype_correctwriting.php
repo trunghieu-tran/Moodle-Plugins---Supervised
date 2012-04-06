bd35dad45d07da80d8ea5c84cfe9f8abd4de56d8
???????????? <????????????@YOUR-8DNSIVLYA0.vmks.ru>
2012-04-06 21:40:20 +0400

    ?????? ?????????/???????? ???? ??????? ???????? ?????????.

--- question/type/correctwriting/lang/en/qtype_correctwriting.php
+++ question/type/correctwriting/lang/en/qtype_correctwriting.php
@@ -4,6 +4,7 @@
 
 $string['correctwriting'] = 'Correct writing';
 $string['pluginname'] = 'Correct writing';
+$string['pluginname_link'] = 'question/type/correctwriting';
 $string['movedmistakemessage'] = 'A lexeme "{$a->type}" is moved from {$a->answerline}:{$a->answerposition} to {$a->responseline}:{$a->responseposition}';
 $string['addedmistakemessage'] = 'A lexeme "{$a->type}" at {$a->line}:{$a->position} is odd in response';
 $string['absentmistakemessage'] = 'A lexeme "{$a}" is skipped';
