a452c9ad881537693687c69cce5a105e6c6d8763
mamontov dp <mamontov.dp@gmail.com>
2012-04-16 12:28:47 +0400

    ??????? ?????? pluginnamesummary ? ?????? questiontype.

--- question/type/correctwriting/lang/en/qtype_correctwriting.php
+++ question/type/correctwriting/lang/en/qtype_correctwriting.php
@@ -5,6 +5,7 @@
 $string['correctwriting'] = 'Correct writing';
 $string['pluginname'] = 'Correct writing';
 $string['pluginname_link'] = 'question/type/correctwriting';
+$string['pluginnamesummary'] = 'Question type that can automatically find errors int student response and grade it by penalty.';
 $string['movedmistakemessage'] = 'A lexeme "{$a->type}" is moved from {$a->answerline}:{$a->answerposition} to {$a->responseline}:{$a->responseposition}';
 $string['addedmistakemessage'] = 'A lexeme "{$a->type}" at {$a->line}:{$a->position} is odd in response';
 $string['absentmistakemessage'] = 'A lexeme "{$a}" is skipped';
