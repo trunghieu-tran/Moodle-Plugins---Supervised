4b412c90cf5ad39d6913039e4ef725afe15ee7bd
mamontov dp <mamontov.dp@gmail.com>
2012-04-20 21:07:03 +0400

    ?????????? ???????????? ? ????? (???????????? ????? ??????? ?? shortanswer) ??????????? get_per_answer_fields

--- question/type/correctwriting/lang/en/qtype_correctwriting.php
+++ question/type/correctwriting/lang/en/qtype_correctwriting.php
@@ -3,9 +3,6 @@
 //Lang string file for the correct writing question type
 
 $string['correctwriting'] = 'Correct writing';
-$string['pluginname'] = 'Correct writing';
-$string['pluginname_link'] = 'question/type/correctwriting';
-$string['pluginnamesummary'] = 'Question type that can automatically find errors int student response and grade it by penalty.';
 $string['movedmistakemessage'] = 'A lexeme "{$a->type}" is moved from {$a->answerline}:{$a->answerposition} to {$a->responseline}:{$a->responseposition}';
 $string['addedmistakemessage'] = 'A lexeme "{$a->type}" at {$a->line}:{$a->position} is odd in response';
 $string['absentmistakemessage'] = 'A lexeme "{$a}" is skipped';
@@ -13,5 +10,15 @@ $string['pleaseenterananswer'] = 'Please enter an answer.';
 $string['caseyes'] = 'Yes, case is important';
 $string['caseno'] = 'No, case is unimportant';
 $string['casesensitive'] = 'Case sensitivity';
-
-?>
+$string['lexicalerrorthreshold'] = 'Lexical error threshold (as fraction to length of word)';
+$string['lexicalerrorweight'] = 'Penalty for lexical error';
+$string['absentmistakeweight'] = 'Penalty for absent lexeme in student response';
+$string['addedmistakeweight'] = 'Penalty for odd lexeme in student response';
+$string['movedmistakeweight'] = 'Penalty for moving lexeme in student response';
+$string['lexemedescriptions'] = 'Descriptions for lexemes';
+$string['pluginname'] = 'Correct writing';
+$string['pluginname_help'] = 'Test';
+$string['pluginname_link'] = 'question/type/correctwriting';
+$string['pluginnameadding'] = 'Adding a correct writing  question';
+$string['pluginnameediting'] = 'Editing a correct writing  question';
+$string['pluginnamesummary'] = 'Question type that can automatically find errors int student response and grade it by penalty.';
