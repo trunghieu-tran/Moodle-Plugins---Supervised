1ecd534b7e7b483badf816bb6a5f9add0948620a
Arkanif <Arkanif@localhost>
2012-04-24 23:30:47 +0400

    ????????? ?????????????? ????? ????????? ?????????.

---
+++ mod/poasassignment/taskgivers/analogy_choice/lang/ru/poasassignmenttaskgivers_analogy_choice.php
@@ -0,0 +1,12 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+$string['pluginname'] = 'Выбор по аналогии';
+$string['parameterchoice'] = 'Выбор по аналогии';
+$string['chosebaseinstance'] = 'Выберите базовый экземпляр модуля poasassignment';
+$string['originalinstance'] = 'Базовый экземпляр модуля poasassignment';
+$string['originalinstance_help'] = 'Студент получает то задание, которое имеет одинаковое название с его заданием в базовом экземпляре. Если в базомом экземпляре у студента не было задания, он не может получить задание.';
+$string['originalinstancesaved'] = 'Базовое задание сохранено';
+$string['originalinstancedeleted'] = 'Базовое задание удалено';
+$string['errornobasetasktaken'] = 'У вас нет базового задания';
+$string['errornobaseinstanceselected'] = 'Преподаватель не выбрал базовый экземпляр модуля poasassignment.';
+$string['errornotaskwithsamename'] = 'В этом модуле нет заданий с таким же именем, которое было у вашего старого задания. Сообщите преподавателю.';
