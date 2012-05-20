b74623ae4163a1a811cf344ffdd573fe244090c3
mamontov dp <mamontov.dp@gmail.com>
2012-05-20 14:45:33 +0400

    ???????? ?????? ?????????, ?????? ??? ???????.

--- blocks/formal_langs/db/install.php
+++ blocks/formal_langs/db/install.php
@@ -6,11 +6,13 @@ function xmldb_block_formal_langs_install() {
     $lang = new stdClass();
     $lang->ui_name = 'Simple english';
     $lang->description = 'Simple english language definition';
-    $lang->name ='simpeng';
+    $lang->name = 'simpeng';
+    $lang->scanrules = null;
+    $lang->parserules = null;
     $lang->version='1.0';
     $lang->visible = 1;
     
-    $DB->insert_record('block_formal_langs',$object);
+    $DB->insert_record('block_formal_langs',$lang);
 
 
 }
