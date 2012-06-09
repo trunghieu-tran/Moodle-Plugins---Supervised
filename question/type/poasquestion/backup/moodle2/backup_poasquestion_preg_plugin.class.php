900d87ce5ce8d78a76ae7f0b3953701332807023
vostreltsov <vostreltsov@gmail.com>
2012-06-09 13:13:26 +0300

    ???????? ??????????? ?????? poasquestion, preg ?????? ????????? ?? poasquestion. ???? ??????????? ?????? ???????? ??? backup/restore. ? ??????????? backup ?????????????? ??????? ????????? ?? ?????, ? restore ????? ???????? ??????? process_<name>, ?????????? ???????????? ??????? (??. ?????????? restore ? preg).

--- question/type/poasquestion/backup/moodle2/backup_poasquestion_preg_plugin.class.php
+++ question/type/poasquestion/backup/moodle2/backup_poasquestion_preg_plugin.class.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->dirroot . '/question/type/poasquestion/backup/moodle2/backup_poasquestion_preg_plugin.class.php');
+require_once($CFG->dirroot . '/question/engine/bank.php');
 
-class backup_qtype_preg_plugin extends backup_qtype_poasquestion_plugin {
+class backup_qtype_poasquestion_plugin extends backup_qtype_plugin {
 
     /**
      * Returns the qtype information to attach to question element.
