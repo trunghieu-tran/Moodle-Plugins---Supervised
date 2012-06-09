f0a2fb07b36f29ef66c0baccd90c23558435d419
vostreltsov <vostreltsov@gmail.com>
2012-06-09 18:33:02 +0300

    ???????? ????? ?????? ? ????? ??? ???.

---
+++ question/type/preg/simpletest/test_preg_string.php
@@ -0,0 +1,55 @@
+﻿<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/question/type/preg/preg_string.php');
+
+/**
+ * Unit tests for preg string class.
+ *
+ * @copyright  2012 Valeriy Streltsov
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_preg_string_test extends UnitTestCase {
+
+    function test_string() {
+        $str1 = new qtype_preg_string('аzб');
+        $str2 = new qtype_preg_string('йц者');
+        $str3 = new qtype_preg_string($str1 . $str2);
+
+        $this->assertTrue(is_a($str3, 'qtype_preg_string'));
+        $this->assertTrue($str3->string() === 'аzбйц者');
+        $this->assertTrue($str3->length() === 6);
+        $this->assertTrue($str3[-1] === null);
+        $this->assertTrue($str3[0] === 'а');
+        $this->assertTrue($str3[1] === 'z');
+        $this->assertTrue($str3[2] === 'б');
+        $this->assertTrue($str3[3] === 'й');
+        $this->assertTrue($str3[4] === 'ц');
+        $this->assertTrue($str3[5] === '者');
+        $this->assertTrue($str3[6] === null);
+
+        $str3[-1] = 'Q';
+        $str3[0] = 'W';
+        $str3[1] = 'E';
+        $str3[4] = '者';
+        $str3[6] = 'ه';
+        $str3->concatenate('ab');
+        $str3->concatenate(new qtype_preg_string('ёя'));
+
+        $this->assertTrue($str3->length() === 11);
+        $this->assertTrue($str3[-1] === null);
+        $this->assertTrue($str3[0] === 'W');
+        $this->assertTrue($str3[1] === 'E');
+        $this->assertTrue($str3[2] === 'б');
+        $this->assertTrue($str3[3] === 'й');
+        $this->assertTrue($str3[4] === '者');
+        $this->assertTrue($str3[5] === '者');
+        $this->assertTrue($str3[6] === 'ه');
+        $this->assertTrue($str3[7] === 'a');
+        $this->assertTrue($str3[8] === 'b');
+        $this->assertTrue($str3[9] === 'ё');
+        $this->assertTrue($str3[10] === 'я');
+        $this->assertTrue($str3[11] === null);
+    }
+}
