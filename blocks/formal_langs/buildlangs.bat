60599970f8e3e72e69f0ce486c24fb9ce834b81a
???????????? <????????????@VERONIKA.vmks.ru>
2012-06-10 17:15:28 +0400

    ??????? ??????? ???????? ????? C. ??????, ????? ??? ???????? ????????? ??????????? ??????.

--- blocks/formal_langs/buildlangs.bat
+++ blocks/formal_langs/buildlangs.bat
@@ -4,3 +4,8 @@ cd langs_src
 ren  simple_english.lex.php language_simple_english.php
 move /y language_simple_english.php ..\language_simple_english.php
 cd ..
+java JLexPHP/Main langs_src/c_language.lex
+cd langs_src
+ren  c_language.lex.php language_c_language.php
+move /y language_c_language.php ..\language_c_language.php
+cd ..
