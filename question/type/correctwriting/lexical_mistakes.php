e6b65249faab7e02a4774d2705bb04d169924516
mamontov dp <mamontov.dp@gmail.com>
2012-06-10 18:21:03 +0400

    ??????? ???????? ?????????? ??????????? ??????, ??????????? ??? ??????????? ????, ????? ??????? ???? ?????????? ? ?????? ????????? ???????????.

---
+++ question/type/correctwriting/lexical_mistakes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Defines an implementation of mistakes, that are determined by lexical analyzer
+ *
+ * @copyright &copy; 2011  Oleg Sychev
+ * @author  Oleg Sychev, Dmitriy Mamontov,Birukova Maria Volgograd State Technical University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package questions
+ */
+ 
+defined('MOODLE_INTERNAL') || die();
+ 
+require_once($CFG->dirroot.'/question/type/correctwriting/response_mistakes.php');
+
+// A marker class to indicate errors from lexical analyzer. We need them to indicate
+// what lexemes was corrected by analyzer.
+class qtype_correctwriting_lexical_mistake extends qtype_correctwriting_response_mistake {
+    // An array of corrected response lexemes
+    public $correctedresponse;
+    // An arraty of fixed lexemes indexes
+    public $correctedresponseindex;
+}
+
+
+
+?>
