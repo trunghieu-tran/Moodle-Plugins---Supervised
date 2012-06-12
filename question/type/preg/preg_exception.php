b8611d67f58db0bf4359b40bcce8a7525d893fe6
vostreltsov <vostreltsov@gmail.com>
2012-06-12 09:52:31 +0300

    ? ?????? ????????????? ???????????? ? qtype_preg_toolargefa_exception ?????? 2 ?????? ?????????. ?????????? ??????????? - $a, ???????? ???????? ??????, ???????? ????????,  ? ?? ???????.

--- question/type/preg/preg_exception.php
+++ question/type/preg/preg_exception.php
@@ -10,33 +10,30 @@
 
 
 /**
- * Preg questiontype exception class
+ * Preg questiontype exception class.
  */
 class qtype_preg_exception extends moodle_exception {
     /**
      * @param string $errorcode
-     * @param string $a
+     * @param object $a
      * @param string $debuginfo
      */
-    function __construct($errorcode, $a=NULL, $debuginfo=null) {
+    function __construct($errorcode, $a = NULL, $debuginfo = null) {
         parent::__construct($errorcode, '', '', $a, $debuginfo);
     }
 }
 
 /**
-* A special class, throwed by finite automaton, Should be catched to generate qtype_preg_too_complex_error object by the code, building automaton
-* No actual info needed since it would be filled by catching code
-*/
+ * A special class, throwed by finite automaton, Should be catched to generate qtype_preg_too_complex_error object by the code, building automaton
+ * No actual info needed since it would be filled by catching code.
+ */
 class qtype_preg_toolargefa_exception extends qtype_preg_exception {
     /**
      * @param string $errorcode
-     * @param string $a
+     * @param object $a
      * @param string $debuginfo
      */
-    function __construct($errorcode, $a=NULL, $debuginfo=null) {
-        parent::__construct($errorcode, '', '', $a, $debuginfo);
+    function __construct($errorcode, $a = NULL, $debuginfo = null) {
+        parent::__construct($errorcode, $a, $debuginfo);
     }
 }
-
-
-?>
