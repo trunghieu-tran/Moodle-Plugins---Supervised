5e42ff4f5a6f21974afbedd614f4708851c6ab37
??????? ??????? <topt.iiiii@gmail.com>
2012-07-17 19:22:52 +0400

    ???????? ???? ??? ????????? ????????????? ???????????????? ??????

--- question/type/preg/tests/description_test.php
+++ question/type/preg/tests/description_test.php
@@ -12,9 +12,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
-require_once($CFG->dirroot . '/question/type/preg/preg_description.php');
+require_once($CFG->dirroot . '/question/type/preg/preg_regex_handler.php');
 
 class qtype_preg_description_test extends PHPUnit_Framework_TestCase {
     function test_first() {
+        $rh = new qtype_preg_regex_handler('(?=.*\d)(?=.*[a-z])(?=.*[A-Z@#$%])(?=.*[@#$%]).{6,20}');
+        echo var_dump($rh);
     }
 }
