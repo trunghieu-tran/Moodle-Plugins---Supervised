35a7ebf9f7405fa5413b79d0d098803ece7698a8
??????? ??????? <topt.iiiii@gmail.com>
2012-07-18 12:54:32 +0400

    test_first() ??????? ??? ???????? ????????? ??????????? ??????

--- question/type/preg/tests/description_test.php
+++ question/type/preg/tests/description_test.php
@@ -16,7 +16,7 @@ require_once($CFG->dirroot . '/question/type/preg/preg_regex_handler.php');
 
 class qtype_preg_description_test extends PHPUnit_Framework_TestCase {
     function test_first() {
-        $rh = new qtype_preg_regex_handler('(?=.*\d)(?=.*[a-z])(?=.*[A-Z@#$%])(?=.*[@#$%]).{6,20}');
+        $rh = new qtype_preg_regex_handler('[^[^:word:]abc0-9]');
         echo var_dump($rh);
     }
 }
