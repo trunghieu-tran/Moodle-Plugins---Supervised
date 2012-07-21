66b7f0584626f9986ecfdfccf77c7b760e711749
??????? ??????? <topt.iiiii@gmail.com>
2012-07-21 14:29:39 +0400

    ????????? ????? ??? ???????

--- question/type/preg/tests/description_test.php
+++ question/type/preg/tests/description_test.php
@@ -12,11 +12,26 @@
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
-require_once($CFG->dirroot . '/question/type/preg/preg_regex_handler.php');
+require_once($CFG->dirroot . '/question/type/preg/preg_description.php');
 
 class qtype_preg_description_test extends PHPUnit_Framework_TestCase {
-    function test_first() {
-        $rh = new qtype_preg_regex_handler('[^[^:word:]abc0-9]');
-        echo var_dump($rh);
+    
+    /**
+     * @dataProvider charset_provider
+     */
+    public function test_charset($regex, $expected)
+    {
+        $handler = new qtype_preg_author_tool_description($regex,null,null);
+        $result = (string)($handler->description('<span class="description_node_%n%o">%s</span>',' operand','%s'));
+        $this->assertEquals($result, $expected);
+    }
+ 
+    public function charset_provider()
+    {
+        return array(
+          array('[^[^:word:]abc\pL]','any symbol except the following: not <span style="color:red">\w AND [:word:]</span>, <span style="color:red">Letter</span>, <span style="color:red">a</span>, <span style="color:red">b</span>, <span style="color:red">c</span>;'),
+          array('a','<span style="color:red">a</span>'),
+          array('[^a]','not <span style="color:red">a</span>'),
+        );
     }
 }
