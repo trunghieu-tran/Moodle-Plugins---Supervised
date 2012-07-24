31132aad8f6591ac5bc839324facb0dc75242556
mamontov dp <mamontov.dp@gmail.com>
2012-07-24 21:26:41 +0400

    ???????? ?????  ?????? ? ?? ??????

--- question/type/correctwriting/runtests.bat
+++ question/type/correctwriting/runtests.bat
@@ -1,2 +1,2 @@
-phpunit qtype_correctwriting_sequence_analyzer_test question/type/correctwriting/tests/test_sequence_analyzer.php
-phpunit qtype_correctwriting_sequence_analyzer_test question/type/correctwriting/tests/test_grade_response.php
+call phpunit qtype_correctwriting_sequence_analyzer_test question/type/correctwriting/tests/test_sequence_analyzer.php
+call phpunit qtype_correctwriting_sequence_analyzer_test question/type/correctwriting/tests/test_grade_response.php
--- question/type/correctwriting/tests/test_grade_response.php
+++ question/type/correctwriting/tests/test_grade_response.php
@@ -39,59 +39,59 @@ require_once($CFG->dirroot.'/blocks/formal_langs/language_simple_english.php');
     }
     // Tests perfect match
     public function test_perfect_match() {
+       $question = clone $this->question;
        $answers = array((object)array('id' => 1, 'answer' => 'a data template', 'fraction' => 1.0));
-       $this->question->answers = $answers;
-       $state = $this->question->grade_response(array('answer' => 'a data template'));
+       $question->answers = $answers;
+       $state = $question->grade_response(array('answer' => 'a data template'));
        $this->assertEquals($state[0],1.0);
-       $this->question->invalidate_cache();
     }
     // Test when exact match with lower mark is preferred over a bigger non-exact match
     public function test_exact_over_non_exact_match() {
+        $question = clone $this->question;
         $answers = array();
         $answers[] = (object)array('id' => 1, 'answer' => 'a data template', 'fraction' => 1.0);
         $answers[] = (object)array('id' => 2, 'answer' => 'a data', 'fraction' => 0.4);
-        $this->question->answers = $answers;
-        $state = $this->question->grade_response(array('answer' => 'a data'));
+        $question->answers = $answers;
+        $state = $question->grade_response(array('answer' => 'a data'));
         $this->assertEquals($state[0],0.4);
-        $this->question->invalidate_cache();
     }
     //  Test fully incorrect response
     public function test_incorrect_response() {
+       $question = clone $this->question;
        $answers = array((object)array('id' => 1, 'answer' => 'a data template', 'fraction' => 1.0));
-       $this->question->answers = $answers;
-       $state = $this->question->grade_response(array('answer' => 'test'));
+       $question->answers = $answers;
+       $state = $question->grade_response(array('answer' => 'test'));
        $this->assertEquals($state[0],0.0);
-       $this->question->invalidate_cache();
     }
     // Test when non-exact match with higher mark is preferred over exact match
     public function test_non_exact_over_exact_match() {
+        $question = clone $this->question;
         $answers = array();
         $answers[] = (object)array('id' => 1, 'answer' => 'a data template', 'fraction' => 1.0);
         $answers[] = (object)array('id' => 2, 'answer' => 'a data', 'fraction' => 0.4);
-        $this->question->answers = $answers;
-        $state = $this->question->grade_response(array('answer' => 'a data test'));
+        $question->answers = $answers;
+        $state = $question->grade_response(array('answer' => 'a data test'));
         $this->assertEquals($state[0],0.77);
-        $this->question->invalidate_cache();
     }
     // Test when one exact match is preferred over another
     public function test_two_exact_matches() {
+        $question = clone $this->question;
         $answers = array();
         $answers[] = (object)array('id' => 1, 'answer' => 'a data', 'fraction' => 0.3);
         $answers[] = (object)array('id' => 2, 'answer' => 'a data', 'fraction' => 0.4);
-        $this->question->answers = $answers;
-        $state = $this->question->grade_response(array('answer' => 'a data'));
+        $question->answers = $answers;
+        $state = $question->grade_response(array('answer' => 'a data'));
         $this->assertEquals($state[0],0.4);
-        $this->question->invalidate_cache();
     }
     // Test when one non-exact match is preferred over another
     public function test_two_non_exact_matches() {
+        $question = clone $this->question;
         $answers = array();
         $answers[] = (object)array('id' => 1, 'answer' => 'a data template', 'fraction' => 0.78);
         $answers[] = (object)array('id' => 2, 'answer' => 'a data a a', 'fraction' => 1.0);
-        $this->question->answers = $answers;
-        $state = $this->question->grade_response(array('answer' => 'a data'));
+        $question->answers = $answers;
+        $state = $question->grade_response(array('answer' => 'a data'));
         $this->assertEquals($state[0],0.78);
-        $this->question->invalidate_cache();
     }
  }
  
