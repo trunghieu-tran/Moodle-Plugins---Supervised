9473bb1dd74901f07f29a8d236c42766893d1a5b
vostreltsov <vostreltsov@gmail.com>
2012-07-29 11:08:59 +0400

    ?????????? blacklist ??? ????? ? ?????-???????, ?????? ??????????????? ????? ????.

--- question/type/preg/tests/matcher_nfa_test.php
+++ question/type/preg/tests/matcher_nfa_test.php
@@ -10,4 +10,8 @@ class qtype_preg_nfa_cross_tester extends qtype_preg_cross_tester {
     public function engine_name() {
         return 'nfa_matcher';
     }
+
+    protected function blacklist_tags() {
+        return array(self::TAG_ASSOC_LEFT);
+    }
 }
