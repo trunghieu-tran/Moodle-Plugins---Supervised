962cfa88fb8a9c23c788867b42a2deb406146322
vostreltsov <vostreltsov@gmail.com>
2012-07-29 16:22:24 +0400

    ??????? ? ???????????????? ?????? " ?? ' ??? ????????, ???????????????? ? ???????? ??????? ? \n.

---
+++ question/type/preg/authors_tool/preg_authors_tool_load.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * Create interactive tree, explain graph and description.
+ * 
+ * @copyright &copy; 2012  Terechov Grigory
+ * @author Terechov Grigory, Volgograd State Technical University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package questions
+ */
+ 
+//defined('MOODLE_INTERNAL') || die();
+
+require_once(dirname(__FILE__) . '/../../../../config.php');
+
+global $CFG;
+//require_once($CFG->libdir.'/formslib.php');
+require_once($CFG->dirroot.'/question/type/preg/authors_tool/explain_graph_tool.php');
+//require_once($CFG->dirroot.'/question/type/preg/question.php');
+//require_once($CFG->dirroot.'/question/type/preg/preg_hints.php');
+require_once($CFG->dirroot.'/question/type/preg/preg_regex_handler.php');
+require_once($CFG->dirroot.'/question/type/preg/preg_dotstyleprovider.php');
+require_once($CFG->dirroot.'/question/type/preg/authors_tool/preg_description.php');
+
+class preg_authors_tool_load {
+    
+    /**
+     * Generate image and map for interative tree
+     * 
+     * @param array $json_array contains link on image and text map of interactive tree
+     */
+    public static function load_tree(&$json_array){
+        
+        global $CFG;
+        $regextext = optional_param('regex', '', PARAM_TEXT);
+        $id = optional_param('id', '', PARAM_INT);
+
+        if(!empty($regextext)) {
+            
+            $regexhandler = new qtype_preg_regex_handler($regextext);
+            
+            //Checking parser errors
+            $pars_error = false;
+            foreach($regexhandler->get_errors() as $error) {
+                if (is_a($error, 'qtype_preg_parsing_error')) {
+                    $pars_error = true;
+                    break;
+                }
+            }
+
+            if($pars_error === false && $regexhandler->get_ast_root() !== NULL) {
+
+                qtype_preg_regex_handler::execute_dot($regexhandler->get_ast_root()->dot_script(new qtype_preg_dot_style_provider()), $CFG->dirroot . '/question/type/preg/tmp_img/tree.png');//Generate image
+                qtype_preg_regex_handler::execute_dot($regexhandler->get_ast_root()->dot_script(new qtype_preg_dot_style_provider()), $CFG->dirroot . '/question/type/preg/tmp_img/tree.cmapx');//Generate map
+                
+                $tree_map ='';//tag <map>                 
+                $tree_handle = fopen($CFG->dirroot . '/question/type/preg/tmp_img/tree.cmapx', 'r');//Open and read tag <map> from file
+                
+                if($tree_handle){//If tree.cmapx is open
+                    while (!feof($tree_handle)) {
+                        $tree_map .= fgets($tree_handle);
+                    }
+                    fclose($tree_handle);
+                } else {
+                    $tree_map = 'Error read map file from disk!';
+                }
+
+                $json_array['tree_src'] = $CFG->wwwroot  . '/question/type/preg/tmp_img/tree.png';//Add tree
+                $json_array['map'] = $tree_map;//Add map
+                
+            } else {                
+                $json_array['tree_src'] = $CFG->wwwroot  . '/question/type/preg/tmp_img/tree_err.png';
+            }
+        } else {
+            $json_array['tree_src'] = $CFG->wwwroot  . '/question/type/preg/tmp_img/tree_dif.png';//Add tree
+        }
+    }
+    
+    /**
+     * Generate image for explain graph
+     * 
+     * @param array $json_array contains link on image of explain graph
+     */
+    public static function load_graph(&$json_array){
+        
+        global $CFG;
+        $regextext = optional_param('regex', '', PARAM_TEXT);
+        $id = optional_param('id', '', PARAM_INT);
+
+        if(!empty($regextext)) {            
+            //Generate graph image
+            $tmp_graph = new qtype_preg_author_tool_explain_graph($regextext);
+            
+            //Checking parser errors
+            $pars_error = false;
+            foreach($tmp_graph->get_errors() as $error) {
+                if (is_a($error, 'qtype_preg_parsing_error')) {
+                    $pars_error = true;
+                    break;
+                }
+            }
+            
+            if($pars_error === false && $tmp_graph->get_ast_root() !== NULL) {
+                
+                $graph = $tmp_graph->create_graph($id);
+                $dot_instructions_graph = $graph->create_dot();
+                
+                qtype_preg_regex_handler::execute_dot($dot_instructions_graph, $CFG->dirroot . '/question/type/preg/tmp_img/graph.png');//Generate image      
+
+                $json_array['graph_src'] = $CFG->wwwroot  . '/question/type/preg/tmp_img/graph.png';//Add graph
+                
+            } else {                
+                $json_array['graph_src'] = $CFG->wwwroot  . '/question/type/preg/tmp_img/graph_err.png';
+            }
+        } else {
+            $json_array['graph_src'] = $CFG->wwwroot  . '/question/type/preg/tmp_img/graph_dif.png';//Add graph
+        }
+    }
+    
+    /**
+     * Generate description
+     * 
+     * @param array $json_array contains text of description
+     */
+    public static function load_description(&$json_array){
+        
+        global $CFG;
+        $regextext = optional_param('regex', '', PARAM_TEXT);
+        $id = optional_param('id', '', PARAM_INT);
+
+        if(!empty($regextext)) {
+            if($id == -1){
+                $description = new qtype_preg_author_tool_description($regextext);
+                
+                //Checking parser errors
+                $pars_error = false;
+                foreach($description->get_errors() as $error) {
+                    if (is_a($error, 'qtype_preg_parsing_error')) {
+                        $pars_error = true;
+                        break;
+                    }
+                }
+                
+                if($pars_error === false && $description->get_ast_root() !== NULL) {
+                    $json_array['description'] = $description->default_description();//Add tree                
+                } else {                
+                    $json_array['description'] = 'Ooops! I can\'t build description!';
+                }
+            }
+        } else {
+            $json_array['description'] = 'Ooops! I can\'t build description!';
+        }
+    }
+    
+    /**
+     * Generate array of links on image of interactive tree, explain graph, text map for interactive tree and text of description
+     * 
+     * @param array $json_array contains author tool content
+     */
+    public static function get_json_array(){
+        
+        $json_array = array();
+        
+        /*$regextext = optional_param('regex', '', PARAM_TEXT);
+        $json_array['regex'] = $regextext;*/
+        
+        self::load_tree($json_array);
+        self::load_graph($json_array);
+        self::load_description($json_array);
+        
+        return $json_array;
+    
+    }
+}
+
+$json_array = preg_authors_tool_load::get_json_array();
+echo json_encode($json_array);
---
+++ question/type/preg/preg_dotstyleprovider.php
@@ -0,0 +1,204 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/question/type/poasquestion/poasquestion_string.php');
+require_once($CFG->dirroot . '/question/type/preg/preg_errors.php');
+
+/**
+ * Provides styles for node drawing using dot.
+ */
+class qtype_preg_dot_style_provider {
+
+    /**
+     * Returns the appropriate string for the given node.
+     * @param $pregnode an abstract syntax tree node.
+     * @return string which can be used as a dot instruction to set style, label etc, for example '[style - dotted, label = \"some label here\"]'.
+     */
+    public function get_style($pregnode) {
+        // Form a label from the user inscription which can be an array of strings.
+        if (is_array($pregnode->userinscription)) {
+            $label = '';
+            foreach ($pregnode->userinscription as $tmp) {
+                $label .= addslashes($tmp);
+            }
+        } else {
+            $label = $pregnode->userinscription;
+        }
+        $id = $pregnode->id;
+
+        // Now the label is ready, just return the appropriate style for node type and subtype.
+        switch ($pregnode->type) {
+            case qtype_preg_node::TYPE_ABSTRACT: {
+                return "[label = \"abstract node\", style = dotted]";  // это пример, замени его потом на пустую строку.
+            }
+            case qtype_preg_node::TYPE_LEAF_CHARSET: {
+                if ($pregnode->negative) {
+                    $label = '[^' . $label . ']';
+                } else if (qtype_poasquestion_string::strlen($label) > 1) {
+                    $label = '[' . $label . ']';
+                }
+                return "[label = \"$label\", tooltip = \"character class\", shape = rectangle, id = $id]";
+            }
+            case qtype_preg_node::TYPE_LEAF_META: {
+                //if($pregnode->subtype === qtype_preg_leaf_meta::SUBTYPE_EMPTY) {
+                return "[label = \"emptiness\", tooltip = emptiness, shape = rectangle, id = $id]";
+            }
+            case qtype_preg_node::TYPE_LEAF_ASSERT: {
+                return "[label = \"assertion $label\", tooltip = assertion, shape = rectangle, id = $id]";
+            }
+            case qtype_preg_node::TYPE_LEAF_BACKREF: {
+                return "[label = \"backreference to ' . $pregnode->number . ' subpattern\", tooltip = backreference, shape = rectangle, id = $id]";
+            }
+            case qtype_preg_node::TYPE_LEAF_RECURSION: {
+                return "[label = \"recursion ' . $pregnode->number . '\", tooltip = recursion, shape = rectangle, id = $id]";
+            }
+            case qtype_preg_node::TYPE_LEAF_CONTROL: {
+                return "[label = control sequence \"$label\", tooltip = \"control sequence\", shape = rectangle, id = $id]";
+            }
+            case qtype_preg_node::TYPE_LEAF_OPTIONS: {
+                return "[label = \"$label\", tooltip = option, shape = rectangle, id = $id]";
+            }
+            case qtype_preg_node::TYPE_NODE_FINITE_QUANT: {
+                return "[label = \"$label\", tooltip = \"finite quantifier\", id = $id]";
+            }
+            case qtype_preg_node::TYPE_NODE_INFINITE_QUANT: {
+                return "[label = \"$label\", tooltip = \"infinite quantifier\", id = $id]";
+            }
+            case qtype_preg_node::TYPE_NODE_CONCAT: {
+                return "[label = \"concat\", tooltip = concatenation, id = $id]";
+            }
+            case qtype_preg_node::TYPE_NODE_ALT: {
+                return "[label = \"$label\", tooltip = alternative, id = $id]";
+            }
+            case qtype_preg_node::TYPE_NODE_ASSERT: {
+                return "[label = \"assertion $label\", tooltip = assertion, id = $id]";
+            }
+            case qtype_preg_node::TYPE_NODE_SUBPATT: {
+                return "[label = \"$label\", tooltip = subpattern, id = $id]";
+            }
+            case qtype_preg_node::TYPE_NODE_COND_SUBPATT: {
+                return "[label = \"$label\", tooltip = \"conditional subpattern\", id = $id]";
+            }
+            case qtype_preg_node::TYPE_NODE_ERROR: {
+
+                return "[label = \"ERROR $label\", tooltip = error, id = $id]";
+            }
+            default: {
+                return "[label = \"Unknown node subtype\", style = dotted]";
+            }
+        }
+    }
+
+    /**
+     * Returns heading of a dot script which is usually looks like "digraph {".
+     * @param string $rankdirlr if true, also adds "rankdir = LR".
+     * @return string the heading of a dot script.
+     */
+    public function get_dot_head($rankdirlr = true) {
+        $result = 'digraph {';
+        if ($rankdirlr) {
+            $result .= 'rankdir = LR;';
+        }
+        return $result;
+    }
+
+    /**
+     * Returns tail of a dot script which is usually looks like "}".
+     * @return string the tail of a dot script.
+     */
+    public function get_dot_tail() {
+        return '}';
+    }
+
+    /**
+     * Makes the subtree of the given node selected.
+     * @param dotscript script with no selection.
+     * @param id id of the root of the subtree to select.
+     * @return modified dot script.
+     */
+    public function select_subtree($dotscript, $id) {
+        $selectstyle = ', style = dotted';
+        $stylelength = qtype_poasquestion_string::strlen($selectstyle);
+        // Our dot script has the format: "[digraph][node styles][node chains].
+        // First, get the chains and find subtree node id's.
+        $script = new qtype_poasquestion_string($dotscript);
+        $index = $script->length() - 2;
+        $chainlength = 1;
+        while ($script[$index - 1] !== ']') {
+            $index--;
+            $chainlength++;
+        }
+        $index++;
+        $chainlength--;
+        $chains = $script->substring($index, $chainlength);
+        $selected = array((int)$id);
+        $tmp = '';
+        $write = false;
+        // Look through the chains of node id's.
+        for ($i = 0; $i < $chains->length(); $i++) {
+            $cur = $chains[$i];
+            if (ctype_digit($cur)) {
+                $tmp .= $cur;
+            } else if ($cur === '-') {
+                // Number ready.
+                if (in_array((int)$tmp, $selected)) {
+                    $write = true;
+                }
+                if ($write) {
+                    if (!in_array((int)$tmp, $selected)) {
+                        $selected[] = (int)$tmp;
+                    }
+                }
+                $tmp = '';
+            } else if ($cur === ';') {
+                // End of a chain.
+                if ($write) {
+                    if (!in_array((int)$tmp, $selected)) {
+                        $selected[] = (int)$tmp;
+                    }
+                }
+                $write = false;
+                $tmp = '';
+            }
+        }
+        // Children nodes obtained, modify their styles.
+        $index = 0;
+        while ($index < $script->length() - $chains->length() - 1) {
+            $cur = $script[$index];
+            // Skip quotes.
+            if ($cur === '"') {
+                $index++;
+                while ($script[$index] !== '"') {
+                    $index++;
+                }
+                $index++;
+                continue;
+            }
+            // Get the current id.
+            if ($script[$index] === ']') {
+                $tmpindex = $index - 1;
+                $tmpid = '';
+                do {
+                    $cur = $script[$tmpindex];
+                    $isdigit = ctype_digit($cur);
+                    if ($isdigit) {
+                        $tmpid = $cur . $tmpid;
+                    }
+                    $tmpindex--;
+                } while ($isdigit);
+                // If this node should be selected, modify its style.
+                if (in_array((int)$tmpid, $selected)) {
+                    $part1 = $script->substring(0, $index);
+                    $part2 = $script->substring($index);
+                    $script = $part1;
+                    $script->concatenate($selectstyle . $part2->string());
+                    $index += $stylelength;
+                }
+
+            }
+            $index++;
+        }
+        return $script->string();
+    }
+}
