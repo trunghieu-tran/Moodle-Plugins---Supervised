04a53d62694e724720f6fc4c896cc35528dc3a4b
Arkanif <oggfire@gamil.com>
2012-08-26 10:14:39 +0400

    ????????? capability ??? ???????? ?????????? ?????? (????? ? moodle 2.3).

--- mod/poasassignment/db/access.php
+++ mod/poasassignment/db/access.php
@@ -37,6 +37,15 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
+    'mod/poasassignment:addinstance' => array(
+        'captype'=>'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array (
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
     'mod/poasassignment:managetasks'=> array(
         'captype'=>'write',
         'contextlevel' => CONTEXT_MODULE,
