308328dbf81d653d6d9a2b963b96482e3bafaeab
vostreltsov <vostreltsov@gmail.com>
2012-09-13 18:20:35 +0000

    ??????????? ????????? ?????? ??????-?????? ? poasquestion. jlex.php ??????? ??? ????.

--- question/type/poasquestion/backup/moodle2/restore_poasquestion_preg_plugin.class.php
+++ question/type/poasquestion/backup/moodle2/restore_poasquestion_preg_plugin.class.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * POAS abstract question type restore code.
@@ -63,10 +77,10 @@ class restore_qtype_poasquestion_plugin extends restore_qtype_plugin {
             }
             $data->answers = implode(',', $answersarr);
 
-            // Insert record
+            // Insert record.
             $newitemid = $DB->insert_record($tablename, $data);
 
-            // Create mapping
+            // Create mapping.
             $this->set_mapping($tablename, $oldid, $newitemid);
         }
     }
