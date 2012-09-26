b593caf4fcac22d23488f0409bce0e6fb307e75e
vaivanov <vaivanov@localhost>
2012-09-26 20:57:25 +0400

    ?????????? ?????? ? ??????? ????????, ????? ??? "????????" ?????????? dot, ? ????? ????????? ??? ? ?????????????????? ?????? ????????.

---
+++ question/behaviour/adaptivehints/renderer.php
@@ -0,0 +1,110 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Renderer for outputting parts of a question belonging to the legacy
+ * adaptive behaviour with hinting.
+ *
+ * @copyright  2011 Oleg Sychev, Volgograd State Technical University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once($CFG->dirroot . '/question/behaviour/adaptive/renderer.php');
+
+class qbehaviour_adaptivehints_renderer extends qbehaviour_adaptive_renderer {
+
+     public function button_cost($str, $penalty, $options) {
+        return '  '.get_string($str, 'qbehaviour_adaptivehints', format_float($penalty, $options->markdp));
+     }
+
+    public function controls(question_attempt $qa, question_display_options $options) {
+        $question = $qa->get_question();
+        $output = parent::controls($qa, $options);//submit button
+        $penalty = $question->penalty;
+        if ($penalty != 0) {
+            $output .= $this->button_cost('withpossiblepenalty', $penalty, $options);
+        }
+        $output .= html_writer::empty_tag('br');
+
+        //Render buttons that should be rendered by behaviour.
+         foreach ($question->available_specific_hint_types() as $hintkey => $hintdescription) {
+
+            $hintobj = $question->hint_object($hintkey);
+
+            if (!$hintobj->button_rendered_by_question()) {//Button(s) isn't rendered by the question, so behaviour must render it.
+
+                //Check whether button should be rendered at all.
+                $laststep = $qa->get_last_step();
+                if ($hintobj->hint_response_based()) {
+                    $showhintbtn = $laststep->has_behaviour_var('_resp_hintbtns');
+                } else {
+                    $showhintbtn = $laststep->has_behaviour_var('_nonresp_hintbtns');
+                }
+                if (!$showhintbtn || !$hintobj->hint_available()) {
+                    continue;
+                }
+
+                //Render button.
+                $attributes = array(
+                    'type' => 'submit',
+                    'id' => $qa->get_behaviour_field_name($hintkey.'btn'),
+                    'name' => $qa->get_behaviour_field_name($hintkey.'btn'),
+                    'value' => get_string('hintbtn', 'qbehaviour_adaptivehints', $hintdescription),
+                    'class' => 'submit btn',
+                );
+                $output .= html_writer::empty_tag('input', $attributes);
+
+                //Cost message
+                if ($hintobj->penalty_response_based()) {//if penalty is response-based
+                    //try to get last response
+                    $response = $qa->get_last_qt_data();
+                    if (empty($response)) {
+                        $response = null;
+                    }
+                    $penalty = $hintobj->penalty_for_specific_hint($response);
+                    if ($penalty != 0) {
+                        $output .= $this->button_cost('withpenaltyapprox', $penalty, $options);//note that reported penalty is approximation since user could change response in adaptive
+                    }
+                } else {
+                    $penalty = $hintobj->penalty_for_specific_hint(null);
+                    if ($penalty != 0) {
+                        $output .= $this->button_cost('withpenalty', $penalty, $options);
+                    }
+                }
+                $output .= html_writer::empty_tag('br');
+
+                /*if (!$options->readonly) {
+                $this->page->requires->js_init_call('M.core_question_engine.init_submit_button',
+                        array($attributes['id'], $qa->get_slot()));
+                }*/
+            }
+        }
+
+        return $output;
+    }
+
+    //Overload penalty_info to show actual penalty
+    protected function penalty_info(question_attempt $qa, $mark,
+            question_display_options $options) {
+        if (!$qa->get_question()->penalty && !$qa->get_last_behaviour_var('_hashint', false)) {//no penalty for the attempts and no hinting done
+            return '';
+        }
+        $output = '';
+
+        // Print details of grade adjustment due to penalties
+        if ($mark->raw != $mark->cur) {
+            $output .= ' ' . get_string('gradingdetailsadjustment', 'qbehaviour_adaptive', $mark);
+        }
+
+        // Print information about any new penalty, only relevant if the answer can be improved.
+        if ($qa->get_behaviour()->is_state_improvable($qa->get_state())) {
+            $output .= ' ' . get_string('gradingdetailspenalty', 'qbehaviour_adaptive',
+                    format_float($qa->get_last_step()->get_behaviour_var('_penalty'), $options->markdp));
+        }
+
+        return $output;
+    }
+}
+
---
+++ question/type/preg/authors_tool/regex_test_push.js
@@ -0,0 +1,233 @@
+/**
+ * Script for button "Test regex" from edit_ast_preg_form.php
+ * 
+ * @copyright &copy; 2012  Terechov Grigory, Pahomov Dmitry
+ * @author Terechov Grigory, Pahomov Dmitry, Volgograd State Technical University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package questions
+ */
+ 
+// TODO - code documentation
+ 
+M.qtype_preg_authors_tool = {};
+ 
+M.qtype_preg_authors_tool.init = function(Y) {
+
+    // variables:
+    var node;
+    var context;
+    var back;
+    var hidden;
+    var currentlineedit;
+
+    // functions:
+    var load_content = function(url) {
+
+        var upd_dialog_Success = function(id, o, a) {
+            // this is debug output (should be deleted is release): !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+            var indexofbracket = o.responseText.indexOf("{");
+            if (indexofbracket != 0) {
+                alert(o.responseText.substr(0,indexofbracket));
+            }
+            // allerting json array:
+            // alert(o.responseText.substr(indexofbracket));
+            // end of debug output !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            var jsonarray = Y.JSON.parse(o.responseText);
+
+            //TODO: add errors message
+            if(typeof jsonarray['tree_src'] != 'undefined') {
+                Y.one('#id_tree').setAttribute("src", '').setAttribute("src", jsonarray['tree_src']);
+            }
+            if(typeof jsonarray['map'] != 'undefined') {
+                Y.one('#tree_map').setHTML(jsonarray['map']);
+                Y.all("#_anonymous_0 > area").on('click', check_tree);
+            }
+            if(typeof jsonarray['graph_src'] != 'undefined') {
+                Y.one('#id_graph').setAttribute("src", '').setAttribute("src", jsonarray['graph_src']);
+            }
+            if(typeof jsonarray['description'] != 'undefined') {
+                Y.one('#description_handler').setHTML(jsonarray['description']);
+            }
+
+            node = Y.one('#id_regex_check');
+            context = Y.one('#id_regex_text');
+            back = Y.one('#id_regex_back');
+            hidden = Y.one('#hidden_id');
+            back.on("click", back_regex, hidden);
+            node.on("click", check_regex, context);
+        }
+
+        var upd_dialog_failure = function(id, o, a) {
+            alert("ERROR " + id + " " + a);
+        }
+
+        var cfg = {
+            method: "GET",
+            xdr: {
+                use: 'native'
+            },
+            on: {
+                success: upd_dialog_Success,
+                failure: upd_dialog_failure
+            }
+        };
+
+        var response = Y.io(url, cfg);
+    }
+
+    var testregexbtn_pressed = function(e) {
+
+        e.preventDefault();
+
+        //var page_regex_auth_helper_height = 1000;
+        var pageregexauthhelperwidth = 1000;
+        currentlineedit = this;
+        //regex = encodeURIComponent(this.get('value'));
+        // TODO - replace preg_www_root with moodle variable
+        var pageregexauthhelperadr = preg_www_root + '/question/type/preg/authors_tool/ast_preg_form.php?regex=' + encodeURIComponent(this.get('value')) + '&id=-1' + '&id_line_edit=' + this.getAttribute('id');
+        if (typeof dialog == 'undefined') {
+            dialog = new Y.Panel({
+                contentBox: Y.Node.create('<div id="dialog" />'),
+                bodyContent: '<div class="message icon-warn">Loading...</div>',
+                width: pageregexauthhelperwidth,
+                //height     : page_regex_auth_helper_height,
+                zIndex: 120,
+                centered: true,
+                modal: true, // modal behavior
+                render: '.example',
+                visible: true, // make visible explicitly with .show()
+                buttons: {
+                    footer: [
+                        {
+                            name: 'cancel',
+                            label: 'Cancel',
+                            action: 'onCancel'
+                        },
+
+                        {
+                            name: 'proceed',
+                            label: 'OK',
+                            action: 'onOK'
+                        }
+                    ]
+                }
+            });
+
+            dialog.onCancel = function(e) {
+                e.preventDefault();
+                this.hide();
+                // the callback is not executed, and is
+                // callback reference removed, so it won't persist
+                this.callback = false;
+                //Y.one('#tree_img').setAttribute('src','');
+            }
+
+            dialog.onOK = function(e) {
+                e.preventDefault();
+                //TODO: implement save text widgets "string test" in database
+                //alert('123');
+                this.hide();
+                // code that executes the user confirmed action goes here
+                if(this.callback) {
+                    this.callback();
+                }
+                //  callback reference removed, so it won't persist
+                this.callback = false;
+                //Y.one('#tree_img').setAttribute('src','');
+            }
+
+            //Y.one('#dialog .message').load('http://localhost/moodle/question/type/preg/ast_preg_form.php?regex='+this.get("value"));
+
+            Y.one('#dialog .message').load(preg_www_root + '/question/type/preg/authors_tool/ast_preg_form.php?regex=' + encodeURIComponent(currentlineedit.get('value')) + '&id=-1', function() {
+                /*Y.Get.js(preg_www_root + '/question/type/preg/authors_tool/preg_authors_tool_script.js', function(err) {
+                    if(err) {
+                        alert('Error loading JS: ' + err[0].error, 'error');
+                        return;
+                    }
+                })*/
+                //TODO: set empty src in all field
+                Y.one('#id_regex_text').set('value', currentlineedit.get('value'));
+                Y.one('#id_tree').setAttribute("src", preg_www_root + '/question/type/preg/tmp_img/spacer.gif');
+                Y.one('#id_graph').setAttribute("src", preg_www_root + '/question/type/preg/tmp_img/spacer.gif');
+                load_content(preg_www_root + '/question/type/preg/authors_tool/preg_authors_tool_load.php?regex=' + encodeURIComponent(currentlineedit.get('value')) + '&id=-1');
+            })
+
+        } else {
+            //TODO: set empty src in all field
+            Y.one('#id_regex_text').set('value', currentlineedit.get('value'));
+            Y.one('#id_tree').setAttribute("src", preg_www_root + '/question/type/preg/tmp_img/spacer.gif');
+            Y.one('#id_graph').setAttribute("src", preg_www_root + '/question/type/preg/tmp_img/spacer.gif');
+            load_content(preg_www_root + '/question/type/preg/authors_tool/preg_authors_tool_load.php?regex=' + encodeURIComponent(currentlineedit.get('value')) + '&id=-1');
+            dialog.show();
+        }
+    }
+
+    var back_regex = function( e ) {
+        
+        e.preventDefault();
+       
+        var new_regex = Y.one(context).get('value');
+        currentlineedit.set('value',new_regex);
+        dialog.hide();
+        
+        //TODO: call OK button
+        //dialog.onOK();
+
+    }
+
+    var highlight_description = function(id){
+        
+        const highlightedclass = 'description_highlighted';
+        var oldhighlighted = Y.one('.'+highlightedclass);
+        
+        if(oldhighlighted!=null){
+           oldhighlighted.removeClass(highlightedclass).setStyle('background-color','transparent');
+        }
+        
+        Y.one('.description_node_'+id).addClass(highlightedclass).setStyle('background-color','yellow');
+    }
+    
+    var check_regex = function( e ) {
+        
+        e.preventDefault();
+        
+        load_content(preg_www_root + '/question/type/preg/authors_tool/preg_authors_tool_load.php?regex='+encodeURIComponent(Y.one('#id_regex_text').get('value'))+'&id=-1');
+    }    
+    
+    var check_tree = function( e ) {
+
+       id = e.currentTarget.getAttribute ( 'id' );
+       //alert(id);
+       highlight_description(id);
+        
+       /*var tmp = encodeURIComponent(context.get("value"));
+       
+       var url = preg_www_root + '/question/type/preg/authors_tool/ast_preg_form.php?regex=' + tmp + '&id=' + id;
+       Y.io(url);
+       Y.one('#id_graph').setAttribute('src','');
+       setTimeout(function() {
+           Y.one('#id_graph').setAttribute('src', preg_www_root + '/question/type/preg/tmp_img/graph.png');
+           //TODO: implement for tree and map
+           }, 500);*/
+       //Y.one('#id_graph').setAttribute('src','').setAttribute('src','http://localhost/moodle/question/type/preg/tmp_img/graph.png');
+       
+       load_content(preg_www_root + '/question/type/preg/authors_tool/preg_authors_tool_load.php?regex='+encodeURIComponent(Y.one('#id_regex_text').get('value')) + '&id=' + id);
+    }
+
+    // code:
+    // installation click handler:
+    var i = 0;
+    var testregexbtn = Y.one('#id_regextest_' + i);
+    var testregexlineedit = Y.one('#id_answer_' + i);
+    while(testregexbtn != null) {
+        testregexbtn.on("click", testregexbtn_pressed, testregexlineedit);
+        ++i;
+        testregexbtn = Y.one('#id_regextest_' + i);
+        testregexlineedit = Y.one('#id_answer_' + i);
+    }
+}
+
+
+YUI().use('node', 'panel', 'node-load', 'get', "io-xdr", "substitute", "json-parse", function(Y) {
+    M.qtype_preg_authors_tool.init(Y);
+});
