571c10ebd1063bd3176c4052eaacb84a73a8d9f7
Arkanif <oggfire@gamil.com>
2012-10-23 19:58:05 +0400

    ???????? ????? ??? ?????? ????????? ?????? ????? ??????? ????????.

--- mod/poasassignment/taskgivers/taskgiver.php
+++ mod/poasassignment/taskgivers/taskgiver.php
@@ -6,6 +6,7 @@
  * @author Arkanif
  */
 abstract class taskgiver {
+    public function process_before_output($cmid, $poasassignment) {}
     public function process_before_tasks($cmid, $poasassignment) {}
     public function get_task_extra_string($taskid, $cmid){}
     public function process_after_tasks() {}
