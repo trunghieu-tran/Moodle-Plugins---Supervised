24c7e8a4eebbe9a55a327147c84514125de812cd
oasychev <oasychev@gmail.com>
2012-11-06 19:47:34 +0300

    ? ????????? ???????????? ????? ????????? ??????? ??? ??????????. ?????????? ????????? ????? ? ????? preg_hints.php

---
+++ question/type/preg/version.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Preg question type - https://code.google.com/p/oasychev-moodle-plugins/
+//
+// Preg question type is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for the Preg question type.
+ *
+ * @package    qtype_preg
+ * @copyright  2012 Oleg Sychev, Volgograd State Technical University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'qtype_preg';
+$plugin->version  = 2012090300;
+$plugin->requires = 2012062501;
+$plugin->release = 'Preg 2.3';
+$plugin->maturity = MATURITY_BETA;
+
+$plugin->dependencies = array(
+    'qtype_shortanswer' => 2011102700,
+    'qbehaviour_adaptivehints' => 2011111902,
+    'qbehaviour_adaptivehintsnopenalties' => 2011111902,
+    'qtype_poasquestion' => 2012060900,
+    'block_formal_langs' => 2012021400
+);
