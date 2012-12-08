a9f2909e27eee9da24893a35c13b5041f0953a61
oasychev <oasychev@gmail.com>
2012-12-08 14:46:19 +0300

    ????????? ?????? Moodle ?  ???????? ? POASQuestion.

--- question/type/poasquestion/version.php
+++ question/type/poasquestion/version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'qtype_poasquestion';
 $plugin->version   = 2012060900;
 $plugin->maturity  = MATURITY_STABLE;
+$plugin->requires = 2012062501;
+$plugin->release = 'Abstract Poasquestion 2.3';
