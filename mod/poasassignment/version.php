cc4542057ef5fb000b6329b2353a7df815d54ef6
Arkanif <oggfire@gamil.com>
2013-01-31 20:12:30 +0400

    ??????????? ????????? ????????? ???????.

--- mod/poasassignment/version.php
+++ mod/poasassignment/version.php
@@ -3,5 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $module->version  = 2012101102;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2010031900;  // Requires this Moodle version
+$module->requires = 2011120500;  // Requires this Moodle version
 $module->cron     = 60;          // Period for cron to check this module (secs)
+$module->maturity = MATURITY_RC;
+$module->release  = '2.4 (2013.01)';
