5889632aff9fadef8628c7443895d18ed5677c6f
oasychev <oasychev@localhost>
2013-02-17 20:08:00 +0300

    ????? ? ????????? ??????? ?? ??????????? ??? Moodle.

--- question/type/correctwriting/textimagerenderer.php
+++ question/type/correctwriting/textimagerenderer.php
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define('FONT' , dirname(__FILE__) . '/Ledger-Regular.ttf');
+define('FONT' , dirname(__FILE__) . '/../../../lib/default.ttf');
 define('FONT_SIZE' , 10.0);
 
 /**
