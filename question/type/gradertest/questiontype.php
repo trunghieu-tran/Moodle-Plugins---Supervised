65f35e75c74315faf004489e8e5f56e769754058
Arkanif <oggfire@gamil.com>
2013-03-13 22:15:51 +0400

    ????????? ????? "???? ???????? ??? ???????? ?????? ?? ????" ? ??????????? ?? ????????? ? ??????? - ??????????, ?????????, ????????. ????? "?????" ? "???????, ? ??????? ????????? ????" ??? ?? ??????????, ???????? ? ????????.

--- question/type/gradertest/questiontype.php
+++ question/type/gradertest/questiontype.php
@@ -1,69 +1,86 @@
 <?
 class qtype_gradertest extends question_type {
 
-    public $fileoptions = array('subdirs' => false,
-                                'maxfiles' => -1,
-                                'maxbytes' => 0);
     function name() {
         return 'gradertest';
     }
+
     /* Saving test data
      */
     function save_question_options($question) {
         global $DB;
-        $gradertest = new stdClass();
-        $gradertest->questionid = $question->id;
         
-        if (!$DB->record_exists('question_gradertest', array('questionid' => $question->id))) {
-            $gradertest->id = $DB->insert_record('question_gradertest', $gradertest);
-        }
-        else {
-            $rec = $DB->get_record('question_gradertest', array('questionid' => $question->id));
-            $gradertest->id = $rec->id;
-            $DB->update_record('question_gradertest', $gradertest);
-        }
-        
-        $DB->delete_records('question_gradertest_tests', array('gradertestid' => $gradertest->id));
+        $DB->delete_records('question_gradertest_tests', array('questionid' => $question->id));
         for ($i = 0; $i < $question->option_repeats; $i++) {
-            if (empty($question->testname[$i])) {
+            $gradertest = new stdClass();
+            $gradertest->questionid = $question->id;
+            if ($question->testdirpath[$i]) {
+                // Тест как путь к папке
+                $gradertest->testdirpath = $question->testdirpath[$i];
+            }
+            elseif ($question->testname[$i]) {
+                // Тест как набор тестовых данных
+                $gradertest->name = $question->testname[$i];
+                $gradertest->testin = $question->testin[$i];
+                $gradertest->testout = $question->testout[$i];
+            }
+            else {
                 continue;
             }
-            $testrec = new stdClass();
-            $testrec->name = $question->testname[$i];
-            $testrec->weight = $question->testweight[$i];
-            $testrec->testin = $question->testin[$i];
-            $testrec->testout = $question->testout[$i];
-            $testrec->gradertestid = $gradertest->id;
-            $DB->insert_record('question_gradertest_tests', $testrec);            
+            $DB->insert_record('question_gradertest_tests', $gradertest);
         }
+
+        $DB->delete_records('question_gradertest_tasktest', array('questionid' => $question->id));
+        foreach ($question->task as $taskID => $val) {
+            if ($val == 1) {
+                $tasktest = new stdClass();
+                $tasktest->poasassignmenttaskid = $taskID;
+                $tasktest->questionid = $question->id;
+
+                $DB->insert_record('question_gradertest_tasktest', $tasktest);
+            }
+        }
+        $DB->delete_records('question_gradertest', array('questionid' => $question->id));
+        $gradertest = new stdClass();
+        $gradertest->questionid = $question->id;
+        if ($question->availablefromhome) {
+            $gradertest->availablefromhome = 1;
+        }
+        else {
+            $gradertest->availablefromhome = 0;
+        }
+        $DB->insert_record('question_gradertest', $gradertest);
         return null;
-        //$this->save_testfiles($question->testfiles, $question->context, 'questiontext', $question->id);
-        
-        //return null;
-    }
-    function save_testfiles($element, $context, $area = 'questiontext', $id) {
-        global $DB;
-        $fs = get_file_storage();
-        file_save_draft_area_files($element, 
-                                   $context->id, 
-                                   'question', 
-                                   $area, 
-                                   $id, 
-                                   $this->fileoptions);
-    }
-    function get_question_options(&$question) {
-        global $DB;
-        $gradertest = $DB->get_record('question_gradertest', array('questionid' => $question->id));
-        $tests = $DB->get_records('question_gradertest_tests', array('gradertestid' => $gradertest->id));
-        $i = 0;
-        foreach($tests as $test) {
-            $question->testname[$i] = $test->name;
-            $question->testin[$i] = $test->testin;
-            $question->testout[$i] = $test->testout;
-            $question->testweight[$i] = $test->weight;
-            $i++;
-        }
     }
 
+    function get_question_options($question) {
+        global $DB;
+        $gradertests = $DB->get_records('question_gradertest_tests', array('questionid' => $question->id), 'id asc');
+        $i = 0;
+        foreach($gradertests as $gradertest) {
+            $question->testdirpath[$i] = $gradertest->testdirpath;
+            $question->testname[$i] = $gradertest->name;
+            $question->testin[$i] = $gradertest->testin;
+            $question->testout[$i] = $gradertest->testout;
+            $i++;
+        }
+        $tasktests = $DB->get_records('question_gradertest_tasktest', array('questionid' => $question->id), 'id asc');
+        foreach ($tasktests as $tasktest) {
+            $question->task[$tasktest->poasassignmenttaskid] = 1;
+        }
+        $gradertest = $DB->get_record('question_gradertest', array('questionid' => $question->id));
+        if ($gradertest->availablefromhome == 1)
+            $question->availablefromhome = 1;
+
+        return true;
+    }
+
+    public function delete_question($questionid, $contextid) {
+        global $DB;
+        $DB->delete_records('question_gradertest_tests', array('questionid' => $questionid));
+        $DB->delete_records('question_gradertest_tasktest', array('questionid' => $questionid));
+        $DB->delete_records('question_gradertest', array('questionid' => $questionid));
+
+        parent::delete_question($questionid, $contextid);
+    }
 }
-//question_register_questiontype(new gradertest_qtype());
