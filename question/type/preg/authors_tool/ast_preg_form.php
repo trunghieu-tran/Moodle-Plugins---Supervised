b15eda3c16edd3d58b554a40237164529da7c84d
gvterechov <gvterechov@localhost>
2013-03-24 19:47:42 +0400

    ??????????? ???????? context ??? ?????????? $PAGE

--- question/type/preg/authors_tool/ast_preg_form.php
+++ question/type/preg/authors_tool/ast_preg_form.php
@@ -14,35 +14,39 @@ require_once(dirname(__FILE__) . '/../../../../config.php');
 
 global $CFG;
 global $PAGE;
+global $DB;
 
 require_once($CFG->dirroot . '/question/type/preg/authors_tool/edit_ast_preg_form.php');
-//require_once('preg_authors_tool_load.php');
+require_once($CFG->libdir . '/questionlib.php');
+//require_once(dirname(__FILE__) . '/previewlib.php');
+
+// Get and validate question id.
+//$id = required_param('id', PARAM_INT);
+$id = 1;
+$question = question_bank::load_question($id);
+
+// Were we given a particular context to run the question in?
+// This affects things like filter settings, or forced theme or language.
+if ($cmid = optional_param('cmid', 0, PARAM_INT)) {
+    $cm = get_coursemodule_from_id(false, $cmid);
+    require_login($cm->course, false, $cm);
+    $context = context_module::instance($cmid);
+
+} else if ($courseid = optional_param('courseid', 0, PARAM_INT)) {
+    require_login($courseid);
+    $context = context_course::instance($courseid);
+
+} else {
+    require_login();
+    $category = $DB->get_record('question_categories',
+            array('id' => $question->category), '*', MUST_EXIST);
+    $context = context::instance_by_id($category->contextid);
+    $PAGE->set_context($context);
+    // Note that in the other cases, require_login will set the correct page context.
+}
 
 //Instantiate simplehtml_form 
 $mform = new qtype_preg_authors_tool_form();
+$mform->display();
 
-//TODO: do check user
-if ($mform->no_submit_button_pressed()) {
-    //$mform->addElement('html', 'Interactive tree</br><img src="" />');
-    
-} else {
-//Form processing and displaying is done here
-/*if ($mform->is_cancelled()) {
-    //Handle form cancel operation, if cancel button is present on form
-    
-} else if ($fromform = $mform->get_data()) {
-    //In this case you process validated data. $mform->get_data() returns data posted in form.
-  
-} else {*/
-    // this branch is executed if the form is submitted but the data doesn't validate and the form should be redisplayed
-    // or on the first display of the form.
-  
-
-    //Set default data (if any)
-    //$mform->set_data($toform);
-
-    //displays the form
-    $mform->display();
-
-}
 ?>
