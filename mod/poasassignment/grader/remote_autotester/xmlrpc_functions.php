c8478fd376cdf48305fac34535a7dfae5f3b8ab5
Arkanif <oggfire@gamil.com>
2013-03-25 22:42:26 +0400

    ??? ?????????? ???????? remote_autotester: 1. ????? ??????? ????? ?????????? 1 ??? - ????? ????? ???????, ?????????????? ???????? ? ??????? poasassignment_gr_ra. 2. ??? ??? ?. 1, ?? ??? ??????????? ?????????? ??????????? ? ??????? poasassignment_gr_ra_tests ?????? ??? - update ??????? ?? insert 3. ????????? ?????? ?????????? kd xmlrpc, ??????? ?????? ??? ??????? 4. ??????????? ?????? ???? ? ?????

--- mod/poasassignment/grader/remote_autotester/xmlrpc_functions.php
+++ mod/poasassignment/grader/remote_autotester/xmlrpc_functions.php
@@ -58,7 +58,7 @@ function notify_compile_started($loginHash, $passwordHash, $attemptid) {
  * @param $attemptid
  * @param $compiled
  */
-function notify_test_started($loginHash, $passwordHash, $attemptid, $test) {
+function notify_test_started($loginHash, $passwordHash, $attemptid) {
     $config = get_config("poasassignment_remote_autotester");
     if (md5($config->login) != $loginHash || md5($config->password) != $passwordHash) {
         XMLRPC_response(XMLRPC_prepare("401 Unauthorized"));
@@ -67,13 +67,9 @@ function notify_test_started($loginHash, $passwordHash, $attemptid, $test) {
         global $DB;
         $records = $DB->get_records('poasassignment_gr_ra', array('attemptid' => $attemptid), 'id DESC', 'id, attemptid');
         if (count($records) > 0) {
-            $recordremote = array_shift($records);
-
-            $testinfo = new stdClass();
-            $testinfo->remote_id = $recordremote->id;
-            $testinfo->test = $test;
-            $testinfo->timeteststarted = time();
-            $DB->insert_record('poasassignment_gr_ra_tests', $testinfo);
+            $record = array_shift($records);
+            $record->timeteststart = time();
+            $DB->update_record('poasassignment_gr_ra', $record);
         }
         XMLRPC_response(XMLRPC_prepare("200 OK"));
     }
@@ -85,7 +81,7 @@ function notify_test_started($loginHash, $passwordHash, $attemptid, $test) {
  * @param $attemptid
  * @param $compiled
  */
-function notify_tested($loginHash, $passwordHash, $attemptid, $test, $istested, $studentout) {
+function notify_tested($loginHash, $passwordHash, $attemptid, $test, $testin, $testout, $studentout, $istested) {
     $config = get_config("poasassignment_remote_autotester");
     if (md5($config->login) != $loginHash || md5($config->password) != $passwordHash) {
         XMLRPC_response(XMLRPC_prepare("401 Unauthorized"));
@@ -95,15 +91,22 @@ function notify_tested($loginHash, $passwordHash, $attemptid, $test, $istested, 
         $records = $DB->get_records('poasassignment_gr_ra', array('attemptid' => $attemptid), 'id DESC', 'id, attemptid');
         if (count($records) > 0) {
             $recordremote = array_shift($records);
-            $testresults = $DB->get_records('poasassignment_gr_ra_tests', array('remote_id' => $recordremote->id, 'test' => $test), 'id DESC', 'id');
-            $testresult = array_shift($testresults);
+
+            $testresult = new stdClass();
+            $testresult->remote_id = $recordremote->id;
+            $testresult->test = $test;
             $testresult->timetested = time();
-            if ($istested)
-                $testresult->testpassed = 1;
-            else
-                $testresult->testpassed = 0;
+            $testresult->testin = $testin;
+            $testresult->testout = $testout;
             $testresult->studentout = $studentout;
-            $DB->update_record('poasassignment_gr_ra_tests', $testresult);
+
+            if ($istested) {
+                $testresult->testpassed = 1;
+            }
+            else {
+                $testresult->testpassed = 0;
+            }
+            $DB->insert_record('poasassignment_gr_ra_tests', $testresult);
         }
         XMLRPC_response(XMLRPC_prepare("200 OK"));
     }
