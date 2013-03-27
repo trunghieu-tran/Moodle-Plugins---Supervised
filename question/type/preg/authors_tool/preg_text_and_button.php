960853b57a3fa34e15b6853a0649d337eefeaf55
gvterechov <gvterechov@localhost>
2013-03-27 22:42:28 +0400

    ???????? ????? MoodleQuickForm_preg_text_and_button ??? ???? ??????? preg

---
+++ question/type/preg/authors_tool/preg_text_and_button.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Defines button-with-text-input widget, parent of abstract poasquestion
+ * text-and-button widget. This class extends parent class with javascript
+ * callbacks for button clicks.
+ *
+ * @package    qtype_preg
+ * @copyright  &copy; 2013 Pahomov Dmitry <topt.iiiii@gmail.com>
+ * @author     Pahomov Dmitry <topt.iiiii@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot.'/question/type/poasquestion/poasquestion_text_and_button.php');
+
+MoodleQuickForm::registerElementType('preg_text_and_button',
+    $CFG->dirroot.'/question/type/preg/authors_tool/preg_text_and_button.php',
+    'MoodleQuickForm_preg_text_and_button');
+
+class MoodleQuickForm_preg_text_and_button extends MoodleQuickForm_text_and_button { 
+    
+    //private $parentjsobjname = 'M.poasquestion_text_and_button';
+    
+    function MoodleQuickForm_preg_text_and_button($elementName=null, $elementButtonName=null, $elementLabel=null, $elementLinks=null, $attributes=null) {
+        parent::MoodleQuickForm_text_and_button($elementName, $elementButtonName, $elementLabel, $elementLinks, $attributes);
+    }
+    
+    function toHtml() {
+        global $CFG;
+        global $PAGE;
+        $parenthtml = parent::toHtml();
+        $jsmodule = array(  'name' => 'preg_text_and_button',
+                            'fullpath' => '/question/type/preg/authors_tool/preg_authors_tool_script.js',
+                            'requires' => array('node', 'io-base')
+        );
+        $jsargs = array(
+            $CFG->wwwroot,
+            'todo',
+        );
+        $PAGE->requires->js_init_call('M.preg_authors_tool_script.init', $jsargs, true, $jsmodule);
+        
+        return $parenthtml;
+    }
+    
+}
