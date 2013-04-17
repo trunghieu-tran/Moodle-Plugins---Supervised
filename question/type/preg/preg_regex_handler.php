9acff49bd1aa7f69cfc7746411319f847faa33ab
vostreltsov <vostreltsov@gmail.com>
2013-04-17 22:40:45 +0000

    ???????? ???? ?? ????????: ???? ?????? ????????? ? ???????? - ?? ????????. ??? ??? ???? ???????? ? ??????? ?????? ?????, ?? ? ????? ?? ???? ????????. ???? ??????? ?????? ???? ???????????, ??????? ???????? isset(type) ? isset(subtype): ????? ?????????? ????? ???? ?????? ??????? ?? ?????, ? ??? ???? ???? ? ???????? qtype_preg_node.

--- question/type/preg/preg_regex_handler.php
+++ question/type/preg/preg_regex_handler.php
@@ -380,20 +380,26 @@ class qtype_preg_regex_handler {
         if (is_a($node, 'qtype_preg_leaf')) {
             // Expression starts from ^
             return ($node->subtype === qtype_preg_leaf_assert::SUBTYPE_CIRCUMFLEX);
-        } else if (isset($node->type) && $node->type == qtype_preg_node::TYPE_NODE_INFINITE_QUANT && $node->leftborder == 0) {
+        }
+
+        if ($node->type == qtype_preg_node::TYPE_NODE_INFINITE_QUANT && $node->leftborder == 0) {
             // Expression starts from .*
             $operand = $node->operands[0];
-            return ($node->leftborder === 0 && isset($operand->type) && $operand->type == qtype_preg_node::TYPE_LEAF_CHARSET &&
+            return ($node->leftborder === 0 && $operand->type == qtype_preg_node::TYPE_LEAF_CHARSET &&
                     count($operand->flags) > 0 && $operand->flags[0][0]->data === qtype_preg_charset_flag::META_DOT);
-        } else if (isset($node->type) && $node->type == qtype_preg_node::TYPE_NODE_CONCAT || isset($node->type) && $node->type == qtype_preg_node::TYPE_NODE_SUBEXPR) {
+        }
+
+        if ($node->type == qtype_preg_node::TYPE_NODE_CONCAT || $node->type == qtype_preg_node::TYPE_NODE_SUBEXPR) {
             // Check the first operand for concatenation and subexpressions.
-            return $this->look_for_circumflex($node->operands[0], $wasconcat || isset($node->type) && $node->type == qtype_preg_node::TYPE_NODE_CONCAT);
-        } else if (isset($node->type) && $node->type == qtype_preg_node::TYPE_NODE_ALT) {
-            // Every branch of alternative is anchored.
+            return $this->look_for_circumflex($node->operands[0], $wasconcat || $node->type == qtype_preg_node::TYPE_NODE_CONCAT);
+        }
+
+        if ($node->type == qtype_preg_node::TYPE_NODE_ALT) {
+            // Every branch of alternation is anchored.
             $cf = true;
             $empty = false;
             foreach ($node->operands as $operand) {
-                $empty = $empty || isset($operand->subtype) && $operand->subtype === qtype_preg_leaf_meta::SUBTYPE_EMPTY;
+                $empty = $empty || $operand->subtype === qtype_preg_leaf_meta::SUBTYPE_EMPTY;
                 $cf = $cf && $this->look_for_circumflex($operand, $wasconcat);
             }
             $empty = $empty && !$wasconcat;
--- question/type/preg/tests/preg_nodes_test.php
+++ question/type/preg/tests/preg_nodes_test.php
@@ -171,10 +171,10 @@ class qtype_preg_nodes_test extends PHPUnit_Framework_TestCase {
         $this->assertFalse($handler->is_regex_anchored());
         $handler = new qtype_preg_regex_handler('^(?:a.+$)|.*cd|(^a|.*x)|^');
         $this->assertTrue($handler->is_regex_anchored());
-        $handler = new qtype_preg_regex_handler('^(?:a.+$)|.*cd|(^a|.*x)|');        // (|c) breaks anchoring
-        $this->assertFalse($handler->is_regex_anchored());
-        $handler = new qtype_preg_regex_handler('^(?:a.+$)|.*cd|(^a|.*x)|(|c)');    // (|c) breaks anchoring
-        $this->assertFalse($handler->is_regex_anchored());
+        $handler = new qtype_preg_regex_handler('^(?:a.+$)|.*cd|(^a|.*x)||||');     // Emptiness makes anchoring
+        $this->assertTrue($handler->is_regex_anchored());
+        $handler = new qtype_preg_regex_handler('^(?:a.+$)|.*cd|(^a|.*x)|(|c)');    // (|c) makes anchoring
+        $this->assertTrue($handler->is_regex_anchored());
     }
 
     function test_syntax_errors() {
