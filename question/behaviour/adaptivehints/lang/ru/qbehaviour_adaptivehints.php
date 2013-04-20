44740dc08c5f46132a2cbe663e97291762d85060
Pahomov Dmitry <topt.iiiii@gmail.com>
2013-04-20 23:28:54 +0400

    ???????? n-????? ???????????? ? ???????????

---
+++ question/behaviour/adaptivehints/lang/ru/qbehaviour_adaptivehints.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Strings for component 'qbehaviour_adaptivehints', language 'ru'.
+ *
+ * @package    qbehaviour
+ * @subpackage adaptivewithhints
+ * @copyright  2011 Oleg Sychev, Volgograd State Technical University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['hintbtn'] = 'Подсказать {$a}';
+$string['hintused'] = 'Запрошена подсказка: {$a->hint} для ответа {$a->response} с вычетом {$a->penalty}.';
+$string['pluginname'] = 'Адаптивный режим с подсказками';
+$string['withpenalty'] = ' с вычетом {$a}';
+$string['withpenaltyapprox'] = ' с вычетом {$a} (может измениться при смене ответа)';
+$string['withpossiblepenalty'] = ' с возможным вычетом {$a} за неправильный ответ';
