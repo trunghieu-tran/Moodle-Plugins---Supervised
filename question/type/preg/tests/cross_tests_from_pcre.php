173d1823fd51209ba547791766a8edb7834d76da
vostreltsov <vostreltsov@gmail.com>
2013-05-08 15:55:59 +0000

    ???????? ?????? ? ????? ? ????????? ???????????.

--- question/type/preg/tests/cross_tests_from_pcre.php
+++ question/type/preg/tests/cross_tests_from_pcre.php
@@ -5172,17 +5172,17 @@ class qtype_preg_cross_tests_from_pcre {
         $test7 = array( 'str'=>"b\bc",
                         'is_match'=>true,
                         'full'=>false,
-                        'index_first'=>array(0=>0,1=>0),
-                        'length'=>array(0=>1,1=>1),
-                        'left'=>array(4),
-                        'next'=>'[^\b]',
+                        'index_first'=>array(0=>0,1=>0,2=>1,3=>2),
+                        'length'=>array(0=>4,1=>1,2=>1,3=>0),
+                        'left'=>array(1),
+                        'next'=>'[^\d]',
                         'tags'=>array(qtype_preg_cross_tester::TAG_FROM_PCRE));
 
         $test8 = array( 'str'=>'baccd',
                         'is_match'=>true,
                         'full'=>false,
-                        'index_first'=>array(0=>0,1=>0,2=>1,3=>2,4=>2),
-                        'length'=>array(0=>4,1=>1,2=>1,3=>0,4=>2),
+                        'index_first'=>array(0=>0,1=>0,2=>1,3=>2),
+                        'length'=>array(0=>4,1=>1,2=>1,3=>0),
                         'left'=>array(1),
                         'next'=>'[^d]',
                         'tags'=>array(qtype_preg_cross_tester::TAG_FROM_PCRE));
