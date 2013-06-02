035d0b9f424706f5500810e6bc4a04801b7cc51d
oasychev <oasychev@gmail.com>
2013-06-02 16:05:54 +0300

    ? ????? ????? ?????????? ?????? ????????? ??????????? ????????? GPL/Moodle.

--- blocks/formal_langs/db/access.php
+++ blocks/formal_langs/db/access.php
@@ -1,18 +1,18 @@
 <?php
-// This file is part of Formal languages block - https://code.google.com/p/oasychev-moodle-plugins/
+// This file is part of Formal Languages block - https://code.google.com/p/oasychev-moodle-plugins/
 //
-// Formal languages block is free software: you can redistribute it and/or modify
+// Formal Languages block is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Formal languages block is distributed in the hope that it will be useful,
+// Formal Languages block is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Formal Languages block.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Formal languages block caps.
