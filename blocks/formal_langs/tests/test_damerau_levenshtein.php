f3d9c24540cf1c05abd94d08bcd912a7da024c04
maria_birvg <maria_birvg@localhost>
2013-06-02 12:50:22 +0400

    ????????? ???????????????? ???????? ??? ??????? ???????-???????????

--- blocks/formal_langs/tests/test_damerau_levenshtein.php
+++ blocks/formal_langs/tests/test_damerau_levenshtein.php
@@ -14,19 +14,24 @@ require_once($CFG->dirroot.'/blocks/formal_langs/tokens_base.php');
 
 class block_formal_langs_token_base_test extends UnitTestCase {
     function test_damerau_levenshtein_tb() {
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hello', 'ehllo')==1);//transposition
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hello', 'hello')==0);//words identical        
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hello', 'hollo')==1);//substitution
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hello', 'hell')==1);//deletion
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('helo', 'hello')==1);//insertion
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hi lo', 'hello')==2);//word has space
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('HELLO', 'heLlo')==4);//toUpper
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('aaaaa', 'ààààà')==5);//different languages
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('1!2-', '1?6:')==3);//numbers and punctuashion
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('mamma', 'ma')==3);//place of word
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein(' ', '	')==1);//spaces and tab
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('kmix ail', 'ksx aali')==4);//large word
-        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('helllo', 'hlello')==1);//1 transposition
+    $options=new block_formal_langs_comparing_options();
+    $options->usecase=true;
+    $options1=new block_formal_langs_comparing_options();
+    $options1->usecase=false;
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hello', 'ehllo',$options)==1);//transposition
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hello', 'hello',$options)==0);//words identical        
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hello', 'hollo',$options)==1);//substitution
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hello', 'hell',$options)==1);//deletion
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('helo', 'hello',$options)==1);//insertion
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('hi lo', 'hello',$options)==2);//word has space
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('HELLO', 'heLlo',$options)==4);//toUpper
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('HELLO', 'heLlo',$options1)==0);//toUpper
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('aaaaa', 'ààààà',$options)==5);//different languages
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('1!2-', '1?6:',$options)==3);//numbers and punctuashion
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('mamma', 'ma',$options)==3);//place of word
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein(' ', '	',$options)==1);//spaces and tab
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('kmix ail', 'ksx aali',$options)==4);//large word
+        $this->assertTrue(block_formal_langs_token_base::damerau_levenshtein('helllo', 'hlello',$options)==1);//1 transposition
     }
 }
 ?>
