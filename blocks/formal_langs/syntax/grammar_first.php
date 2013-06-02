d6c832f437c740dc699f84523fec4a3d70bea605
oasychev <oasychev@gmail.com>
2013-06-02 16:05:54 +0300

    ? ????? ????? ?????????? ?????? ????????? ??????????? ????????? GPL/Moodle.

--- blocks/formal_langs/syntax/grammar_first.php
+++ blocks/formal_langs/syntax/grammar_first.php
@@ -1,4 +1,18 @@
-<?
+<?php
+// This file is part of Formal Languages block - https://code.google.com/p/oasychev-moodle-plugins/
+//
+// Formal Languages block is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Formal Languages block is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Formal Languages block.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Describes a FIRST(X) function, as defined in
  * A.V. Aho, R. Sethi, J. D. Ullman. Compilers: Principles, Techniques, and Tools
