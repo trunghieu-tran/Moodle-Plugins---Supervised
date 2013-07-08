5ec32bf6d777e6921d7243793852b2493f0e7f23
vaivanov <vaivanov@localhost>
2013-07-08 10:12:33 +0400

    ????????? ????????? ?? ????????? ??????? ???????. ???? ? ??????? ?????? ?? ?????????? ?????? ? code_checker.

---
+++ question/type/preg/tests/matcher_nfa_test.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/question/type/preg/tests/cross_tester.php');
+
+$CFG->qtype_preg_nfa_transition_limit = 1000;
+$CFG->qtype_preg_nfa_state_limit = 1000;
+
+class qtype_preg_nfa_cross_tester extends qtype_preg_cross_tester {
+
+    public function engine_name() {
+        return 'nfa_matcher';
+    }
+}
