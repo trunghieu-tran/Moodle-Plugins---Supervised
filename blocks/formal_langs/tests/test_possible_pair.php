3ce03b2c9e2e561bdcf0dbe6a3bc665a018a4255
maria_birvg <maria_birvg@localhost>
2013-07-10 19:15:44 +0400

    ????????? ????? ?? possible_pair

--- blocks/formal_langs/tests/test_possible_pair.php
+++ blocks/formal_langs/tests/test_possible_pair.php
@@ -28,7 +28,11 @@ class block_formal_langs_tokens_base_test extends UnitTestCase {
         $lexem6=new block_formal_langs_token_base(null, 'type', 'hi llo', null, 6);
         $lexem7=new block_formal_langs_token_base(null, 'type', 'he lo', null, 7);
         $lexem8=new block_formal_langs_token_base(null, 'type', 'HeLLO', null, 8);
-        $lexem9=new block_formal_langs_token_base(null, 'type', 'HE LO', null, 8);
+        $lexem9=new block_formal_langs_token_base(null, 'type', 'HE LO', null, 9);
+
+        $lexem10=new block_formal_langs_token_base(null, 'type', 'misha', null, 10);
+        $lexem11=new block_formal_langs_token_base(null, 'type', 'michi', null, 11);
+        $lexem12=new block_formal_langs_token_base(null, 'type', 'MIsha', null, 12);
 
         $this->assertTrue($lexem->possible_pair($lexem, 2, $options)==0);      // threshold 0.7
         $this->assertTrue($lexem->possible_pair($lexem2, 2, $options)==1);     // threshold 0.7
@@ -40,6 +44,12 @@ class block_formal_langs_tokens_base_test extends UnitTestCase {
         $this->assertTrue($lexem7->possible_pair($lexem, 3, $options)==1);     // threshold 0.5
         $this->assertTrue($lexem->possible_pair($lexem8, 3, $options1)==0);    // threshold 0.5
         $this->assertTrue($lexem->possible_pair($lexem9, 3, $options1)==1);     // threshold 0.5
+
+        $this->assertTrue($lexem10->possible_pair($lexem11, 1, $options)==-1);
+        $this->assertTrue($lexem10->possible_pair($lexem11, 1, $options1)==-1);
+        $this->assertTrue($lexem10->possible_pair($lexem12, 3, $options)==2);
+        $this->assertTrue($lexem10->possible_pair($lexem12, 3, $options1)==0);
+        $this->assertTrue($lexem10->possible_pair($lexem12, 0, $options)==-1);
     }
 }
 ?>
