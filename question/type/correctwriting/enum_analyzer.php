4aa7ae69893df1c0ab7e6bd2fd954d62e7907a19
vadim <vadim@vadim-K53SM>
2013-07-17 13:00:55 +0400

    ????????? ??????????? ?????, ??????????? ?????????? string_pair ? enum_analyzer.

--- question/type/correctwriting/enum_analyzer.php
+++ question/type/correctwriting/enum_analyzer.php
@@ -18,6 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 //Other necessary requires
 require_once($CFG->dirroot.'/question/type/correctwriting/sequence_analyzer.php');
+require_once($CFG->dirroot.'/question/type/correctwriting/string_pair.php');
 
 class  qtype_correctwriting_enum_analyzer {
 
