d6c47b37c44e68b85f89ea01a7c04104c867cbf8
oasychev <oasychev@gmail.com>
2013-07-17 17:22:38 +0300

    ????????? ???????, ????????????, ???????? ?? ??? ??????? ?? ?????????? ?????? ?????????????? ??? ???????????? ??? ???.

--- blocks/formal_langs/tests/test_processed_string.php
+++ blocks/formal_langs/tests/test_processed_string.php
@@ -26,9 +26,10 @@
 
 global $CFG;
 require_once($CFG->dirroot.'/blocks/formal_langs/tokens_base.php');
+require_once($CFG->dirroot.'/blocks/formal_langs/language_simple_english.php');
 
 class block_formal_langs_processed_string_tests extends PHPUnit_Framework_TestCase {
-    //Tests for processed_string single_line_string function.
+    // Tests for processed_string single_line_string function.
     public function test_single_line_string() {
         $string = new block_formal_langs_processed_string(null);
         $string->string = 'abc';
@@ -37,6 +38,28 @@ class block_formal_langs_processed_string_tests extends PHPUnit_Framework_TestCa
         $this->assertFalse($string->single_line_string());
         $string->string = "абвг\nде";
         $this->assertFalse($string->single_line_string());
+    }
+
+    public function test_token_has_equal_to_student() {
+        $lang = new block_formal_langs_language_simple_english();
+
+        $string = $lang->create_from_string('abc Abc cde fff xxx abc str str ptr ptr');
+        $string->set_descriptions_from_array(array(2 => 'first', 3 => 'first', 4 => 'second', 6 => 'only', 8 => 'other1', 9 => 'other2'));
+        // Equal values (no description).
+        $this->assertTrue($string->token_has_equal_to_student(0));
+        $this->assertTrue($string->token_has_equal_to_student(5));
+        // Case-insentitive equal values (no description).
+        $this->assertFalse($string->token_has_equal_to_student(1));
+        // Values equal, but only first token has description.
+        $this->assertFalse($string->token_has_equal_to_student(6));
+        // Values equal, but only second token has description.
+        $this->assertFalse($string->token_has_equal_to_student(7));
+        // Values equal, but description are not.
+        $this->assertFalse($string->token_has_equal_to_student(8));
+        $this->assertFalse($string->token_has_equal_to_student(9));
+        // Descriptions are equal.
+        $this->assertTrue($string->token_has_equal_to_student(2));
+        $this->assertTrue($string->token_has_equal_to_student(3));
     }
 }
  ?>
