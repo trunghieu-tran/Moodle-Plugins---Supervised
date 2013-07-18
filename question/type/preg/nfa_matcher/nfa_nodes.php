b1aa369e5d2d93167fdc33efe8ba3a90b9f2ce07
Elena <Elena@localhost>
2013-07-18 20:29:57 +0400

    ?????????? ???????? ? intersect: 1)?????? ? ????? ???????;2)?????? ??? ????????? ? ????????;3)???????? ???????????? ???????? ? ???????????.

--- question/type/preg/nfa_matcher/nfa_nodes.php
+++ question/type/preg/nfa_matcher/nfa_nodes.php
@@ -122,14 +122,15 @@ class qtype_preg_nfa_transition extends qtype_preg_fa_transition {
      * Returns intersection of transitions.
      *
      * @param other another transition for intersection.
+     * @return result transition.
      */
-    public function intersect_transitions($other) {
+    public function intersect($other) {
         $resulttran = parent::intersect($other);
         if ($resulttran != null)        
-            $resulttran->subpatt_start = array_merge($this->subpatt_start(), $other->subpatt_start());
-            $resulttran->subpatt_end = array_merge($this->subpatt_end(), $other->subpatt_end());
-            $resulttran->subexpr_start = array_merge($this->subexpr_start(), $other->subexpr_start());
-            $resulttran->subexpr_end = array_merge($this->subexpr_end(), $other->subexpr_end());
+            $resulttran->subpatt_start = array_merge($this->subpatt_start, $other->subpatt_start);
+            $resulttran->subpatt_end = array_merge($this->subpatt_end, $other->subpatt_end);
+            $resulttran->subexpr_start = array_merge($this->subexpr_start, $other->subexpr_start);
+            $resulttran->subexpr_end = array_merge($this->subexpr_end, $other->subexpr_end);
             remove_same_elements($resulttran->subpatt_start);
             remove_same_elements($resulttran->subpatt_end);
             remove_same_elements($resulttran->subexpr_start);
