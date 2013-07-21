66c08982760d90d903e4f849de3a4571d2f2149b
Elena <Elena@localhost>
2013-07-21 15:04:13 +0400

    ?????????? ???????????? ???????? intersect ? ?????? nfa_transition. ???????????? ?????? nfa_transition, ?????? fa_transition.

--- question/type/preg/nfa_matcher/nfa_nodes.php
+++ question/type/preg/nfa_matcher/nfa_nodes.php
@@ -126,7 +126,8 @@ class qtype_preg_nfa_transition extends qtype_preg_fa_transition {
      */
     public function intersect($other) {
         $resulttran = parent::intersect($other);
-        if ($resulttran != null)        
+        $resulttran = new qtype_preg_nfa_transition ($resulttran->from, $resulttran->pregleaf, $resulttran->to);
+        if ($resulttran != null) {    
             $resulttran->subpatt_start = array_merge($this->subpatt_start, $other->subpatt_start);
             $resulttran->subpatt_end = array_merge($this->subpatt_end, $other->subpatt_end);
             $resulttran->subexpr_start = array_merge($this->subexpr_start, $other->subexpr_start);
