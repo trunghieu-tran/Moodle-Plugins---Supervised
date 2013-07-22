7f1bcb4292f5a2cbc1de77332a7b0bf944b963cd
oasychev <oasychev@gmail.com>
2013-07-23 00:43:51 +0300

    ????? ?????? ????? ?? ????????? ???????? ? ????.

---
+++ mod/poasassignment/lang/en/poasassignment.php
@@ -0,0 +1,201 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for poasassignment
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package   mod_poasassignment
+ * @copyright 2010 Your Name
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'POAS assignment';
+$string['modulenameplural'] = 'POAS assignments';
+$string['poasassignmentfieldset'] = 'Individual tasks';
+$string['poasassignmentname'] = 'POAS assignment name';
+$string['poasassignment'] = 'POAS assignment';
+$string['poasassignmentintro']='POAS assignment intro';
+$string['poasassignmentadministration']='Administration';
+$string['availabledate']= 'Available from';
+$string['choicedate']= 'Make choice before';
+$string['deadline']= 'Due date';
+$string['preventlate']='Prevent late submissions';
+$string['preventlatechoice']='Prevent late choice';
+$string['randomtasksafterchoicedate']='Set free random task to student after choice date';
+$string['answers']='Answers';
+$string['severalattempts']='Several attempts';
+$string['severalattempts_help']='severalattempts_help';
+$string['notifyteachers']='Notify teachers';
+$string['notifyteachers_help']='notifyteachers_help';
+$string['notifystudents']='Notify students';
+$string['notifystudents_help']='notifystudents_help';
+$string['answerfile']='File upload';
+$string['answerfile_help']='answerfile_help';
+$string['answertext']='Online text';
+$string['answertext_help']='answertext_help';
+$string['maximumsize'] = 'Maximum file size';
+$string['fileextensions']='File extensions';
+$string['fileextensions_help']='fileextensions_help';
+$string['activateindividualtasks']='Activate individual tasks';
+$string['howtochoosetask']='How students choose tasks?';
+$string['randomtask']='Random task';
+$string['parameterchoice']='Using the parameters';
+$string['studentchoice']='Students choose their tasks';
+$string['secondchoice']='Second choice';
+$string['uniqueness']='Task uniqueness';
+$string['nouniqueness']='No uniqueness';
+$string['uniquewithingroup']='Unique within group';
+$string['uniquewithincourse']='Unique within course';
+$string['teacherapproval']='Teacher approval';
+$string['poasassignmentfiles']='Attach file(s) to the assignment';
+$string['attachedpoasassignmentfiles']='File(s) attached to the assignment';
+$string['choicebeforeopen']='Choice date before available date';
+$string['deadlinebeforeopen']='Deadline before available date';
+$string['deadlinebeforechoice']='Deadline before choice date';
+$string['submissionfilesamount']='How much files can be uploaded as an answer?';
+$string['availablefrom']='Available from';
+$string['selectbefore']='Select before';
+$string['tasksfields']="Task's fields";
+$string['tasks'] = 'Tasks';
+$string['grades'] = 'Grades';
+$string['ftype'] = 'Field type';
+$string['taskfieldname'] = 'Field name';
+$string['showintable'] = 'Show in table';
+$string['searchparameter'] ="Parameter of student's search";
+$string['maxvalue'] = 'Maximum value';
+$string['minvalue']='Minimum value';
+$string['view'] = 'View';
+$string['poasassignment:managetasksfields'] = 'poasassignment:managetasksfields';
+$string['taskfieldaddheader'] = 'Add new task field';
+$string['taskfieldeditheader'] = 'Edit task field';
+$string['criterionname'] = 'Criterion name';
+$string['criterionweight'] = 'Criterion weight';
+$string['criterionsource'] = 'Criterion source';
+$string['criterionmanual'] = 'Manual criterion';
+$string['gradesfields'] = "Grade's fields";
+$string['secretfield'] = 'Secret field';
+$string['random'] = 'Random field';
+$string['variants'] = 'Variants';
+$string['errornovariants'] = 'No variants';
+$string['criterions'] = 'Rating criterions';
+$string['criterionideditheader'] = 'Edit criterions';
+$string['criterionidaddheader'] = 'Add new criterion';
+$string['errorindtaskmodeisdisabled'] = 'Invividual task mode is disabled!';
+$string['char']='String';
+$string['text']='Text';
+$string['float']='Fraction';
+$string['int']='Number';
+$string['date']='Date';
+$string['file']='File';
+$string['list']='List of elements';
+$string['taskeditheader']='Edit task';
+$string['taskaddheader']='Add new task';
+$string['taskname']='Task name';
+$string['taskintro']='Task description';
+$string['errorfiledduplicatename'] = 'This name is used by another field';
+$string['addbuttontext']='Add';
+$string['erroroutofrange']='Value out of range';
+$string['sendsubmission'] = 'Send submission';
+$string['answertexteditor']='Type your answer here';
+$string['task'] = 'Task';
+$string['errormaxislessthenmin'] = 'Maximum value is less then minimum value';
+$string['valuemustbe'] = 'Value must be';
+$string['morethen'] = 'more then';
+$string['and'] = 'and';
+$string['lessthen'] = 'less then';
+$string['invalidtaskid'] ='Invalid task id';
+$string['invaliduserid']='Invalid user id';
+$string['taketask'] = 'Take this task';
+$string['taketaskconfirmation'] ='Are you sure you want to take this task?';
+$string['alreadyhavetask'] = 'You have a task already';
+$string['addsubmission'] = 'Add submission';
+$string['youhavetask']= 'Your task is';
+$string['youhavenotask']='You have no task';
+$string['yoursubmissions']='Your submissions';
+$string['yourfiles'] = 'Your files';
+$string['editsubmission'] = 'Edit submission';
+$string['owntasklink']='My task';
+$string['submissions']='Submissions';
+$string['fullname'] = 'Student';
+$string['submission']='Submission';
+$string['errorvariants'] = 'It must be at least two variants';
+$string['variants_help'] = 'Input variants of list separated by enter';
+$string['notask'] = 'No task';
+$string['status']='Status';
+$string['grade']='Grade';
+$string['taskcompleted']='Task completed';
+$string['taskinwork']='Task is in work';
+$string['notask']='No task';
+$string['addgrade']='Add grade';
+$string['gradeeditheader']='Edit grade';
+$string['errornocriterions']='No grade criteions. Add at least 1 criterion';
+$string['normalizedcriterionweight']='Normalized criterion weignt';
+$string['studentsubmission']="Student's submission";
+$string['comment']='Comment';
+$string['editgrade']='Edit grade';
+$string['criteriondescription']='Criterion description';
+$string['feedback']='Feedback';
+$string['commentfiles']='Add comment file';
+$string['studentsubmission']="Student's submission";
+$string['getrandomtask']='Get random task';
+$string['multilist']='Multi-list';
+$string['finalgrade']='Finalize grade';
+$string['invalidfieldid']='Invalid field id';
+$string['deletefieldconfirmation']='Are you sure you want to delete this field?';
+$string['taskhidden']='Task is hidden from students';
+$string['errormustbefloat']='It must be fraction';
+$string['errormustbeint']='It must be integer';
+$string['nosatisfyingtasks']='There is no task that satisfies your requirements';
+$string['inputparameters']='Input preferred parameters of task';
+$string['taskunavailable']='Task is hidden';
+$string['errornoname']='You must define name';
+$string['invalidassigneeid']='Invalid assignee id';
+$string['totalratingis']='Total grade is';
+$string['thismoduleisntopenedyet']="This module isn't opened yet";
+$string['gototassktab']='Click here to take task';
+$string['taskfielddescription']="Field's description";
+$string['submissiondate']='Submission date';
+$string['answerfiles']='Load submission files';
+$string['picture']="User's picture";
+$string['gradedate']='Grade date';
+$string['attemptnumber']='Attempt number';
+$string['oldfeedback']='Outdated';
+$string['outdated']='Outdated';
+$string['view_help']='view_help';
+$string['submissions_help']='submissions_help';
+$string['tasks_help']='tasks_help';
+$string['criterions_help']='criterions_help';
+$string['tasksfields_help']='tasksfields_help';
+$string['of']='of';
+$string['needgrade'] = 'need grade';
+$string['penalty']='Penalty for attempt';
+$string['penalty_help']='penalty_help';
+$string['ago'] = 'ago';
+$string['newattemptbeforegrade']='Consider all attempts as one attempt if none were graded';
+$string['newattemptbeforegrade_help']='newattemptbeforegrade_help';
+$string['draft']='Draft';
+$string['myattempts']='My attempts';
+$string['studentattempts']='Student attempts';
+$string['disablepenalty']='Turn off penalty for this attempt';
+$string['enablepenalty']='Turn on penalty for this attempt';
+$string['range']='Range';
