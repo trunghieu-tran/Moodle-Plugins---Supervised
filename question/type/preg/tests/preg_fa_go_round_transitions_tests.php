0716a04b664ef40492134ad1ccfaa41be04d5adc
Elena <Elena@localhost>
2013-07-23 13:15:15 +0400

    ?????????? ????? ??? go_round_transitions. 1)?????????? ????? ???????????? ?? ????????;2)???????? ????????? ????????? ?-?? ??????? ?????????;3) ????????? ?????? ????? ????????.

--- question/type/preg/tests/preg_fa_go_round_transitions_tests.php
+++ question/type/preg/tests/preg_fa_go_round_transitions_tests.php
@@ -9,80 +9,74 @@ require_once($CFG->dirroot . '/question/type/preg/nfa_matcher/nfa_nodes.php');
 class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase {
 
     public function test_one_uncapturing_transition() {
-        $dotdescription = 'digraph example 
-                            {
-                                0;
-                                4;
-                                0->1[label="[a-z]"];
-                                1->2[label="[0-9]"];
-                                2->3[label="[^]"];
-                                0->3[label="[xy]"];
-                                3->4[label="[a-c]"];
-                            }';
-        $dotresult = 'digraph example 
-                    {
+        $dotdescription = 'digraph example {
                         0;
                         4;
                         0->1[label="[a-z]"];
                         1->2[label="[0-9]"];
+                        2->3[label="[^]"];
                         0->3[label="[xy]"];
                         3->4[label="[a-c]"];
-                        2->4[label="[^a-c]"];
                     }';
+        $dotresult = 'digraph example {
+                    0;
+                    4;
+                    0->1[label="[a-z]"];
+                    1->2[label="[0-9]"];
+                    0->3[label="[xy]"];
+                    3->4[label="[a-c]"];
+                    2->4[label="[^a-c]"];
+                }';
 
         $input = new qtype_preg_nfa(0, 0, 0, array());
         $input->read_fa($dotdescription);
-
-        $del = $input->states[2]->outtransitions[0];
+        $realnumbers = $input->get_state_numbers();
+        $number = array_search('2', $realnumbers);
+        $outtransitions = $input->get_state_outtransitions($number);
+        $number = array_search('3', $realnumbers);
+        $del = $outtransitions[$number];
         $input->go_round_transitions($del);
-
-        $result = $input->write_fa();
-        $search = '
-                    ';
-        $replace = '\n';
-        $dotresult = str_replace($search, $replace, $dotresult);
-        $this->assertEquals($result, $dotresult, 'Result automata is not equal to expected');
+        $result = new qtype_preg_nfa(0, 0, 0, array());
+        $result->read_fa($dotresult);
+        var_dump($input->get_state_outtransitions(3)[1]);
+        $this->assertEquals($input, $result, 'Result automata is not equal to expected');
     }
 
     public function test_unsuccessful_merging_last_state_with_tag() {
-        $dotdescription = 'digraph example 
-                            {
-                                0;
-                                3;
-                                0->1[label="[a-z]"];
-                                1->2[label="[0-9]"];
-                                0->2[label="[a-c]"];
-                                2->3[label="[()]"];
-                                0->3[label="[xy]"];
-                            }';
-        $dotresult = 'digraph example 
-                    {
+        $dotdescription = 'digraph example {
+                            0;
+                            3;
+                            0->1[label="[a-z]"];
+                            1->2[label="[0-9]"];
+                            0->2[label="[a-c]"];
+                            2->3[label="[(/)/]"];
+                            0->3[label="[xy]"];
+                        }';
+        $dotresult = 'digraph example {
                         0;
                         3;
                         0->1[label="[a-z]"];
                         1->2[label="[0-9]"];
                         0->2[label="[a-c]"];
-                        2->3[label="[()]"];
+                        2->3[label="[(/)/]"];
                         0->3[label="[xy]"];
                     }';
 
         $input = new qtype_preg_nfa(0, 0, 0, array());
         $input->read_fa($dotdescription);
-
-        $del = $input->states[2]->outtransitions[0];
+        $realnumbers = $input->get_state_numbers();
+        $number = array_search('2', $realnumbers);
+        $outtransitions = $input->get_state_outtransitions($number);
+        $number = array_search('3', $realnumbers);
+        $del = $outtransitions[$number];
         $input->go_round_transitions($del);
-
-        $result = $input->write_fa();
-        $search = '
-                    ';
-        $replace = '\n';
-        $dotresult = str_replace($search, $replace, $dotresult);
-        $this->assertEquals($result, $dotresult, 'Result automata is not equal to expected');
+        $result = new qtype_preg_nfa(0, 0, 0, array());
+        $result->read_fa($dotresult);
+        $this->assertEquals($input, $result, 'Result automata is not equal to expected');
     }
 
     public function test_unsuccessful_merging_last_state_with_assert() {
-        $dotdescription = 'digraph example 
-                            {
+        $dotdescription = 'digraph example {
                                 0;
                                 3;
                                 0->1[label="[a-z]"];
@@ -91,8 +85,7 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
                                 2->3[label="[\\Z]"];
                                 0->3[label="[xy]"];
                             }';
-        $dotresult = 'digraph example 
-                    {
+        $dotresult = 'digraph example {
                         0;
                         3;
                         0->1[label="[a-z]"];
@@ -104,21 +97,20 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
 
         $input = new qtype_preg_nfa(0, 0, 0, array());
         $input->read_fa($dotdescription);
-
-        $del = $input->states[2]->outtransitions[0];
+        $realnumbers = $input->get_state_numbers();
+        $number = array_search('2', $realnumbers);
+        $outtransitions = $input->get_state_outtransitions($number);
+        $number = array_search('3', $realnumbers);
+        $del = $outtransitions[$number];
         $input->go_round_transitions($del);
-
-        $result = $input->write_fa();
-        $search = '
-                    ';
-        $replace = '\n';
-        $dotresult = str_replace($search, $replace, $dotresult);
-        $this->assertEquals($result, $dotresult, 'Result automata is not equal to expected');
+        $result = new qtype_preg_nfa(0, 0, 0, array());
+        $result->read_fa($dotresult);
+        var_dump($input);
+        $this->assertEquals($input, $result, 'Result automata is not equal to expected');
     }
 
     public function test_merging_last_state() {
-        $dotdescription = 'digraph example 
-                            {
+        $dotdescription = 'digraph example {
                                 0;
                                 3;
                                 0->1[label="[a-z]"];
@@ -127,8 +119,7 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
                                 2->3[label="[]"];
                                 0->3[label="[xy]"];
                             }';
-        $dotresult = 'digraph example 
-                    {
+        $dotresult = 'digraph example {
                         0;
                         3;
                         0->1[label="[a-z]"];
@@ -140,21 +131,19 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
 
         $input = new qtype_preg_nfa(0, 0, 0, array());
         $input->read_fa($dotdescription);
-
-        $del = $input->states[2]->outtransitions[0];
+        $realnumbers = $input->get_state_numbers();
+        $number = array_search('2', $realnumbers);
+        $outtransitions = $input->get_state_outtransitions($number);
+        $number = array_search('3', $realnumbers);
+        $del = $outtransitions[$number];
         $input->go_round_transitions($del);
-
-        $result = $input->write_fa();
-        $search = '
-                    ';
-        $replace = '\n';
-        $dotresult = str_replace($search, $replace, $dotresult);
-        $this->assertEquals($result, $dotresult, 'Result automata is not equal to expected');
+        $result = new qtype_preg_nfa(0, 0, 0, array());
+        $result->read_fa($dotresult);
+        $this->assertEquals($input, $result, 'Result automata is not equal to expected');
     }
 
     public function test_capturing_transitions_between_states() {
-        $dotdescription = 'digraph example 
-                            {
+        $dotdescription = 'digraph example {
                                 0;
                                 4;
                                 0->1[label="[a-z]"];
@@ -164,8 +153,7 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
                                 0->3[label="[xy]"];
                                 3->4[label="[a-c]"];
                             }';
-        $dotresult = 'digraph example 
-                    {
+        $dotresult = 'digraph example {
                         0;
                         4;
                         0->1[label="[a-z]"];
@@ -178,21 +166,19 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
 
         $input = new qtype_preg_nfa(0, 0, 0, array());
         $input->read_fa($dotdescription);
-
-        $del = $input->states[2]->outtransitions[0];
+        $realnumbers = $input->get_state_numbers();
+        $number = array_search('2', $realnumbers);
+        $outtransitions = $input->get_state_outtransitions($number);
+        $number = array_search('3', $realnumbers);
+        $del = $outtransitions[$number];
         $input->go_round_transitions($del);
-
-        $result = $input->write_fa();
-        $search = '
-                    ';
-        $replace = '\n';
-        $dotresult = str_replace($search, $replace, $dotresult);
-        $this->assertEquals($result, $dotresult, 'Result automata is not equal to expected');
+        $result = new qtype_preg_nfa(0, 0, 0, array());
+        $result->read_fa($dotresult);
+        $this->assertEquals($input, $result, 'Result automata is not equal to expected');
     }
 
     public function test_several_transitions() {
-        $dotdescription = 'digraph example 
-                            {
+        $dotdescription = 'digraph example {
                                 0;
                                 5;
                                 0->1[label="[a-z]"];
@@ -202,8 +188,7 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
                                 3->4[label="[a-c]"];
                                 3->5[label="[01]"];
                             }';
-        $dotresult = 'digraph example 
-                    {
+        $dotresult = 'digraph example {
                         0;
                         5;
                         0->1[label="[a-z]"];
@@ -217,21 +202,19 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
 
         $input = new qtype_preg_nfa(0, 0, 0, array());
         $input->read_fa($dotdescription);
-
-        $del = $input->states[2]->outtransitions[0];
+        $realnumbers = $input->get_state_numbers();
+        $number = array_search('2', $realnumbers);
+        $outtransitions = $input->get_state_outtransitions($number);
+        $number = array_search('3', $realnumbers);
+        $del = $outtransitions[$number];
         $input->go_round_transitions($del);
-
-        $result = $input->write_fa();
-        $search = '
-                    ';
-        $replace = '\n';
-        $dotresult = str_replace($search, $replace, $dotresult);
-        $this->assertEquals($result, $dotresult, 'Result automata is not equal to expected');
+        $result = new qtype_preg_nfa(0, 0, 0, array());
+        $result->read_fa($dotresult);
+        $this->assertEquals($input, $result, 'Result automata is not equal to expected');
     }
 
     public function test_unsuccsessful_merging_state_for_intersection() {
-        $dotdescription = 'digraph example 
-                            {
+        $dotdescription = 'digraph example {
                                 0;
                                 3;
                                 0->1[label="[a-z]"];
@@ -239,8 +222,7 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
                                 2->3[label="[]"];
                                 0->3[label="[xy]"];
                             }';
-        $dotresult = 'digraph example 
-                    {
+        $dotresult = 'digraph example {
                         0;
                         3;
                         0->1[label="[a-z]"];
@@ -251,15 +233,14 @@ class qtype_preg_fa_go_round_transitions_test extends PHPUnit_Framework_TestCase
 
         $input = new qtype_preg_nfa(0, 0, 0, array());
         $input->read_fa($dotdescription);
-
-        $del = $input->states[2]->outtransitions[0];
+        $realnumbers = $input->get_state_numbers();
+        $number = array_search('2', $realnumbers);
+        $outtransitions = $input->get_state_outtransitions($number);
+        $number = array_search('3', $realnumbers);
+        $del = $outtransitions[$number];
         $input->go_round_transitions($del);
-
-        $result = $input->write_fa();
-        $search = '
-                    ';
-        $replace = '\n';
-        $dotresult = str_replace($search, $replace, $dotresult);
-        $this->assertEquals($result, $dotresult, 'Result automata is not equal to expected');
+        $result = new qtype_preg_nfa(0, 0, 0, array());
+        $result->read_fa($dotresult);
+        $this->assertEquals($input, $result, 'Result automata is not equal to expected');
     }
 }
