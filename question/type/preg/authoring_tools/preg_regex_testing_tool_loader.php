1f8bc27459647d479e394f5bd01cf2a79c176037
gvterechov <gvterechov@localhost>
2013-07-24 14:15:32 +0400

    ??????? ??? ???????? ???????????? ??????????? ?????????.

--- question/type/preg/authoring_tools/preg_regex_testing_tool_loader.php
+++ question/type/preg/authoring_tools/preg_regex_testing_tool_loader.php
@@ -20,12 +20,12 @@ function qtype_preg_get_json_array() {
     global $CFG;
     $json_array = array();
     $regextext = optional_param('regex', '', PARAM_RAW);
-	$answer = optional_param('answer', '', PARAM_RAW);
-	$matcher = optional_param('matcher', '', PARAM_RAW);
-	
-	$regex_testing_tool = new qtype_preg_regex_testing_tool($regextext, array('answer' => $answer), $matcher);
-	$regex_testing_tool->generate_json($json_array);
-	
+    $answers = optional_param('answer', '', PARAM_RAW);
+    $matcher = optional_param('matcher', '', PARAM_RAW);
+
+    $regex_testing_tool = new qtype_preg_regex_testing_tool($regextext, $answers, $matcher);
+    $regex_testing_tool->generate_json($json_array);
+
     return $json_array;
 }
 
