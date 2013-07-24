45580ea2c5cb0ef09cfa40f3f86ebc0c00354607
gvterechov <gvterechov@localhost>
2013-07-25 00:22:52 +0400

    ????????? ????????? ?????????? ?? ????? ?????????????? ??????? ??? ???????????? ?????????? ?????????.

--- question/type/preg/authoring_tools/preg_regex_testing_tool_loader.php
+++ question/type/preg/authoring_tools/preg_regex_testing_tool_loader.php
@@ -22,8 +22,22 @@ function qtype_preg_get_json_array() {
     $regextext = optional_param('regex', '', PARAM_RAW);
     $answers = optional_param('answer', '', PARAM_RAW);
     $matcher = optional_param('matcher', '', PARAM_RAW);
-
-    $regex_testing_tool = new qtype_preg_regex_testing_tool($regextext, $answers, $matcher);
+    $usecase = optional_param('usecase', '', PARAM_INT);
+    $exactmatch = optional_param('exactmatch', '', PARAM_INT);
+    
+    if($usecase == 1){
+        $usecase = true;
+    } else {
+        $usecase = false;
+    }
+    
+    if($exactmatch == 1){
+        $exactmatch = true;
+    } else {
+        $exactmatch = false;
+    }
+    
+    $regex_testing_tool = new qtype_preg_regex_testing_tool($regextext, $answers, $matcher, $usecase, $exactmatch);
     $regex_testing_tool->generate_json($json_array);
 
     return $json_array;
