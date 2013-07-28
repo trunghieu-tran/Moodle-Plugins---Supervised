c8495905ce98ad6431c4bbf186db859aceb8a040
Elena <Elena@localhost>
2013-07-28 15:54:04 +0400

    ?????? ???? ?????????, ??????? ????? ???????????? ?? nfa_transitions, ????? ?? ???????? ???? ??? ???????????.

---
+++ blocks/formal_langs/lang/en/block_formal_langs.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * A language strings file of block
+ *
+ * @package    formal_langs
+ * @copyright  2012 Sychev Oleg
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+$string['clanguagemulticharliteral'] = 'There is multicharacter literal at {$a->line}:{$a->col}';
+$string['clanguageunknownsymbol'] = 'There is unknown symbol "{$a->value}" at {$a->line}:{$a->col}';
+$string['clanguageunmatchedquote'] = 'There is unmatched quote at {$a->line}:{$a->col}';
+$string['clanguageunmatchedsquote'] = 'There is unmatched single quote at {$a->line}:{$a->col}';
+$string['lang_c_language'] = 'C programming language';
+$string['lang_c_language_help'] = 'C programmging languge. Scanning only.';
+$string['lang_simple_english'] = 'English';
+$string['lang_simple_english_help'] = 'English Language with support for words, numbers and punctuation marks. Scanning only.';
+$string['lexical_error_message'] = 'Cannot match input {$a->symbol} near {$a->line}:{$a->position} ';
+$string['pluginname'] = 'Formal languages block';
