8188566e04d049366ec468b7d10f9a69371f5a75
mamontov dp <mamontov.dp@gmail.com>
2013-09-18 16:33:39 +0400

    ?????????? ??? ??????? ??????? ?????????? ??????, ????????????????? codestyle.

--- blocks/formal_langs/db/install.php
+++ blocks/formal_langs/db/install.php
@@ -18,7 +18,7 @@ function xmldb_block_formal_langs_install() {
     global $DB;
 
     $lang = new stdClass();
-    $lang->ui_name = 'Simple english';
+    $lang->uiname = 'Simple english';
     $lang->description = 'Simple english language definition';
     $lang->name = 'simple_english';
     $lang->scanrules = null;
@@ -30,7 +30,7 @@ function xmldb_block_formal_langs_install() {
     $DB->insert_record('block_formal_langs',$lang);
 
     $lang = new stdClass();
-    $lang->ui_name = 'C programming language';
+    $lang->uiname = 'C programming language';
     $lang->description = 'C language, with only lexer. One-line comments not supported';
     $lang->name = 'c_language';
     $lang->scanrules = null;
@@ -43,7 +43,7 @@ function xmldb_block_formal_langs_install() {
 
 
     $lang = new stdClass();
-    $lang->ui_name = 'C++ programming language';
+    $lang->uiname = 'C++ programming language';
     $lang->description = 'C++ language, with only lexer. One-line comments not supported';
     $lang->name = 'cpp_language';
     $lang->scanrules = null;
@@ -54,7 +54,7 @@ function xmldb_block_formal_langs_install() {
     $DB->insert_record('block_formal_langs',$lang);
 
     $lang = new stdClass();
-    $lang->ui_name = 'C formatting string rules';
+    $lang->uiname = 'C formatting string rules';
     $lang->description = 'C formatting string rules, as used in printf';
     $lang->name = 'printf_language';
     $lang->scanrules = null;
