79f3131ea1be6ba47eae4b35a4b68b9f6d7a67ca
AndreyU <AndreyU@localhost>
2013-10-01 23:29:05 +0400

    Plugin's code follows the Moodle coding guidelines (http://docs.moodle.org/dev/Coding_style).

--- blocks/supervised/block_supervised.php
+++ blocks/supervised/block_supervised.php
@@ -1,18 +1,33 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 class block_supervised extends block_base {
     public function init() {
         $this->title = get_string('supervised', 'block_supervised');
     }
-	
+
     public function get_content() {
-    if ($this->content !== null) {
-      return $this->content;
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $this->content         =  new stdClass;
+        $this->content->text   = 'The content of supervised block!';
+        $this->content->footer = 'Footer here...';
+
+        return $this->content;
     }
- 
-    $this->content         =  new stdClass;
-    $this->content->text   = 'The content of supervised block!';
-    $this->content->footer = 'Footer here...';
- 
-    return $this->content;
-  }
 }
