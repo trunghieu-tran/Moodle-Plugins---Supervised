285ec40e9c46330643b51bbd32ab34ecd65cabbb
AndreyU <AndreyU@localhost>
2013-10-01 22:55:05 +0400

    Updated database scheme.

--- blocks/supervised/version.php
+++ blocks/supervised/version.php
@@ -12,7 +12,6 @@
  
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013091801;       			// The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013051400;    			// Requires this Moodle version
+$plugin->version   = 2013091802;       		// The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013051400;    		// Requires this Moodle version
 $plugin->component = 'block_supervised';	// Full name of the plugin (used for diagnostics)
-
