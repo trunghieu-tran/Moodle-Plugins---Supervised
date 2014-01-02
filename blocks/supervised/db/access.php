01c91b980747cbbe511d1cde678e3481716b5f95
AndreyU <AndreyU@localhost>
2014-01-02 22:55:58 +0400

    Added addinstance capability.

--- blocks/supervised/db/access.php
+++ blocks/supervised/db/access.php
@@ -16,6 +16,19 @@
 
 $capabilities = array(
 
+    'block/supervised:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+
     'block/supervised:besupervised' => array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_BLOCK,
