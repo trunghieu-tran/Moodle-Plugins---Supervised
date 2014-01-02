d814c3ef1b7ae256a14d6328582cea7c28f3ea7b
AndreyU <AndreyU@localhost>
2014-01-02 21:00:33 +0400

    Added capability check for viewing lesson types.

--- blocks/supervised/lessontypes/view.php
+++ blocks/supervised/lessontypes/view.php
@@ -16,7 +16,7 @@ if ($site->id == $course->id) {
 }
 
 require_login($course);
-// TODO Capabilities
+require_capability('block/supervised:editlessontypes', $PAGE->context);
 $PAGE->set_url('/blocks/supervised/lessontypes/view.php', array('courseid' => $courseid));
 $PAGE->set_pagelayout('standard');
 $PAGE->set_title(get_string('lessontypespagetitle', 'block_supervised'));
