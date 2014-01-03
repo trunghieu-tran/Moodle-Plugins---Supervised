0999c1531eba6e3f2866e8b665dc7388460cb8ff
AndreyU <AndreyU@localhost>
2014-01-03 21:41:03 +0400

    Added backup/restore for supervisedcheck quiz access subplugin.

--- blocks/supervised/backup/moodle2/backup_supervised_block_task.class.php
+++ blocks/supervised/backup/moodle2/backup_supervised_block_task.class.php
@@ -18,7 +18,7 @@
 /**
  * @package moodlecore
  * @subpackage backup-moodle2
- * @copyright 2003 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @copyright
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
