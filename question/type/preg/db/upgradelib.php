db1c4ccc083c67ee061c13e6a89f2d274d66c365
Pahomov Dmitry <Pahomov Dmitry@localhost>
2014-01-03 22:33:28 +0300

    ??????? ???????? ????????

---
+++ question/type/preg/db/upgradelib.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Preg question type - https://code.google.com/p/oasychev-moodle-plugins/
+//
+// Preg question type is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Preg question type upgrade code.
+ *
+ * @package    qtype_preg
+ * @copyright  2012 Oleg Sychev, Volgograd State Technical University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class for converting attempt data for preg questions when upgrading
+ * attempts to the new question engine.
+ * This class is used by the code in question/engine/upgrade/upgradelib.php.
+ */
+class qtype_preg_qe2_attempt_updater extends question_qtype_attempt_updater {
+    public function right_answer() {
+        return $this->question->options->correctanswer;
+    }
+
+    public function was_answered($state) {
+        return !empty($state->answer);
+    }
+
+    public function response_summary($state) {
+        if (!empty($state->answer)) {
+            return $state->answer;
+        } else {
+            return null;
+        }
+    }
+
+    public function set_first_step_data_elements($state, &$data) {
+    }
+
+    public function supply_missing_first_step_data(&$data) {
+    }
+
+    public function set_data_elements_for_step($state, &$data) {
+        if (!empty($state->answer)) {
+            $data['answer'] = $state->answer;
+        }
+    }
+}
