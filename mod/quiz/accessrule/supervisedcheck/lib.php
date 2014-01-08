679ce3fb64de6911ba40dccc7b155229133e2fc1
AndreyU <AndreyU@localhost>
2014-01-08 22:17:57 +0400

    Added PHPdoc for supervised access rules.

--- mod/quiz/accessrule/supervisedcheck/lib.php
+++ mod/quiz/accessrule/supervisedcheck/lib.php
@@ -1,14 +1,27 @@
 <?php
 
+/**
+ * Removes all supervised access rules for nonexistent quizzes
+ */
 function supervisedcheck_cleanup(){
-    global $DB;
     // TODO Remove all supervised access rules for nonexistent quizzes.
     echo('supervisedcheck_cleanup...</br>');
 }
+
+/**
+ * Course deleted (event). Remove all out-of-date quiz access rules.
+ *
+ * @param $course int course id
+ */
 function event_handler_course_deleted($course){
     supervisedcheck_cleanup();
 }
 
+/**
+ * Course deleted (event). Remove all out-of-date quiz access rules.
+ *
+ * @param $course int course id
+ */
 function event_handler_course_content_removed($course){
     supervisedcheck_cleanup();
 }
