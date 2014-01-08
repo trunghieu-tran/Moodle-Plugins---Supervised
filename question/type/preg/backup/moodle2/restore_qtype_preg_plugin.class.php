dec6e2c40515f52aa8612721e76c395a1c4f2463
Valeriy Streltsov <Valeriy Streltsov@localhost>
2014-01-08 18:27:58 +0000

    ???????? ???????????? ??? ?????? ?????-???????

--- question/type/preg/backup/moodle2/restore_qtype_preg_plugin.class.php
+++ question/type/preg/backup/moodle2/restore_qtype_preg_plugin.class.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
-require_once($CFG->dirroot . '/question/type/poasquestion/backup/moodle2/restore_poasquestion_preg_plugin.class.php');
+require_once($CFG->dirroot . '/question/type/poasquestion/backup/moodle2/restore_poasquestion_plugin.class.php');
 
 class restore_qtype_preg_plugin extends restore_qtype_poasquestion_plugin {
 
