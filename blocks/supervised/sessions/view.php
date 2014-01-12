f0961f91a3df417455b0e1603f2877c6fb3b74a3
AndreyU <AndreyU@localhost>
2014-01-12 18:12:21 +0400

    Added CSS settings for course selector.

--- blocks/supervised/sessions/view.php
+++ blocks/supervised/sessions/view.php
@@ -130,6 +130,6 @@ function print_courses_selector($courseid, $course, $perpage, $from, $to, $class
     }
 
     $select = new url_select($urls, $active, null, 'supervisedblock_selectcourseform');
-    $select->set_label(get_string('course', 'block_supervised'));
+    $select->set_label(get_string('course', 'block_supervised'), array("id"=>"supervisedblock_courselabel"));
     echo $OUTPUT->render($select);
 }
