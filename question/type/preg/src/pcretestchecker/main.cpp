d5437354e28965684af7cdad59a5464620c8d203
Valeriy Streltsov <Valeriy Streltsov@localhost>
2014-01-14 00:26:28 +0000

    ????????? ??????????? ?????? ?? Qt, ?????? ?? ????? ???????? ? ????????

--- question/type/preg/src/pcretestchecker/main.cpp
+++ question/type/preg/src/pcretestchecker/main.cpp
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include <string.h>
+#include <QCoreApplication>
+#include <QTextStream>
 #include "pcre.h"
-#include <iostream>
 
 #define max(a, b) (a > b ? a : b)
+
+QTextStream qin(stdin);
+QTextStream qout(stdout);
 
 void reset_ovector(int ovector[]) {
     for (int j = 0; j < 1024; j++) {
@@ -11,7 +13,7 @@ void reset_ovector(int ovector[]) {
     }
 }
 
-void compare_match(const char * methodname, const char * regex, int options, const char * string, const int oexpected[], const int oobtained[], int count_expected, int count_obtained, bool partial)
+bool compare_match(QString methodname, QString regex, int options, QString string, const int oexpected[], const int oobtained[], int count_expected, int count_obtained, bool partial)
 {
     if (partial) {
         count_expected = 1;
@@ -24,95 +26,109 @@ void compare_match(const char * methodname, const char * regex, int options, con
     }
 
     if (passed) {
-        return;
+        return true;
     }
 
-    std::cout << "==================================================\n";
+    qout << "==================================================\n";
     if (partial) {
-        std::cout << "failed partial match comparison\n";
+        qout << "failed partial match comparison\n";
     } else {
-        std::cout << "failed full match comparison\n";
+        qout << "failed full match comparison\n";
     }
 
-    std::cout << "method name: " << methodname << "\n";
-    std::cout << "regex: " << regex << "\n";
-    std::cout << "options: " << options << "\n";
-    std::cout << "string: " << string << "\n";
-    std::cout << "expected " << count_expected << " subexpression(s):\n";
+    qout << "method name: " << methodname << "\n";
+    qout << "regex: " << regex << "\n";
+    qout << "options: " << options << "\n";
+    qout << "string: " << string << "\n";
+    qout << "expected " << count_expected << " subexpression(s):\n";
     for (int i = 0; i < 2 * count_expected; i += 2) {
         if (oexpected[i] != -1) {
-            std::cout << i / 2 << ": (" << oexpected[i] << "," << oexpected[i + 1] - oexpected[i] << ") ";
+            qout << i / 2 << ": (" << oexpected[i] << "," << oexpected[i + 1] - oexpected[i] << ") ";
         }
     }
-    std::cout << "\n";
-    std::cout << "obtained " << count_obtained << " subexpression(s):\n";
+    qout << "\n";
+    qout << "obtained " << count_obtained << " subexpression(s):\n";
     for (int i = 0; i < 2 * count_obtained; i += 2) {
         if (oobtained[i] != -1) {
-            std::cout << i / 2 << ": (" << oobtained[i] << "," << oobtained[i + 1] - oobtained[i] << ") ";
+            qout << i / 2 << ": (" << oobtained[i] << "," << oobtained[i + 1] - oobtained[i] << ") ";
         }
     }
-    std::cout << "\n";
+    qout << "\n";
+    return false;
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    char tmp;
+    qin.setCodec("UTF-8");
+    qin.autoDetectUnicode();
+
+    qout.setCodec("UTF-8");
+    qout.autoDetectUnicode();
+
+    int failed = 0;
+    int passed = 0;
 
     // Test method name length and method name itself
     int methodname_length;
-    char methodname[1024 * 1024];
 
-    std::cin >> methodname_length;
-    std::cin.get(tmp);
+    qin >> methodname_length;
+    qin.read(1);
 
-    std::cin.get(methodname, methodname_length + 1, EOF);
-    std::cin.get(tmp);
+    QString methodname = qin.read(methodname_length);
+    qin.read(1);
+
+    //qout << "methodname: " << methodname << "\n";
 
     // Regex length and regex itself
     int regex_length;
-    char regex[1024 * 1024];
 
-    std::cin >> regex_length;
-    std::cin.get(tmp);
+    qin >> regex_length;
+    qin.read(1);
 
-    std::cin.get(regex, regex_length + 1, EOF);
-    std::cin.get(tmp);
+    //qout << "regex_length: " << regex_length << "\n";
+
+    QString regex = qin.read(regex_length);
+    qin.read(1);
+
+    //qout << "regex: " << regex << "\n";
 
     // Regex options
     int regex_options;
 
-    std::cin >> regex_options;
-    std::cin.get(tmp);
+    qin >> regex_options;
+    qin.read(1);
+    regex_options = regex_options | PCRE_UTF8;
 
     // Compile pattern
     const char * error;
     int erroffset;
-    pcre * re = pcre_compile(regex, regex_options, &error, &erroffset, NULL);
+    pcre * re = pcre_compile(regex.toStdString().data(), regex_options, &error, &erroffset, NULL);
     if (!re) {
-        std::cout << "compilation failed: " << error << "\n";
-        std::cout << "regex: " << regex << "\n";
-        std::cout << "options: " << regex_options << "\n";
+        qout << "compilation failed: " << error << "\n";
+        qout << "method name: " << methodname << "\n";
+        qout << "regex: " << regex << "\n";
+        qout << "options: " << regex_options << "\n";
         return 1;
     }
 
     // Number of tests and tests themselves
     int tests_count;
 
-    std::cin >> tests_count;
-    std::cin.get(tmp);
+    qin >> tests_count;
+    qin.read(1);
 
     for (int i = 0; i < tests_count; i++) {
         // String length and string itself
         int string_length;
-        char string[1024 * 1024];
 
-        std::cin >> string_length;
-        std::cin.get(tmp);
+        qin >> string_length;
+        qin.read(1);
 
-        if (string_length > 0) {
-            std::cin.get(string, string_length + 1, EOF);
-        }
-        std::cin.get(tmp);
+        QString string = qin.read(string_length);
+        qin.read(1);
+        QByteArray stringBytes = string.toUtf8();
+
+        //qout << "string: " << string << "\n";
 
         // Number of subexpressions and offsets
         int subexprs_count;
@@ -120,17 +136,34 @@ int main (int argc, char *argv[])
         int subexprs[1024];
         reset_ovector(subexprs);
 
-        std::cin >> subexprs_count;
-        std::cin.get(tmp);
+        qin >> subexprs_count;
+        qin.read(1);
 
         for (int j = 0; j < subexprs_count; j++) {
             int _subexpr, _index, _length;
-            std::cin >> _subexpr;
-            std::cin.get(tmp);
-            std::cin >> _index;
-            std::cin.get(tmp);
-            std::cin >> _length;
-            std::cin.get(tmp);
+            qin >> _subexpr;
+            qin.read(1);
+            qin >> _index;
+            qin.read(1);
+            qin >> _length;
+            qin.read(1);
+
+            // Convert utf8 offsets to plain char* offsets
+            if (string.size() != stringBytes.size()) {
+                int _newIndex;
+                int _newLength;
+                if (_index > 0) {
+                    QString tmp = string.left(_index);
+                    _newIndex = tmp.toUtf8().size();
+                }
+                if (_index + _length > 0) {
+                    QString tmp = string.mid(_index, _length);
+                    _newLength = tmp.toUtf8().size();
+                }
+                _index = _newIndex;
+                _length = _newLength;
+            }
+
             subexprs[2 * _subexpr] = _index;
             subexprs[2 * _subexpr + 1] = _index + _length;
             subexpr_max = max(subexpr_max, _subexpr);
@@ -142,14 +175,14 @@ int main (int argc, char *argv[])
         reset_ovector(ovector);
         int match_options = PCRE_PARTIAL;
         int longest_count = 0;
-        int leftborder = string_length == 0
-                       ? 0
-                       : 1;
+        int leftborder = string_length == 0 ? 0 : 1;
 
         // Here be a terrible code beacuse PCRE can't find partial matches if the string contains some unmatched suffix
         for (int len = string_length; len >= leftborder; len--) {
             int cur_ovector[1024];
-            count = pcre_exec(re, NULL, string, len, 0, match_options, cur_ovector, 1024);
+            QString choppedString = string.left(len);
+            int bytesCount = choppedString.toUtf8().size();
+            count = pcre_exec(re, NULL, choppedString.toStdString().data(), bytesCount, 0, match_options, cur_ovector, 1024);
 
             if (count != PCRE_ERROR_PARTIAL && count <= 0) {
                 // Match not found
@@ -167,18 +200,31 @@ int main (int argc, char *argv[])
             }
         }
 
+        bool current_passed;
+
         if (longest_count == PCRE_ERROR_PARTIAL) {
             // Partial match
-            compare_match(methodname, regex, regex_options, string, subexprs, ovector, subexpr_max + 1, longest_count, true);
+            current_passed = compare_match(methodname, regex, regex_options, string, subexprs, ovector, subexpr_max + 1, longest_count, true);
         } else if (longest_count > 0) {
             // Full match
-            compare_match(methodname, regex, regex_options, string, subexprs, ovector, subexpr_max + 1, longest_count, false);
+            current_passed = compare_match(methodname, regex, regex_options, string, subexprs, ovector, subexpr_max + 1, longest_count, false);
         } else {
             // No match
-            compare_match(methodname, regex, regex_options, string, subexprs, ovector, subexpr_max + 1, 1, false);
+            current_passed = compare_match(methodname, regex, regex_options, string, subexprs, ovector, subexpr_max + 1, 1, false);
         }
-
+        if (current_passed) {
+            passed++;
+        } else {
+            if (string.size() != stringBytes.size()) {
+                qout << "(utf8 offsets were converted to plain char* offsets\n";
+            }
+            failed++;
+        }
     }
+
+    //qout << "==================================================\n";
+    //qout << "passed: " << passed<< "\n";
+    //qout << "failed: " << failed << "\n";
 
     return 0;
 }
