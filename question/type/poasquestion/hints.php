b8e1745e7ccecc0fdefcc9f742e693edf7a90921
oasychev <oasychev@gmail.com>
2014-01-18 02:15:59 +0300

    ????????? ?????????? ? ?????????? - ??????? ?????????? ? hintkeys ?????? ????? ??????.

--- question/type/poasquestion/hints.php
+++ question/type/poasquestion/hints.php
@@ -88,11 +88,18 @@ abstract class qtype_specific_hint {
      *  Choosen multiple instance hint allows several hint buttons, from which the user
      *  (either teacher or student, depending on behaviour) could choose one they want. 
      *  Example is hint, that would show how you should place misplaced lexem in correct writing question type.
+     *
+     *  In the hint options for interactive mode (save_hint_options function) available choosen multiple instance
+     *  hint keys should end on '_' character, behaviour will add all instances returned by available_specific_hints
+     *  starting from the prefixes from save_hint_options.
      */
     const CHOOSEN_MULTIPLE_INSTANCE_HINT = 2;
     /** 
      *  Sequential multuple instance hint allows several hints, that could be used only in sequence. 
      *  Current moodle text hints are example of this ones since there are no way to allow students to choose between them.
+     *
+     *  Hintkeys for sequential multiple instance hints should be returned ending with '#' character. Behaviour will expand
+     *  them appending number of instance.
      */
     const SEQENTIAL_MULTIPLE_INSTANCE_HINT = 3;
 
