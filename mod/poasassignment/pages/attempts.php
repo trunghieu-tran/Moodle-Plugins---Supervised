9b29e66f2525b2c3886d3451e8e22596c4751b08
nixorv <nixorv@localhost>
2014-02-23 22:12:33 +0400

    ???? ??????? ?????????, ?? ??? ?????????? ???????????? ??????????? ? ??????????.

--- mod/poasassignment/pages/attempts.php
+++ mod/poasassignment/pages/attempts.php
@@ -237,4 +237,44 @@ class attempts_page extends abstract_page {
             //echo $OUTPUT->box_end();
         }
     }
+
+    /**
+     * Returns HTML to display comments on all previous attempts.
+     * @param stdObject $attempt
+     * @return string
+     */
+    public static function show_comments($attempt) {
+        global $DB, $OUTPUT;
+        $poasmodel = poasassignment_model::get_instance();
+        $context = $poasmodel->get_context();
+        $html = '';
+
+        $options = new stdClass();
+        $options->area    = 'poasassignment_comment';
+        $options->component    = 'mod_poasassignment';
+        $options->pluginname = 'poasassignment';
+        $options->context = $context;
+        $options->autostart = true;
+        $options->notoggle = true;
+
+        $html .= $OUTPUT->box_start();
+        $html .= '<p><b><i>' . get_string('attempt', 'poasassignment') . ' ' . $attempt->attemptnumber . '</i></b> (' . userdate($attempt->attemptdate) . ')</p>';
+
+        $criterions = $DB->get_records('poasassignment_criterions',
+                                       array('poasassignmentid' => $poasmodel->get_poasassignment()->id));
+        foreach ($criterions as $criterion) {
+            $html .= '<p><b>' . $criterion->name . '</b>' . $poasmodel->help_icon($criterion->description) . '</p>';
+
+            $ratingvalue=$DB->get_record('poasassignment_rating_values',
+                                         array('criterionid' => $criterion->id, 'attemptid' => $attempt->id));
+            $options->itemid  = $ratingvalue->id;
+            $comment = new comment($options);
+            $comment->set_post_permission(false);
+            $html .= $comment->output(true);
+        }
+
+        $html .= $OUTPUT->box_end();
+
+        return $html;
+    }
 }
