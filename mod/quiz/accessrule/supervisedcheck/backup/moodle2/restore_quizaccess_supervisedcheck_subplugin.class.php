43c6447672cde59c4ab271035028bd30b2d79643
AndreyU <AndreyU@localhost>
2014-03-03 17:41:03 +0400

    ???????? ???????????? ??????????? ?? ???? ?????? supervised access rules.

--- mod/quiz/accessrule/supervisedcheck/backup/moodle2/restore_quizaccess_supervisedcheck_subplugin.class.php
+++ mod/quiz/accessrule/supervisedcheck/backup/moodle2/restore_quizaccess_supervisedcheck_subplugin.class.php
@@ -1,12 +1,12 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Student Access Control Kit - https://code.google.com/p/oasychev-moodle-plugins/
 //
-// Moodle is free software: you can redistribute it and/or modify
+// Access Control Kit is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// Access Control Kit is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -17,9 +17,10 @@
 /**
  * Restore code for the quizaccess_supervisedcheck plugin.
  *
- * @package   quizaccess_supervisedcheck
- * @copyright
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     quizaccess_supervisedcheck
+ * @author      Andrey Ushakov <andrey200964@yandex.ru>
+ * @copyright   2014 Oleg Sychev, Volgograd State Technical University
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 
--- mod/quiz/accessrule/supervisedcheck/db/events.php
+++ mod/quiz/accessrule/supervisedcheck/db/events.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * @package     quizaccess_supervisedcheck
+ * @author      Andrey Ushakov <andrey200964@yandex.ru>
+ * @copyright   2014 Oleg Sychev, Volgograd State Technical University
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
 $handlers = array (
     'course_deleted' => array (
         'handlerfile'      => '/mod/quiz/accessrule/supervisedcheck/lib.php',
