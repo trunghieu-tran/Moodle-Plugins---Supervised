40f030f179b4b934177a7af3b8a860b1493ea3ed
AndreyU <AndreyU@localhost>
2014-03-03 17:14:42 +0400

    ???????? ?????????? ??????????? ? ?????? ???? ?????? ????? supervised.

--- blocks/supervised/lessontypes/addedit_form.php
+++ blocks/supervised/lessontypes/addedit_form.php
@@ -1,12 +1,12 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Student Access Control Kit - https://code.google.com/p/oasychev-moodle-plugins/
 //
-// Moodle is free software: you can redistribute it and/or modify
+// Student Access Control Kit is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// Student Access Control Kit is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -21,6 +21,12 @@ require_once("{$CFG->libdir}/formslib.php");
  * Class addedit_lessontype_form
  *
  * The form for adding of editing lesson types
+ *
+ * @package     block
+ * @subpackage  supervised
+ * @author      Andrey Ushakov <andrey200964@yandex.ru>
+ * @copyright   2014 Oleg Sychev, Volgograd State Technical University
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class addedit_lessontype_form extends moodleform {
 
--- blocks/supervised/version.php
+++ blocks/supervised/version.php
@@ -1,12 +1,12 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Student Access Control Kit - https://code.google.com/p/oasychev-moodle-plugins/
 //
-// Moodle is free software: you can redistribute it and/or modify
+// Student Access Control Kit is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// Student Access Control Kit is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -15,12 +15,11 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
- *
  * @package    block
  * @subpackage supervised
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author     Andrey Ushakov <andrey200964@yandex.ru>
+ * @author      Andrey Ushakov <andrey200964@yandex.ru>
+ * @copyright   2014 Oleg Sychev, Volgograd State Technical University
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
