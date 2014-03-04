1e4cdfa6fd2fe66eadb57a2b6033dcdf14e00354
oasychev <oasychev@localhost>
2014-03-04 22:21:49 +0300

    ?????????? ???????????? ??????????? ? ??????? (???????? ???????).

--- mod/quiz/accessrule/supervisedcheck/backup/moodle2/restore_quizaccess_supervisedcheck_subplugin.class.php
+++ mod/quiz/accessrule/supervisedcheck/backup/moodle2/restore_quizaccess_supervisedcheck_subplugin.class.php
@@ -1,12 +1,12 @@
 <?php
 // This file is part of Student Access Control Kit - https://code.google.com/p/oasychev-moodle-plugins/
 //
-// Access Control Kit is free software: you can redistribute it and/or modify
+// Student Access Control Kit is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Access Control Kit is distributed in the hope that it will be useful,
+// Student Access Control Kit is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
