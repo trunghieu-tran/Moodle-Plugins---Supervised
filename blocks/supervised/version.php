beff8079747f2861045773bcfe172c40c4825517
oasychev <oasychev@localhost>
2014-03-28 23:04:38 +0300

    ????????? ?????? ????????? Moodle ?? 2.6

--- blocks/supervised/version.php
+++ blocks/supervised/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version    = 2014032200;           // The current module version (Date: YYYYMMDDXX).
-$plugin->requires   = 2013051400;           // Requires this Moodle version.
+$plugin->requires   = 2013110500;           // Requires this Moodle version.
 $plugin->component  = 'block_supervised';   // Full name of the plugin (used for diagnostics).
 $plugin->cron       = 300;                  // Minimum execution interval for cron function in secs.
 $plugin->release    = 'Block Supervised 2.6';
