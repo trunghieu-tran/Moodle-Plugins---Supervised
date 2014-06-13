f7b31c1fb641a9762f9ca1d883fce63731235e62
mamontov dp <mamontov.dp@gmail.com>
2014-06-13 22:01:36 +0400

    ?????? ?????????? ??????????????? ???????????. ??????????? ??????????? ???????? ??????.

--- question/type/correctwriting/syntax_analyzer.php
+++ question/type/correctwriting/syntax_analyzer.php
@@ -57,7 +57,6 @@ class qtype_correctwriting_syntax_analyzer extends qtype_correctwriting_abstract
      */
     public function __construct($question = null, $basepair = null, $language = null, $bypass = true) {
         parent::__construct($question, $basepair, $language, $bypass);
-
     }
 
     /**
@@ -79,7 +78,100 @@ class qtype_correctwriting_syntax_analyzer extends qtype_correctwriting_abstract
         //  - special function
         //NOTE: if response is null just check for errors - Pashaev
         //NOTE: if some stage create errors, stop processing right there
-        parent::bypass();
+        $result =  clone $this->basestringpair;
+
+        // Group indexes into sequences
+        $skippedlexemeindexes = $result->skippedlexemesindexes;
+        $addedlexemeindexes = $result->addedlexemesindexes;
+        $movedlexemeindexes = $result->movedlexemesindexes;
+
+        $skippedlexemegroups = array();
+        if (count($skippedlexemeindexes)) {
+            sort($skippedlexemeindexes);
+            foreach($skippedlexemeindexes as  $index) {
+                if (count($skippedlexemegroups) == 0) {
+                    $skippedlexemegroups[] = array( $index );
+                } else {
+                    $lastgroup = $skippedlexemegroups[count($skippedlexemegroups) - 1];
+                    $lastitem = $lastgroup[count($lastgroup) - 1];
+                    if ($index == $lastitem + 1) {
+                        $skippedlexemegroups[count($skippedlexemegroups) - 1][] = $index;
+                    } else {
+                        $skippedlexemegroups[] = array( $index );
+                    }
+                }
+            }
+        }
+
+        $addedlexemegroups = array();
+        if (count($addedlexemeindexes)) {
+            sort($addedlexemeindexes);
+            foreach($addedlexemeindexes as  $index) {
+                if (count($addedlexemegroups) == 0) {
+                    $addedlexemegroups[] = array( $index );
+                } else {
+                    $lastgroup = $addedlexemegroups[count($addedlexemegroups) - 1];
+                    $lastitem = $lastgroup[count($lastgroup) - 1];
+                    if ($index == $lastitem + 1) {
+                        $addedlexemegroups[count($addedlexemegroups) - 1][] = $index;
+                    } else {
+                        $addedlexemegroups[] = array( $index );
+                    }
+                }
+            }
+        }
+
+
+        $movedlexemegroups = array();
+        if (count($movedlexemeindexes)) {
+            ksort($movedlexemeindexes);
+            foreach($movedlexemeindexes as $key => $value) {
+                if (count($movedlexemegroups) == 0) {
+                    $movedlexemegroups[] = array( $key => $value );
+                } else {
+                    $lastgroup = $movedlexemegroups[count($movedlexemegroups) - 1];
+                    $keys = array_keys($lastgroup);
+                    $lastkey = $keys[count($keys) - 1];
+                    $lastvalue = $lastgroup[$lastkey];
+                    if ($key == $lastkey + 1 && $value == $lastvalue + 1) {
+                        $movedlexemegroups[count($movedlexemegroups) - 1][$key] = $value;
+                    } else {
+                        $movedlexemegroups[] = array( $key => $value );
+                    }
+                }
+            }
+        }
+
+
+
+
+        // Erase sequence errors
+        $mistakes = $result->mistakes();
+        if (count($mistakes)) {
+            foreach($mistakes as $key => $value) {
+                if (is_a($value, 'qtype_correctwriting_sequence_mistake')) {
+                    unset($mistakes[$key]);
+                }
+            }
+            $result->set_mistakes($mistakes);
+        }
+
+        // Convert found mistake to a found data
+
+        // Return value
+        $this->resultstringpairs[] = $result;
+
+    }
+
+
+    /**
+     * Fill resultstringpairs with a string pair, that simulates work of this analyzer allowing subsequent analyzers to work.
+     *
+     * You are normally would overload this, starting overload with parent function call, then add you work.
+     * Don't actually analyze something, no mistakes generated: just fill necessary fields in string pair.
+     */
+    protected function bypass() {
+        $this->resultstringpairs[] = clone $this->basestringpair; //Clone string pair for future use.
     }
 
 
