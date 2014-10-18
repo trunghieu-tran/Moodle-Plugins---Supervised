048b46094ea45347e69e3b6384799aa5d810b162
Elena Lepilkina <Elena Lepilkina@localhost>
2014-10-18 13:11:24 +0400

    ?????? ????????? ????? ?????? ???????. ??????????? ?????????? ? ???? "????????????????? ?????".

---
+++ local/ajaxcategories/version.php
@@ -0,0 +1,10 @@
+<?php
+
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2014101500;
+$plugin->release   = '0.0.0';
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->requires  = 2014051200;
+$plugin->component = 'local_ajaxcategories';
