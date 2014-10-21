a113068470c58873556d460a8ed18648e4c34370
mariabirvg <mariabirvg@localhost>
2014-10-21 22:22:40 +0400

    ?????????? ?????? ?? ??????????? ??????????

---
+++ blocks/formal_langs/block_formal_langs.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * A main class of block
+ *
+ * @package    formal_langs
+ * @copyright  2012 Sychev Oleg
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once($CFG->dirroot.'/blocks/formal_langs/language_base.php');
+require_once($CFG->dirroot.'/blocks/moodleblock.class.php');
+
+class block_formal_langs extends block_base {
+    //TODO: Implement this
+    public function init() {
+        $this->title = get_string('pluginname', 'block_formal_langs');
+    }
+
+    /**
+     * Returns an array of languages for given context
+     *
+     * @param contextid id of context, null means whole site
+     * @return array where key is language id and value is user interface language name (received throught get_string)
+     */
+    public static function available_langs($contextid = null) {
+        //TODO: Replace it with actual code
+        global $DB;
+        
+        //BUG: When installing moodle 2.5 settings of correctwriting will eventually call this function
+        // before table created
+        $dbman = $DB->get_manager();
+        if ($dbman->table_exists('block_formal_langs') == false) {
+            return array();
+        }
+        
+        //Get all visible records
+        $records = $DB->get_records('block_formal_langs', array('visible' => '1'));
+        
+        //Map, that checks amount of unique names in table. Populate it with values
+        $counts = array();
+        foreach($records as $record) {
+            if ($record->name !== null) {//Predefined language, ui_name is actually a language string, so replace it with actual name.
+                $record->ui_name = get_string('lang_' . $record->name , 'block_formal_langs');
+            }
+            if (array_key_exists($record->ui_name, $counts)) {
+                $counts[$record->ui_name] = $counts[$record->ui_name] + 1;
+            } else {
+                $counts[$record->ui_name] = 1;
+            }
+        }
+        //Populate result array
+        $result = array();
+        foreach($records as $record) {
+            if ($counts[$record->ui_name] > 1) {
+                $result[$record->id] = $record->ui_name . ' ' . $record->version;
+            } else {
+                $result[$record->id] = $record->ui_name;
+            }
+        }
+        
+        return $result;
+    }
+
+    /**
+     * Constructs and returns a language object for given languaged id
+     *
+     * @param int $langid id of the language
+     * @return block_formal_langs_abstract_language an intialised object of the child of the block_formal_langs_abstract_language class
+     */
+    public static function lang_object($langid) {
+        global $DB, $CFG;
+        $record = $DB->get_record('block_formal_langs', array('id' => $langid));
+        $result = null;
+        $arrayrecord = (array)$record;
+        if ($arrayrecord['name'] == null) {
+            $result = new block_formal_langs_userdefined_language($langid, $record->version, $record);
+        } else {
+            require_once($CFG->dirroot.'/blocks/formal_langs/language_' . $record->name . '.php');
+            $langname = 'block_formal_langs_language_' . $record->name;
+            $result = new $langname($langid, $record);
+        }
+        return $result;
+    }
+
+    /**
+     * Finds or insers language definition.
+     * All fields must be set
+     * @param array $language as tuple <ui_name, description, name, scanrules, parserules, version visible>.
+     * @return int id of inserted language
+     */
+    public static function find_or_insert_language($language) {
+        global $DB;
+        // Seek for language and insert it if not found, handling some error stuff
+        // Also cannot compare strings in some common case.
+        $sql = 'SELECT id
+                      FROM {block_formal_langs}
+                     WHERE ';
+        $filternames = array('name', 'version');
+        $filtervalues = array($language['name'], $language['version']);
+        if ($language['scanrules'] != null || $language['parserules'] != null) {
+            $filternames[] = 'scanrules';
+            $filternames[] = 'parserules';
+            $filtervalues[]  = $language['scanrules'] ;
+            $filtervalues[]  = $language['parserules'];
+        }
+        // Transform columns into sql comparisons
+        $sqlfilternames = array();
+        foreach($filternames as $name) {
+            $sqlfilternames[] = $DB->sql_compare_text($name, 512) . ' = ' . $DB->sql_compare_text('?', 512);
+        }
+        // Build actual sql request
+        $sql .= implode(' AND ', $sqlfilternames);
+        $sql .= ';';
+
+        $record = $DB->get_record_sql($sql, $filtervalues);
+        if ($record == false) {
+            $result = $DB->insert_record('block_formal_langs', $language);
+        } else {
+            $result = $record->id;
+        }
+        return $result;
+    }
+}
