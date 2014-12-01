b7d1abf25bbfe5472a25800c774ddfec857d61d5
Dmitry Mamontov <Dmitry Mamontov@localhost>
2014-12-01 22:01:17 +0400

    ???????? ?????????? ???? ? ????????. ????? ????? ?????????? ? ? ??????????.

---
+++ blocks/formal_langs/parsertests/input/types/std__int__vector__int__inner_2.php
@@ -0,0 +1,11 @@
+<?php
+$namespacetree = array(
+    'std' => array(
+        'vector' => array(
+            'inner' => array(
+            )
+        )
+    )
+);
+
+$string = "std<int>::vector<2>::inner";
