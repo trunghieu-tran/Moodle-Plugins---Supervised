d190b8a393eacb61433b9aebb6bcbc0e0fe84674
Dmitry Mamontov <Dmitry Mamontov@localhost>
2014-12-05 22:58:54 +0400

    ???????? ???????? 284 ? 299 ? ???????.

---
+++ blocks/formal_langs/parsertests/input/issues/284_1.php
@@ -0,0 +1,14 @@
+<?php
+$namespacetree = array(
+	'vector' => array(
+        
+	),
+    'std' => array(
+        'vector' => array(
+            'inner' => array(
+            )
+        )
+    )
+);
+
+$string = "Expr1 == Expr2 && Expr3 != Expr4 && Expr5 || Expr6;";
