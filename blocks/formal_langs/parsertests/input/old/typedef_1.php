7cf38190580b3dba2579c511236e2ed489abd61f
Dmitry Mamontov <Dmitry Mamontov@localhost>
2014-12-08 09:52:15 +0300

    ???????? ????? ?????? ?? PHPUnit: ?? ??????? ?????, ??? typedef_1.txt ??????

---
+++ blocks/formal_langs/parsertests/input/old/typedef_1.php
@@ -0,0 +1,14 @@
+<?php
+$namespacetree = array(
+	'vector' => array(
+        
+	),
+    'std' => array(
+        'vector' => array(
+            'inner' => array(
+            )
+        )
+    )
+);
+
+$string = "typedef int __int32; __int32 A = 32 + 64;";
