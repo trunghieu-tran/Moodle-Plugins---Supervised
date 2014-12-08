d9b22d3f8b0c86759d6e7b7f3091fc42a4225e0f
Dmitry Mamontov <Dmitry Mamontov@localhost>
2014-12-08 11:27:40 +0300

    ????????? ??? ?????? ?????. ????????, ????? ????? ????????? ????????.

---
+++ blocks/formal_langs/parsertests/input/old/if_else_1.php
@@ -0,0 +1,14 @@
+<?php
+$namespacetree = array(
+	'vector' => array(
+        
+	),
+    'std' => array(
+        'vector' => array(
+            'inner' => array(
+            )
+        )
+    )
+);
+
+$string = "if (a > 22) a+=5; else { int b = rand(); a += b; }";
