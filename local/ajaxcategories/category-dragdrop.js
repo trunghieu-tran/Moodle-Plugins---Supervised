e790eb7d930f89fe89ab09c4bb091434d85e92d0
Elena Lepilkina <Elena Lepilkina@localhost>
2014-12-19 13:20:24 +0300

    ???? ?? ????????????? sortable ? ???????? dd, ?????? ????? ???????????? ????? ???????????.

--- local/ajaxcategories/category-dragdrop.js
+++ local/ajaxcategories/category-dragdrop.js
@@ -9,7 +9,104 @@ function dump(obj) {
     }
     alert(out);
 }
-YUI().use( 'dd' , 'sortable', 'gallery-treeviewlite', function(Y) {
+
+YUI().use('dd-constrain', 'dd-proxy', 'dd-drop', function(Y) {
+  //Listen for all drop:over events
+    Y.DD.DDM.on('drop:over', function(e) {
+        //Get a reference to our drag and drop nodes
+        var drag = e.drag.get('node'),
+            drop = e.drop.get('node');
+
+        //Are we dropping on a li node?
+        if (drop.get('tagName').toLowerCase() === 'li') {
+            //Are we not going up?
+            if (!goingUp) {
+                drop = drop.get('nextSibling');
+            }
+            //Add the node to this list
+            e.drop.get('node').get('parentNode').insertBefore(drag, drop);
+            //Resize this nodes shim, so we can drop on it later.
+            e.drop.sizeShim();
+        }
+    });
+
+    //Listen for all drag:drag events
+    Y.DD.DDM.on('drag:drag', function(e) {
+        //Get the last y point
+        var y = e.target.lastXY[1];
+        //is it greater than the lastY var?
+        if (y < lastY) {
+            //We are going up
+            goingUp = true;
+        } else {
+            //We are going down.
+            goingUp = false;
+        }
+        //Cache for next check
+        lastY = y;
+    });
+    //Listen for all drag:start events
+    Y.DD.DDM.on('drag:start', function(e) {
+        //Get our drag object
+        var drag = e.target;
+        //Set some styles here
+        drag.get('node').setStyle('opacity', '.25');
+        drag.get('dragNode').set('innerHTML', drag.get('node').get('innerHTML'));
+        drag.get('dragNode').setStyles({
+            opacity: '.5',
+            borderColor: drag.get('node').getStyle('borderColor'),
+            backgroundColor: drag.get('node').getStyle('backgroundColor')
+        });
+    });
+    //Listen for a drag:end events
+    Y.DD.DDM.on('drag:end', function(e) {
+        var drag = e.target;
+        //Put our styles back
+        drag.get('node').setStyles({
+            visibility: '',
+            opacity: '1'
+        });
+    });
+    //Listen for all drag:drophit events
+    Y.DD.DDM.on('drag:drophit', function(e) {
+        var drop = e.drop.get('node'),
+            drag = e.drag.get('node');
+
+        //if we are not on an li, we must have been dropped on a ul
+        if (drop.get('tagName').toLowerCase() !== 'li') {
+            if (!drop.contains(drag)) {
+                drop.appendChild(drag);
+            }
+        }
+    });
+
+    //Static Vars
+    var goingUp = false, lastY = 0;
+var items = Y.Node.all('#ajaxlistitem');
+items.each(function(v, k) {
+    var dd = new Y.DD.Drag({
+        node: v,
+        //Make it Drop target and pass this config to the Drop constructor
+        target: {
+            padding: '0 0 0 0'
+        }
+    }).plug(Y.Plugin.DDProxy, {
+        //Don't move the node at the end of the drag
+        moveOnEnd: false
+    }).plug(Y.Plugin.DDConstrained, {
+        //Keep it inside the #play node
+        constrain2node: '#ajaxcategorylist'
+    });
+});
+
+var uls = Y.Node.all('#ajaxcategorylist ul');
+    uls.each(function(v, k) {
+        var tar = new Y.DD.Drop({
+            node: v
+        });
+    });
+});
+/*YUI().use( 'dd' , 'sortable', 'gallery-treeviewlite', function(Y) {
 
  var list1 = new Y.Sortable({
         container: '#ajaxcategorylist',
@@ -87,11 +184,11 @@ var recNode;
               catch(e){}
               break;*/
 
-          default:
+         /* default:
               break;
 
         }
 
 
     });
-});
+});*/
