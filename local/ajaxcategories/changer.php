4f04b035a9581e1b176a90958516f91f741e809d
Elena Lepilkina <Elena Lepilkina@localhost>
2015-01-05 21:52:51 +0300

    ????????? ajax, ???????? ?????????? ? ?? ?????????.

--- local/ajaxcategories/changer.php
+++ local/ajaxcategories/changer.php
@@ -7,34 +7,25 @@ require_once("../../config.php");
 require_once($CFG->dirroot."/question/editlib.php");
 require_once($CFG->dirroot."/local/ajaxcategories/category_class.php");
 
-$movingid = $_REQUEST['movingid'];
-$before = $_REQUEST['before'];
-$after = $_REQUEST['after'];
-$level = $_REQUEST['level'];
-$dest = $_REQUEST['dest'];
+$movingid = optional_param('movingid', 0, PARAM_INT);
+$before = optional_param('before', 0, PARAM_INT);
+$after = optional_param('after', 0, PARAM_INT);
+$level = optional_param('level', '',PARAM_ALPHA);
+$dest = optional_param('dest', 0, PARAM_INT);
 
     list($thispageurl, $contexts, $cmid, $cm, $module, $pagevars) =
         question_edit_setup('categories', '/local/ajaxcategories/index.php');
 
     // Get values from form for actions on this page.
     $param = new stdClass();
-    $param->moveup = optional_param('moveup', 0, PARAM_INT);
-    $param->movedown = optional_param('movedown', 0, PARAM_INT);
-    $param->moveupcontext = optional_param('moveupcontext', 0, PARAM_INT);
-    $param->movedowncontext = optional_param('movedowncontext', 0, PARAM_INT);
-    $param->tocontext = optional_param('tocontext', 0, PARAM_INT);
-    $param->left = optional_param('left', 0, PARAM_INT);
-    $param->right = optional_param('right', 0, PARAM_INT);
+
     $param->delete = optional_param('delete', 0, PARAM_INT);
-    $param->confirm = optional_param('confirm', 0, PARAM_INT);
-    $param->cancel = optional_param('cancel', '', PARAM_ALPHA);
-    $param->move = optional_param('move', 0, PARAM_INT);
-    $param->moveto = optional_param('moveto', 0, PARAM_INT);
     $param->edit = optional_param('edit', 0, PARAM_INT);
 
     $qcobject = new ajax_question_category_object($pagevars['cpage'], $thispageurl,
         $contexts->having_one_edit_tab_cap('categories'), $param->edit,
         $pagevars['cat'], $param->delete, $contexts->having_cap('moodle/question:add'));
+
 
     $fromlist = $qcobject->find_list($movingid);
     $environment = array();
