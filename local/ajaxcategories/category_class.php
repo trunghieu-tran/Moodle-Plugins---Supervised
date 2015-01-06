a446f6c665b178745946ed8b235bb0cbcd6b13c1
Elena Lepilkina <Elena Lepilkina@localhost>
2015-01-06 20:38:08 +0300

    ????????? ????? ????, ???????? ???????????, ?????? ?????????? ??????. + ????????? ?????=?? ???????? ???.

--- local/ajaxcategories/category_class.php
+++ local/ajaxcategories/category_class.php
@@ -1,29 +1,4 @@
 ﻿<?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * A class for representing question categories.
- *
- * @package    moodlecore
- * @subpackage questionbank
- * @copyright  1999 onwards Martin Dougiamas {@link http://moodle.com}
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-
 defined('MOODLE_INTERNAL') || die();
 
 // number of categories to display on page
@@ -33,9 +8,8 @@ require_once($CFG->libdir . '/listlib.php');
 require_once($CFG->dirroot . '/question/category_form.php');
 require_once($CFG->dirroot . '/question/move_form.php');
 
-
 /**
- * Class representing a list of question categories
+ * Class representing a list of ajax question categories
  *
  */
 class ajax_question_category_list extends moodle_list {
@@ -49,7 +23,9 @@ class ajax_question_category_list extends moodle_list {
      * @var reference to list displayed above this one.
      */
     public $lastlist = null;
-
+    /**
+     * @var context og this list.
+     */
     public $context = null;
     public $sortby = 'parent, sortorder, name';
 
@@ -62,7 +38,6 @@ class ajax_question_category_list extends moodle_list {
         $this->records = get_categories_for_contexts($this->context->id, $this->sortby);
     }
 
-
     /**
      * Replace category item in choosen place.
      *
@@ -74,20 +49,19 @@ class ajax_question_category_list extends moodle_list {
      */
     function change_category_list($movingid, $environment) {
         global $DB;
-
+        // Get item by id.
         $item = $this->find_item($movingid);
         // Change context.
         if ($environment['dest']->context->id != $this->context->id) {
-            // Moving to a new context. Must move files belonging to questions.
+            // Get all children items of moving item.
             $children = $item->get_all_children();
             $children[] = $movingid;
-            var_dump($children);
+            // Change context for all replacing categories.
             foreach ($children as $child) {
                 $oldcat = $DB->get_record('question_categories', array('id' => $child), '*', MUST_EXIST);
                 $oldcat->contextid = $environment['dest']->context->id;
                 $DB->update_record('question_categories', $oldcat);
             }
-
         }
 
         //  Define the place of replacing
@@ -105,27 +79,38 @@ class ajax_question_category_list extends moodle_list {
                 $DB->set_field($environment['dest']->table, "parent", $newparent, array("id"=>$item->id));
                 $newpeers = $environment['dest']->get_items_peers($afteritem->id);
                 var_dump($newpeers);
+                // Place of item after which should be added moving category.
                 $oldkey = array_search($afteritem->id, $newpeers);
+                // Place of moving category.
                 $key = array_search($item->id, $newpeers);
-                var_dump($key);
+                // If moving category was at the same list reoder categories in list.
                 if ($key) {
+                    // Replace moving category up.
                     if ($oldkey < $key) {
-                        $neworder = array_merge(array_slice($newpeers, 0, $oldkey+1), array($item->id), array_slice($newpeers, $oldkey+1, $key-1), array_slice($newpeers, $key+1));
+
+                        if ($key !== count($newpeers) - 1) {
+                           $neworder = array_merge(array_slice($newpeers, 0, $oldkey+1), array($item->id), array_slice($newpeers, $oldkey+1, $key-1), array_slice($newpeers, $key+1));
+                        } else {
+                            $neworder = array_merge(array_slice($newpeers, 0, $oldkey+1), array($item->id), array_slice($newpeers, $oldkey+1, $key - 2));
+                        }
                     } else {
+                        // Replace moving category down.
                         $neworder = array_merge(array_slice($newpeers, 0, $key), array_slice($newpeers, $key+1, $oldkey), array($item->id),  array_slice($newpeers, $oldkey +1));
                     }
                 } else {
                     $neworder = array_merge(array_slice($newpeers, 0, $oldkey+1), array($item->id), array_slice($newpeers, $oldkey+1));
                 }
-                var_dump($neworder);
+                // Set new order of categories.
                 $environment['dest']->reorder_peers($neworder);
             } else {
+                // Create new nested list. Replace moving category to it.
                 $newlist = new ajax_question_category_list($this->type, $this->attributes, $this->editable, $this->pageurl, $this->page, $this->pageparamname,  $this->itemsperpage, $this->context);
                 $newlist->parentitem = $afteritem;
                 $newparent = $afteritem->id;
                 $DB->set_field($environment['dest']->table, "parent", $newparent, array("id"=>$item->id));
             }
         } else {
+            // Replacing at the same level before some item.
             $beforeitem = $environment['dest']->find_item($environment['before']);
             if (isset($beforeitem->parentlist->parentitem)) {
                 $newparent = $beforeitem->parentlist->parentitem->id;
@@ -134,15 +119,16 @@ class ajax_question_category_list extends moodle_list {
             }
             $DB->set_field($environment['dest']->table, "parent", $newparent, array("id"=>$item->id));
             $newpeers = $environment['dest']->get_items_peers($beforeitem->id);
-            var_dump($newpeers);
+            // Place of item after which should be added moving category.
             $oldkey = array_search($beforeitem->id, $newpeers);
+            // Place of moving category.
             $key = array_search($item->id, $newpeers);
+            // If moving category was at the same list reoder categories in list.
             if ($key) {
                 $neworder = array_merge(array_slice($newpeers, 0, $oldkey), array($item->id), array_slice($newpeers, $oldkey, $key), array_slice($newpeers, $key+1));
             } else {
                 $neworder = array_merge(array_slice($newpeers, 0, $oldkey), array($item->id), array_slice($newpeers, $oldkey));
             }
-            var_dump($neworder);
             $environment['dest']->reorder_peers($neworder);
         }
     }
@@ -154,7 +140,6 @@ class ajax_question_category_list extends moodle_list {
      * @param integer $indent depth of indentation.
      */
     public function to_html($indent=0, $extraargs=array()) {
-
         $attributes = array(
             'id' => 'ajaxlistitem',
         );
@@ -163,25 +148,26 @@ class ajax_question_category_list extends moodle_list {
             'id' => 'placeholder',
         );
 
-
         if (count($this->items)) {
             $tabs = str_repeat("\t", $indent);
             $first = true;
             $itemiter = 1;
             $lastitem = '';
             $html = '';
-            //$html .= html_writer::start_div('ajaxcategorylist',  array('id' => 'ajaxcategorylist'));
+            // Get html string for each list.
             foreach ($this->items as $item) {
+                // Create item attributes.
                 $itemattributes = array(
                     'id' => 'ajaxitem',
                     'data-id' => $item->id,
                 );
+                // Add first placeholder for first item.
                 if ($first) {
                     $html .= html_writer::start_tag('div' ,$placeholder);
                     $html .= html_writer::end_tag('div');
                 }
+                // Write item html.
                 $html .= html_writer::start_tag('li', $attributes);
-
                 $html .= html_writer::start_div('ajaxitem', $itemattributes);
                 $last = (count($this->items) == $itemiter);
                 if ($this->editable) {
@@ -191,7 +177,6 @@ class ajax_question_category_list extends moodle_list {
                     $html .= $itemhtml;
                 }
                 $html .= html_writer::end_div();
-
                 $html .= html_writer::end_tag('li');
                 $html .= html_writer::start_tag('div' ,$placeholder);
                 $html .= html_writer::end_tag('div');
@@ -199,7 +184,6 @@ class ajax_question_category_list extends moodle_list {
                 $lastitem = $item;
                 $itemiter++;
             }
-            //$html .= html_writer::end_div();
         } else {
             $html = '';
         }
@@ -220,6 +204,10 @@ class ajax_question_category_list extends moodle_list {
  *
  */
 class ajax_question_category_list_item extends list_item {
+    /**
+     * Returns array of all children items of this.
+     *
+     */
     public function get_all_children() {
         $children = array();
         if (isset($this->children)) {
@@ -251,6 +239,7 @@ class ajax_question_category_list_item extends list_item {
         $questionbankurl->param('cat', $category->id . ',' . $category->contextid);
         $catediturl = new moodle_url($this->parentlist->pageurl, array('edit' => $this->id));
         $item = '';
+        // Add drag-handle icon.
         if ($this->parentlist !== null && ($this->parentlist->parentitem !== null || count($this->parentlist->items) > 1)) {
             $item .= html_writer::div($OUTPUT->pix_icon('i/move_2d', 'You can drag and drop this category'), 'drag-handle');
         }
--- local/ajaxcategories/lib.php
+++ local/ajaxcategories/lib.php
@@ -6,7 +6,6 @@ defined('MOODLE_INTERNAL') || die;
 function local_ajaxcategories_extends_settings_navigation(settings_navigation $nav, context $context) {
     $coursenode = $nav->get('courseadmin');
     if ($coursenode) {
-        //var_dump($coursenode);
 	   $questionbank = $coursenode->find(12, navigation_node::TYPE_CONTAINER);
         if ($questionbank) {
     	   $params = array();
