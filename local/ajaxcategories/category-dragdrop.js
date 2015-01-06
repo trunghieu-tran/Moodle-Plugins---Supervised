cd582b30168e3ecc50c6c2d8f28bf558df4a73fd
Elena Lepilkina <Elena Lepilkina@localhost>
2015-01-06 20:38:08 +0300

    ????????? ????? ????, ???????? ???????????, ?????? ?????????? ??????. + ????????? ?????=?? ???????? ???.

--- local/ajaxcategories/category-dragdrop.js
+++ local/ajaxcategories/category-dragdrop.js
@@ -1,17 +1,9 @@
-function dump(obj) {
-    var out = "";
-    if(obj && typeof(obj) == "object"){
-        for (var i in obj) {
-            out += i + ": " + obj[i] + "\n";
-        }
-    } else {
-        out = obj;
-    }
-    alert(out);
-}
-
-var options = {};
-
+/**
+ * Form string with parametrs for GET-request
+ *
+ * @param object with parametrs
+ * @return string with parametrs for GET-request
+*/
 function get_params(options) {
     var string = '';
     for (var key in options) {
@@ -23,36 +15,11 @@ function get_params(options) {
     return string;
 }
 
-/*YUI().use("io-base", function(Y) {
-    var uri = "./changer.php?";
-    uri += get_params(options);
-    // Define a function to handle the response data.
-    function complete(id, o, args) {
-        var id = id; // Transaction ID.
-        var data = o.responseText; // Response data.
-        var args = args[1]; // 'ipsum'.
-    };
-
-    // Subscribe to event "io:complete", and pass an array
-    // as an argument to the event handler "complete", since
-    // "complete" is global.   At this point in the transaction
-    // lifecycle, success or failure is not yet known.
-    Y.on('io:complete', complete, Y, ['lorem', 'ipsum']);
-
-    // Make an HTTP request to 'get.php'.
-    // NOTE: This transaction does not use a configuration object.
-    var request = Y.io(uri);
-    alert(request);
-});*/
-
 YUI().use('dd-constrain', 'dd-proxy', 'dd-drop', 'dd-plugin','io-base', function(Y) {
-    var before;
     var addedNode;
     var child;
     var after;
-    var block;
     var change = true;
-    var blockX;
     var contextid;
     var beforeitemid;
     var afteritemid;
@@ -61,25 +28,35 @@ YUI().use('dd-constrain', 'dd-proxy', 'dd-drop', 'dd-plugin','io-base', function
     var addedNodewasadd = false;
     var ancestor;
     var cloneancestor;
+    var options = {};
 
-function ajaxRequest(uri) {
-
-    // Make the request. It has to be on your domain, not cross domain.
-    Y.io(uri, {
-        on: {
-            success: function (x, o) {
-                console.log("!!!!!!!!!!!!!!!!!");
-
+    /**
+     * Make ajax request.
+     *
+     * @param string with GET-request
+    */
+    function ajaxRequest(uri) {
+        // Make the request
+        Y.io(uri, {
+            on: {
+                success: function (x, o) {
+                }
             }
-        }
-    });
-}
-  //Listen for all drop:over events
+        });
+    }
+
+    //Listen for all drop:over events
     Y.DD.DDM.on('drag:over', function(e) {
+        var tar;
+        var tar1;
+        var item;
         //Get a reference to our drag and drop nodes
-        var drag = e.drag.get('node'),
-            drop = e.drop.get('node');
+        var drag = e.drag.get('node');
+        var drop = e.drop.get('node');
+
+        // Get parent node of drop node.
         var parent = drop.ancestor();
+        // Remove old nodes created for previous drag-and-drop.
         if (child !== undefined && child !== null && !change) {
             child.remove();
         }
@@ -87,9 +64,9 @@ function ajaxRequest(uri) {
             addedNode.remove();
         }
         change = false;
+        // Get contextid of dropped list.
         contextid = drop.getAttribute('data-id');
         if (contextid  === null ||  contextid == '') {
-            //alert('aaaaa');
             parent = drop.ancestor('ul[data-id]');
             if (parent !== null) {
                 contextid = parent.getAttribute('data-id');
@@ -99,35 +76,42 @@ function ajaxRequest(uri) {
         } else {
             contextid = drop.getAttribute('data-id');
         }
-        //alert(contextid);
+
+        // Create node of placeholders.
         child = Y.Node.create( '<div id = "placeholder"></div>' );
-        var tar = new Y.DD.Drop({
+        // Make node be droppable.
+        tar = new Y.DD.Drop({
             node: child
         });
+        // Add category as a child.
         if (drop.get('tagName').toLowerCase() === 'li') {
+            // No nested list.
             if (drop.one("ul") == undefined) {
                 addedNode = Y.Node.create( '<ul><div id = "placeholder"></div></ul>' );
                 addedNode.setAttribute("id", Y.guid() );
-                var tar1 = new Y.DD.Drop({
-            node: addedNode.one('#placeholder')
-        });
-
+                tar1 = new Y.DD.Drop({
+                    node: addedNode.one('#placeholder')
+                });
+                // Add dragged node to list.
                 addedNode.append(child);
                 addedNode.append(drag);
                 addedNode.append(child);
                 drop.append(addedNode);
-                var item = drop.one('.ajaxitem[data-id]');
+                // Get id of parent node.
+                item = drop.one('.ajaxitem[data-id]');
                 beforeitemid = -1;
                 afteritemid = item.getAttribute('data-id');
                 level = 'inner';
                 addedNodewasadd = true;
             } else {
+                // Add to nested list created before/
                 addedNodewasadd = false;
                 addedNode = drop.one("ul");
                 addedNode.append(drag);
+                // Get id of parent node.
                 if ((addedNode.get('children').size()-3) >= 0)
                 {
-                    var item = addedNode.get('children').item(addedNode.get('children').size()-3);
+                    item = addedNode.get('children').item(addedNode.get('children').size()-3);
                     item = item.one('.ajaxitem[data-id]');
                     if (item) {
                         beforeitemid = -1;
@@ -138,32 +122,28 @@ function ajaxRequest(uri) {
                 addedNode.append(child);
             }
         } else {
-            console.log('sssssss');
+            // Add node at the same level with other nodes in list/
             if (drop.one('#placeholder') !== undefined) {
                 beforeitemid = -1;
                 afteritemid = -1;
-                var item = drop.get('nextSibling');
+                item = drop.get('nextSibling');
+                // Get id of item which should be undo dragged node.
                 if (item !== undefined && item !== null) {
-                    //if (item.one('.ajaxitem[data-id]') === undefined || item.one('.ajaxitem[data-id]') === null) {
-                        console.log(item);
-                        if (item.get('nextSibling')) {
-                            if (item.get('nextSibling').get('tagName').toLowerCase() === 'li') {
-                                item = item.get('nextSibling');
-                            }
+                    if (item.get('nextSibling')) {
+                        if (item.get('nextSibling').get('tagName').toLowerCase() === 'li') {
+                            item = item.get('nextSibling');
                         }
-
-                        console.log(item);
-                    //}
+                    }
                     if (item !== null) {
-                    item = item.one('.ajaxitem[data-id]');
-                    if (item !== undefined && item !== null) {
-                        beforeitemid = item.getAttribute('data-id');
-                        contextid = item.ancestor('ul').getAttribute('data-id');
-                        level = 'normal';
-
+                        item = item.one('.ajaxitem[data-id]');
+                        if (item !== undefined && item !== null) {
+                            beforeitemid = item.getAttribute('data-id');
+                            contextid = item.ancestor('ul').getAttribute('data-id');
+                            level = 'normal';
+                        }
                     }
                 }
-                }
+                // Get id of item which should be above dragged node.
                 item = drop.get('previousSibling');
                 if (item !== undefined && item !== null) {
                     item = item.one('.ajaxitem[data-id]');
@@ -173,25 +153,12 @@ function ajaxRequest(uri) {
                         level = 'normal';
                     }
                 }
+                // Add dragged node to list.
                 drop.insert(child, 'after');
                 drop.insert(drag, 'after');
                 level = 'normal';
             }
         }
-
-        //alert(drop.getAttribute('data-id'));
-
-        //Are we dropping on a li node?
-        /*if (drop.get('tagName').toLowerCase() === 'li') {
-            //Are we not going up?
-            if (!goingUp) {
-                drop = drop.get('nextSibling');
-            }
-            //Add the node to this list
-            e.drop.get('node').get('parentNode').insertBefore(drag, drop);
-            //Resize this nodes shim, so we can drop on it later.
-            e.drop.sizeShim();
-        }*/
     });
 
     //Listen for all drag:start events
@@ -199,15 +166,17 @@ function ajaxRequest(uri) {
         //Get our drag object
         var drag = e.target;
         var dragnode = drag.get('node');
+        // Get copy of parent.
         cloneancestor = dragnode.get('parentNode').cloneNode(true);
-        console.log(cloneancestor);
         ancestor = dragnode.get('parentNode');
-        //Set some styles here
+        //Set new style
         drag.get('node').setStyle('opacity', '.25');
+        // Remove bottom placeholder.
         var next = drag.get('node').get('nextSibling');
         if (next !== null) {
             next.remove();
         }
+        //Set new style
         drag.get('dragNode').set('innerHTML', drag.get('node').get('innerHTML'));
         drag.get('dragNode').setStyles({
             opacity: '.5',
@@ -215,38 +184,42 @@ function ajaxRequest(uri) {
             backgroundColor: drag.get('node').getStyle('backgroundColor')
         });
         var item = drag.get('node').one('.ajaxitem[data-id]');
+        // Get id of dragged category.
         movingid = item.getAttribute('data-id');
+        // Remove dragged node from old place.
         drag.get('node').remove();
-
-
     });
-
 
     //Listen for a drag:end events
     Y.DD.DDM.on('drag:end', function(e) {
         var drag = e.target;
         var item = drag.get('node').one("ul");
+        // Get ul with context id.
         while (item !== undefined && item !== null) {
             item.setAttribute('data-id', contextid);
             item = item.one("ul[data-id]");
         }
-        //Put our styles back
+        //Put styles back
         drag.get('node').setStyles({
             visibility: '',
             opacity: '1'
         });
         change = true;
+        // Check count of top categories in each context.
+        // Get top items.
         var topitems = Y.Node.all('ul[data-id]');
         topitems.each(function(value, key) {
             var parentnode = value.get('parentNode');
             if (!parentnode.hasClass('ajaxitem')) {
                 var childrennodes = parentnode.one('ul').get("children");
                 var childrencount = 0;
+                // Count children of node.
                 childrennodes.each(function(child, key) {
                     if (child.get('tagName').toLowerCase() === 'li') {
                         childrencount++;
                     }
                 });
+                // In case if top categories in context more than one add drag-handle.
                 if (childrencount > 1) {
                     childrennodes.each(function(child, key) {
                         if (child.get('tagName').toLowerCase() === 'li' && child.one('#ajaxitem') !== null && !child.one('#ajaxitem').get("children").item(0).hasClass('drag-handle')) {
@@ -254,6 +227,7 @@ function ajaxRequest(uri) {
                         }
                     });
                 } else {
+                    // In case of single top category remove drag-handle.
                     childrennodes.each(function(child, key) {
                         if (child.get('tagName').toLowerCase() === 'li' && child.one('#ajaxitem') !== null && child.one('#ajaxitem').get("children").item(0).hasClass('drag-handle')) {
                             child.one('#ajaxitem').get("children").item(0).remove();
@@ -262,152 +236,56 @@ function ajaxRequest(uri) {
                 }
             }
         });
-if (beforeitemid == -1 && afteritemid == -1 || beforeitemid === undefined || afteritemid == undefined) {
-    console.log(cloneancestor);
-ancestor.replace(cloneancestor);
-
-} else {
-        options['movingid'] = movingid;
-        options['before'] = beforeitemid.toString();
-        options['after'] = afteritemid.toString();
-        options['level'] = level;
-        options['dest'] = contextid;
-        get_params(options);
-        var uri = "changer.php" + location.search + '&';
-        uri += get_params(options);
-        uri = M.cfg.wwwroot + '/local/ajaxcategories/' + uri;
-        console.log(uri);
-        ajaxRequest(uri);
-    }
-        //var request = new Y.io(uri);
-        //request.send();
-        //alert(request);
-        //alert(movingid);
-        //alert(beforeitemid);
-        //alert(afteritemid);
-        //alert(contextid);
+        // If dropped place is invalid, return category to start position.
+        if (beforeitemid == -1 && afteritemid == -1 || beforeitemid === undefined || afteritemid == undefined) {
+            ancestor.replace(cloneancestor);
+        } else {
+            // Fill options.
+            options['movingid'] = movingid;
+            options['before'] = beforeitemid.toString();
+            options['after'] = afteritemid.toString();
+            options['level'] = level;
+            options['dest'] = contextid;
+            // Get uri for GET-request.
+            get_params(options);
+            var uri = "changer.php" + location.search + '&';
+            uri += get_params(options);
+            uri = M.cfg.wwwroot + '/local/ajaxcategories/' + uri;
+            console.log(uri);
+            // Make ajax request.
+            ajaxRequest(uri);
+        }
     });
 
 
     //Static Vars
     var goingUp = false, lastY = 0;
-var items = Y.Node.all('#ajaxcategorylist li');
-items.each(function(v, k) {
-
-
-    var dd = new Y.DD.Drag({
-        node: v,
-        //Make it Drop target and pass this config to the Drop constructor
-        target: {
-            padding: '0 0 0 0'
-        }
-    }).plug(Y.Plugin.DDProxy, {
-        //Don't move the node at the end of the drag
-        moveOnEnd: false
-    }).plug(Y.Plugin.DDConstrained, {
-        //Keep it inside the #play node
-        constrain2node: '#main'
+    // Draggable nodes
+    var items = Y.Node.all('#ajaxcategorylist li');
+    items.each(function(v, k) {
+        var dd = new Y.DD.Drag({
+            node: v,
+            //Make it Drop target and pass this config to the Drop constructor
+            target: {
+                padding: '0 0 0 0'
+            }
+        }).plug(Y.Plugin.DDProxy, {
+            //Don't move the node at the end of the drag
+            moveOnEnd: false
+        }).plug(Y.Plugin.DDConstrained, {
+            //Keep it inside the #play node
+            constrain2node: '#main'
+        });
+        dd.addHandle('.drag-handle');
     });
-   dd.addHandle('.drag-handle');
 
-
-
-});
-
-var uls = Y.Node.all('#placeholder');
-    uls.each(function(v, k) {
-        var tar = new Y.DD.Drop({
-            node: v
+    // Droppable nodes.
+    var uls = Y.Node.all('#placeholder');
+        uls.each(function(v, k) {
+            var tar = new Y.DD.Drop({
+                node: v
+            });
         });
     });
-});
 
-var draghandle = Y.Node.one('.drag-handle').cloneNode(true);
-
-
-/*YUI().use( 'dd' , 'sortable', 'gallery-treeviewlite', function(Y) {
-
- var list1 = new Y.Sortable({
-        container: '#ajaxcategorylist',
-        nodes: 'li',
-        opacity: '.5',
-
-    });
-
-    Y.one( "#ajaxcategorylist" ).plug( Y.Plugin.TreeviewLite );
-    // nodes we're moving around
-    var addedNode, newNode;
-
-var recNode;
-
-    // make a new node that can be inserted as a new tree sometimes
-    Y.DD.DDM.on("drag:start", function(ev){
-
-            newNode = Y.Node.create( "<ul></ul>" );
-            newNode.setAttribute("id", Y.guid() );
-            recNode = Y.Node.create( '<li class="placeholder"></li>');
-            recNode.setAttribute("id", Y.guid() );
-    });
-
-
-    // insert the nodes where needed
-    Y.DD.DDM.on("drag:over", function(ev){
-
-                  // remove it from where it was
-                  if( addedNode !== undefined ) {
-                      addedNode.remove();
-                  }
-
-
-        var t = ev.drop.get("node"),
-            // tOl is looking for a child ol below the li
-            tOl = t.one( "ul" );
-
-
-
-        // if we've over an li, add the new ol child block
-        switch( t.get("nodeName").toLowerCase() ) {
-
-           case "li":
-
-                // try and append it to existing ol on the target
-                if( tOl ) {
-                  try {
-                    //addedNode = placeholder;
-                    tOl.append( ev.drag.get("node") );
-                  } catch(e){ }
-                }
-
-                // else add a new ol to the target
-                else {
-
-                  // try adding newNode
-                  try{
-                    t.append( newNode );
-                    newNode.append( ev.drag.get( "node" ) );
-
-                    addedNode = newNode;
-
-
-                  } catch(e){ }
-                }
-                break;
-
-
-        // if we're over an ul, just add this as a new li child
-         /* case "ul":
-              try{
-                t.prepend( ev.drag.get("node" ) );
-                dump(t);
-              }
-              catch(e){}
-              break;*/
-
-         /* default:
-              break;
-
-        }
-
-
-    });
-});*/
+    var draghandle = Y.Node.one('.drag-handle').cloneNode(true);
--- local/ajaxcategories/changer.php
+++ local/ajaxcategories/changer.php
@@ -13,32 +13,31 @@ $after = optional_param('after', 0, PARAM_INT);
 $level = optional_param('level', '',PARAM_ALPHA);
 $dest = optional_param('dest', 0, PARAM_INT);
 
-    list($thispageurl, $contexts, $cmid, $cm, $module, $pagevars) =
-        question_edit_setup('categories', '/local/ajaxcategories/index.php');
+list($thispageurl, $contexts, $cmid, $cm, $module, $pagevars) =
+    question_edit_setup('categories', '/local/ajaxcategories/index.php');
 
-    // Get values from form for actions on this page.
-    $param = new stdClass();
+// Get values from form for actions on this page.
+$param = new stdClass();
 
-    $param->delete = optional_param('delete', 0, PARAM_INT);
-    $param->edit = optional_param('edit', 0, PARAM_INT);
+$param->delete = optional_param('delete', 0, PARAM_INT);
+$param->edit = optional_param('edit', 0, PARAM_INT);
 
-    $qcobject = new ajax_question_category_object($pagevars['cpage'], $thispageurl,
-        $contexts->having_one_edit_tab_cap('categories'), $param->edit,
-        $pagevars['cat'], $param->delete, $contexts->having_cap('moodle/question:add'));
+$qcobject = new ajax_question_category_object($pagevars['cpage'], $thispageurl,
+    $contexts->having_one_edit_tab_cap('categories'), $param->edit,
+    $pagevars['cat'], $param->delete, $contexts->having_cap('moodle/question:add'));
 
+// Get parametrs for changing category list.
+$fromlist = $qcobject->find_list($movingid);
+$environment = array();
+$environment['before'] = $before;
+$environment['after'] = $after;
+$environment['level'] = $level;
+$environment['dest'] = $dest;
+if ($environment['after'] !== -1) {
+    $tolist = $qcobject->find_list($environment['after']);
+} else {
+    $tolist = $qcobject->find_list($environment['before']);
+}
 
-    $fromlist = $qcobject->find_list($movingid);
-    //var_dump($fromlist);
-    $environment = array();
-    $environment['before'] = $before;
-    $environment['after'] = $after;
-    $environment['level'] = $level;
-    $environment['dest'] = $dest;
-    if ($environment['after'] !== -1) {
-        $tolist = $qcobject->find_list($environment['after']);
-    } else {
-        $tolist = $qcobject->find_list($environment['before']);
-    }
-    //var_dump($tolist->context->id);
-    $environment['dest'] = $tolist;
-    $fromlist->change_category_list($movingid, $environment);
+$environment['dest'] = $tolist;
+$fromlist->change_category_list($movingid, $environment);
