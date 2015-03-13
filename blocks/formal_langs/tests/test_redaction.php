42a91a75e7214a8c4d3b64cf195e6f7f6486279b
mamontov dp <mamontov.dp@gmail.com>
2015-03-13 23:33:04 +0300

    ??????, ????? redaction ??????????? ?????????? ???????-???????????. ???????? ?????.

--- blocks/formal_langs/tests/test_redaction.php
+++ blocks/formal_langs/tests/test_redaction.php
@@ -13,17 +13,24 @@ global $CFG;
 require_once($CFG->dirroot.'/blocks/formal_langs/tokens_base.php');
 
 class block_formal_langs_tokens_base_test extends UnitTestCase {
-    function test_redaction() {
-        $this->assertTrue(block_formal_langs_token_base::redaction('ggg', 'ggg')=='mmm');
-        $this->assertTrue(block_formal_langs_token_base::redaction('eh', 'helllo')=='imriii');
-        $this->assertTrue(block_formal_langs_token_base::redaction('helllo', 'eh')=='dmrddd');
-        $this->assertTrue(block_formal_langs_token_base::redaction('helllo', 'hlello')=='mimmmdm');
-        $this->assertTrue(block_formal_langs_token_base::redaction('hlello', 'helllo')=='mdmmmim');
-        $this->assertTrue(block_formal_langs_token_base::redaction('sunday', 'snuday')=='mimdmmm');
-        $this->assertTrue(block_formal_langs_token_base::redaction('hlelo', 'hello')=='mdmmim');
-        $this->assertTrue(block_formal_langs_token_base::redaction('hello', 'hllo')=='mdmmm');
-        $this->assertTrue(block_formal_langs_token_base::redaction('hlla', 'hello')=='mimmr');
-
+    /**
+     * Tests, whether redaction matches
+     * @param string $result resulting redaction
+     * @param string $ethalon an ethalon string
+     */
+    protected function assert_redaction($result, $ethalon) {
+        $this->assertTrue($result == $ethalon, $result);
+    }
+    public function test_redaction() {
+        $this->assert_redaction(block_formal_langs_token_base::redaction('ggg', 'ggg'), 'mmm');
+        $this->assert_redaction(block_formal_langs_token_base::redaction('eh', 'helllo'),'tiiii');
+        $this->assert_redaction(block_formal_langs_token_base::redaction('helllo', 'eh'),'tdddd');
+        $this->assert_redaction(block_formal_langs_token_base::redaction('helllo', 'hlello'),'mtmmm');
+        $this->assert_redaction(block_formal_langs_token_base::redaction('hlello', 'helllo'),'mtmmm');
+        $this->assert_redaction(block_formal_langs_token_base::redaction('sunday', 'snuday'),'mtmmm');
+        $this->assert_redaction(block_formal_langs_token_base::redaction('hlelo', 'hello'),'mtmm');
+        $this->assert_redaction(block_formal_langs_token_base::redaction('hello', 'hllo'),'mdmmm');
+        $this->assert_redaction(block_formal_langs_token_base::redaction('hlla', 'hello'),'mimmr');
     }
 }
 ?>
