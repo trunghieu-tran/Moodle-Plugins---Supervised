a2f2b3a5596a85e07f7275938e2aa98fc2e90b46
mamontov dp <mamontov.dp@gmail.com>
2015-03-30 17:12:42 +0300

    ?????????? ??????????? whatis ? ?????? ??????????? ?????? ? ????????, ????? ???????? ????? ?? ??????????? ?????? ???????????.

--- question/type/correctwriting/lexical_mistakes.php
+++ question/type/correctwriting/lexical_mistakes.php
@@ -52,6 +52,58 @@ class qtype_correctwriting_lexical_mistake extends qtype_correctwriting_response
         $this->tokenpair = $tokenpair;
         $this->str='typo_'.$this->tokenpair->correcttokens[0];
     }
+
+    /**
+     * Returns description for whatis hint as text
+     * @return string
+     */
+    public function what_is_description() {
+        $description = $this->token_descriptions();
+        $a = new stdClass();
+        $a->tokendescr = $description;
+
+        $comparedstring = '';
+        $indexes = $this->tokenpair->comparedtokens;
+        if (count($indexes)) {
+            sort($indexes);
+            $strings = array();
+            foreach($indexes as $index) {
+                /** @var block_formal_langs_token_base $token */
+                $token = $this->stringpair->comparedstring()->stream->tokens[$index];
+                $string = $token->value();
+                if (is_object($string)) {
+                    /** @var qtype_poasquestion\string $string */
+                    $string = $string->string();
+                }
+                $strings[]=$string;
+            }
+            $result = implode(' ', $strings);
+            $comparedstring = $result;
+        }
+
+        $a->tokenvalue = $comparedstring;
+        $a->inthiscase =  get_string('inyouranswer', 'qtype_correctwriting', $a);
+        if (!is_string($a->tokenvalue)) {
+            $a->tokenvalue = $a->tokenvalue->string();
+        }
+        $description = get_string('whatishint', 'qtype_correctwriting', $a);
+        return $description;
+    }
+
+    public function token_descriptions($andvalue = false) {
+        if ($this->tokenpair->type != block_formal_langs_matched_tokens_pair::TYPE_MISSING_SEPARATOR) {
+            return parent::token_descriptions($andvalue);
+        }
+        $correctstring = $this->stringpair->correctstring();
+        $hasdescriptions = true;
+        foreach ($this->tokenpair->correcttokens as $index) {
+            $hasdescriptions = $hasdescriptions && $correctstring->has_description($index);
+        }
+        if ($hasdescriptions) {
+            return parent::token_descriptions($andvalue);
+        }
+        return null;
+    }
 }
 
 class qtype_correctwriting_scanning_mistake extends qtype_correctwriting_lexical_mistake {
