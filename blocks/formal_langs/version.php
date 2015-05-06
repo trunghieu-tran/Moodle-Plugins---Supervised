19c779e51dc4d0f8f8d2205fc1ca2f90bf8886e9
mamontov dp <mamontov.dp@gmail.com>
2015-05-06 16:19:34 +0300

    ?????????? ????? C++ ???????? ?? cpp_parseable_language.

--- blocks/formal_langs/version.php
+++ blocks/formal_langs/version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_formal_langs';
-$plugin->version  = 2015042900;
+$plugin->version  = 2015050600;
 $plugin->requires = 2014111000;
 $plugin->release = 'Formal languages 2.8';
 $plugin->maturity = MATURITY_BETA;
