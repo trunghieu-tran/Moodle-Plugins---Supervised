e3f6149d814cfbf0bd8e0609fea3cf27846bd274
vad23klev <vad23klev@localhost>
2015-06-07 22:54:00 +0300

    ???????? ???? ?? ?????????????? ???????????? ???????????? ? ???????????????????.

--- question/type/correctwriting/tests/test_analyzers_interaction.php
+++ question/type/correctwriting/tests/test_analyzers_interaction.php
@@ -107,4 +107,34 @@ class qtype_correctwriting_analyzers_interaction_test extends advanced_testcase 
         $this->assertEquals('qtype_correctwriting_lexical_mistake', get_class($question->matchedresults->mistakes()[2]));
         $this->assertEquals('qtype_correctwriting_lexeme_moved_mistake', get_class($question->matchedresults->mistakes()[3]));
     }
+    // Test typo, drop, move and additional lexemes in enumerations. Enumeration and sequence analyzers.
+    public function test_typo_drop_moved_addition_lexemes_in_enumerations_enum_sequence() {
+        $language = new block_formal_langs_language_cpp_parseable_language();
+        $question = new qtype_correctwriting_question();
+        $question->usecase = true;
+        $question->lexicalerrorthreshold = 3000;
+        $question->lexicalerrorweight = 0.1;
+        $question->usedlanguage = $language;
+        $question->movedmistakeweight = 0.1;
+        $question->absentmistakeweight = 0.11;
+        $question->addedmistakeweight = 0.12;
+        $question->hintgradeborder = 0.75;
+        $question->maxmistakepercentage = 0.95;
+        $question->qtype = new qtype_correctwriting();
+        $question->islexicalanalyzerenabled = 0;
+        $question->isenumanalyzerenabled = 1;
+        $question->issequenceanalyzerenabled = 1;
+        $question->issyntaxanalyzerenabled = 0;
+        $answers = array((object)array('id' => 1, 'answer' => 'int i,j,k, hash, fraction;', 'fraction' => 1.0));
+        $question->answers = $answers;
+        $state = $question->grade_response(array('answer' => 'int k,je,fraction;f'));
+        $this->assertEquals(7, count($question->matchedresults->mistakes()));
+        $this->assertEquals('qtype_correctwriting_lexeme_absent_mistake', get_class($question->matchedresults->mistakes()[0]));
+        $this->assertEquals('qtype_correctwriting_lexeme_absent_mistake', get_class($question->matchedresults->mistakes()[1]));
+        $this->assertEquals('qtype_correctwriting_lexeme_absent_mistake', get_class($question->matchedresults->mistakes()[2]));
+        $this->assertEquals('qtype_correctwriting_lexeme_absent_mistake', get_class($question->matchedresults->mistakes()[3]));
+        $this->assertEquals('qtype_correctwriting_lexeme_absent_mistake', get_class($question->matchedresults->mistakes()[4]));
+        $this->assertEquals('qtype_correctwriting_lexeme_added_mistake', get_class($question->matchedresults->mistakes()[5]));
+        $this->assertEquals('qtype_correctwriting_lexeme_added_mistake', get_class($question->matchedresults->mistakes()[6]));
+    }
 }
