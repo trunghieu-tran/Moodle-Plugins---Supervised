78f7326c341c5f191d716b126f8276fa91f90f5d
????????? <?????????@localhost>
2016-01-04 21:13:55 +0400

    ?????? ?? ???????????? ? Supervised Check ???????? ?? ??????????.

--- mod/quiz/accessrule/supervisedcheck/lib.js
+++ mod/quiz/accessrule/supervisedcheck/lib.js
@@ -1,4 +1,4 @@
-// This file is part of Student Access Control Kit - https://code.google.com/p/oasychev-moodle-plugins/
+// This file is part of Student Access Control Kit - https://bitbucket.org/oasychev/moodle-plugins/overview
 //
 // Access Control Kit is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
